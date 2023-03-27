58be684ef0774512637091ae966f2efb5e2b78ab
girazoki <gorka.irazoki@gmail.com>
2023-03-27 16:47:14 +0200

    add test checking configuration changes

--- runtime/test/tests/integration_test.rs
+++ runtime/test/tests/integration_test.rs
@@ -407,6 +407,80 @@ fn test_parachains_deregister_collators_re_assigned() {
 }
 
 #[test]
+fn test_parachains_deregister_collators_config_change_reassigned() {
+    ExtBuilder::default()
+        .with_balances(vec![
+            // Alice gets 10k extra tokens for her mapping deposit
+            (AccountId::from(ALICE), 210_000 * UNIT),
+            (AccountId::from(BOB), 100_000 * UNIT),
+            (AccountId::from(CHARLIE), 100_000 * UNIT),
+            (AccountId::from(DAVE), 100_000 * UNIT),
+        ])
+        .with_collators(vec![
+            (AccountId::from(ALICE), 210 * UNIT),
+            (AccountId::from(BOB), 100 * UNIT),
+            (AccountId::from(CHARLIE), 100 * UNIT),
+            (AccountId::from(DAVE), 100 * UNIT),
+        ])
+        .with_para_ids(vec![1001, 1002])
+        .with_config(pallet_configuration::HostConfiguration {
+            max_collators: 100,
+            moondance_collators: 2,
+            collators_per_container: 2,
+        })
+        .build()
+        .execute_with(|| {
+            run_to_block(2, true);
+            // Assert current slot gets updated
+            assert_eq!(Aura::current_slot(), 1u64);
+            // slot 4, alice
+            assert!(Authorship::author().unwrap() == AccountId::from(BOB));
+
+            // Alice and Bob are authorities
+            let alice_id = get_aura_id_from_seed(&AccountId::from(ALICE).to_string());
+            let bob_id = get_aura_id_from_seed(&AccountId::from(BOB).to_string());
+
+            assert_eq!(Aura::authorities(), vec![alice_id, bob_id]);
+
+            // Set moondance collators to 1
+            assert_ok!(Configuration::set_moondance_collators(root_origin(), 1), ());
+
+            // Set container chain collators to 3
+            assert_ok!(
+                Configuration::set_collators_per_container(root_origin(), 3),
+                ()
+            );
+
+            // Charlie and Dave to 1001
+            let assignment = CollatorAssignment::collator_container_chain();
+            assert_eq!(
+                assignment.container_chains[&1001u32],
+                vec![CHARLIE.into(), DAVE.into()]
+            );
+
+            // Assignment should happen after 2 sessions
+            run_to_session(1u32, true);
+
+            let assignment = CollatorAssignment::collator_container_chain();
+            assert_eq!(
+                assignment.container_chains[&1001u32],
+                vec![CHARLIE.into(), DAVE.into()]
+            );
+
+            run_to_session(2u32, true);
+
+            // Charlie, Dave and BOB should be assigne dot para 1001 this time
+            let assignment = CollatorAssignment::collator_container_chain();
+            assert_eq!(
+                assignment.container_chains[&1001u32],
+                vec![CHARLIE.into(), DAVE.into(), BOB.into()]
+            );
+
+            assert_eq!(assignment.orchestrator_chain, vec![ALICE.into()]);
+        });
+}
+
+#[test]
 fn test_configuration_on_session_change() {
     ExtBuilder::default()
         .with_balances(vec![
