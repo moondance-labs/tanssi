af7302d14c2793e4ef9c8fa498cf24c8e6f0a0fc
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2023-03-31 10:03:32 +0200

    pallet-author-noting: mapping (#39)
    * Store author of multiple para_ids as a mapping

--- pallets/author-noting/src/test.rs
+++ pallets/author-noting/src/test.rs
@@ -1,17 +1,21 @@
 use crate::mock::*;
+use cumulus_primitives_core::ParaId;
 use hex_literal::hex;
 use parity_scale_codec::Encode;
 use sp_consensus_aura::inherents::InherentType;
 use sp_consensus_aura::AURA_ENGINE_ID;
 use sp_runtime::generic::DigestItem;
 use sp_runtime::traits::BlakeTwo256;
+
 #[test]
 fn test_author_id_insertion() {
     BlockTests::new()
         .with_relay_sproof_builder(|_, relay_block_num, sproof| match relay_block_num {
             1 => {
                 let slot: InherentType = 13u64.into();
-                sproof.author_id =
+                let mut s = OwnRelayStateSproofBuilderItem::default();
+                s.para_id = 1001.into();
+                s.author_id =
                     HeaderAs::NonEncoded(sp_runtime::generic::Header::<u32, BlakeTwo256> {
                         parent_hash: Default::default(),
                         number: Default::default(),
@@ -20,12 +24,13 @@ fn test_author_id_insertion() {
                         digest: sp_runtime::generic::Digest {
                             logs: vec![DigestItem::PreRuntime(AURA_ENGINE_ID, slot.encode())],
                         },
-                    })
+                    });
+                sproof.items.push(s);
             }
             _ => unreachable!(),
         })
         .add(1, || {
-            assert_eq!(AuthorNoting::latest_author(), Some(13u64));
+            assert_eq!(AuthorNoting::latest_author(ParaId::from(1001)), Some(13u64));
         });
 }
 
@@ -45,15 +50,90 @@ fn test_author_id_insertion_real_data() {
             );
 
             match relay_block_num {
-                1 => sproof.author_id = HeaderAs::AlreadyEncoded(statemint_data.to_vec()),
+                1 => {
+                    let mut s = OwnRelayStateSproofBuilderItem::default();
+                    s.para_id = 1001.into();
+                    s.author_id = HeaderAs::AlreadyEncoded(statemint_data.to_vec());
+                    sproof.items.push(s);
+                }
                 _ => unreachable!(),
             }
         })
         .add(1, || {
             assert_eq!(
-                AuthorNoting::latest_author(),
+                AuthorNoting::latest_author(ParaId::from(1001)),
                 // Our mock author fetcher will just note the slot
                 Some(140006956u64)
             );
+        });
+}
+
+#[test]
+fn test_author_id_insertion_many_paras() {
+    BlockTests::new()
+        .with_relay_sproof_builder(|_, relay_block_num, sproof| match relay_block_num {
+            1 => {
+                // Since the default parachain list is vec![1001],
+                // we must always include a sproof for this para_id
+                let slot: InherentType = 10u64.into();
+                let mut s = OwnRelayStateSproofBuilderItem::default();
+                s.para_id = 1001.into();
+                s.author_id =
+                    HeaderAs::NonEncoded(sp_runtime::generic::Header::<u32, BlakeTwo256> {
+                        parent_hash: Default::default(),
+                        number: Default::default(),
+                        state_root: Default::default(),
+                        extrinsics_root: Default::default(),
+                        digest: sp_runtime::generic::Digest {
+                            logs: vec![DigestItem::PreRuntime(AURA_ENGINE_ID, slot.encode())],
+                        },
+                    });
+                sproof.items.push(s);
+            }
+            2 => {
+                let slot: InherentType = 13u64.into();
+                let mut s = OwnRelayStateSproofBuilderItem::default();
+                s.para_id = 1001.into();
+                s.author_id =
+                    HeaderAs::NonEncoded(sp_runtime::generic::Header::<u32, BlakeTwo256> {
+                        parent_hash: Default::default(),
+                        number: Default::default(),
+                        state_root: Default::default(),
+                        extrinsics_root: Default::default(),
+                        digest: sp_runtime::generic::Digest {
+                            logs: vec![DigestItem::PreRuntime(AURA_ENGINE_ID, slot.encode())],
+                        },
+                    });
+                sproof.items.push(s);
+
+                let slot: InherentType = 14u64.into();
+                let mut s = OwnRelayStateSproofBuilderItem::default();
+                s.para_id = 1002.into();
+                s.author_id =
+                    HeaderAs::NonEncoded(sp_runtime::generic::Header::<u32, BlakeTwo256> {
+                        parent_hash: Default::default(),
+                        number: Default::default(),
+                        state_root: Default::default(),
+                        extrinsics_root: Default::default(),
+                        digest: sp_runtime::generic::Digest {
+                            logs: vec![DigestItem::PreRuntime(AURA_ENGINE_ID, slot.encode())],
+                        },
+                    });
+                sproof.items.push(s);
+            }
+            _ => unreachable!(),
+        })
+        .add(1, || {
+            // Writing to this pallet storage will only change the sproofs of the next block,
+            // not the ones of the current block
+            MockData::mutate(|m| {
+                m.container_chains = vec![1001.into(), 1002.into()];
+            });
+            assert_eq!(AuthorNoting::latest_author(ParaId::from(1001)), Some(10u64));
+            assert_eq!(AuthorNoting::latest_author(ParaId::from(1002)), None);
+        })
+        .add(2, || {
+            assert_eq!(AuthorNoting::latest_author(ParaId::from(1001)), Some(13u64));
+            assert_eq!(AuthorNoting::latest_author(ParaId::from(1002)), Some(14u64));
         });
 }
