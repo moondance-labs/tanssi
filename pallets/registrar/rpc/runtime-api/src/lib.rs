950740456795bf8ca58c0efdb905f82b730b813e
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-04-03 09:59:56 +0200

    Update docs

--- pallets/registrar/rpc/runtime-api/src/lib.rs
+++ pallets/registrar/rpc/runtime-api/src/lib.rs
@@ -1,11 +1,8 @@
 #![cfg_attr(not(feature = "std"), no_std)]
-//! Runtime API for CollatorAssignment pallet. Can be used by collators to check
-//! which parachain will they be collating, as well as the current assignment of
-//! collators to parachains and parachains to collators.
+//! Runtime API for Registrar pallet
 use scale_info::prelude::vec::Vec;
 
 sp_api::decl_runtime_apis! {
-    /// The API to query account nonce (aka transaction index).
     pub trait RegistrarApi<ParaId> where
         ParaId: parity_scale_codec::Codec,
     {
