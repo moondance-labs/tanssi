fc8f2e5e5d7f64fd3d94edba9bff724a40a6462f
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-04-04 09:44:32 +0200

    Add integration test, rename to registered_paras

--- runtime/test/src/lib.rs
+++ runtime/test/src/lib.rs
@@ -748,8 +748,8 @@ impl_runtime_apis! {
     }
 
     impl pallet_registrar_runtime_api::RegistrarApi<Block, u32> for Runtime {
-        /// Return the registered parachain ids
-        fn parachains() -> Vec<u32> {
+        /// Return the registered para ids
+        fn registered_paras() -> Vec<u32> {
             Registrar::registered_para_ids().to_vec()
         }
     }
