9086173d9920d80b1b162b9d6dec9af5b9061a82
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2023-04-12 17:30:17 +0200

    Update substrate 0.9.40 (#60)

--- pallets/registrar/src/lib.rs
+++ pallets/registrar/src/lib.rs
@@ -30,7 +30,6 @@ pub mod pallet {
     use sp_std::prelude::*;
 
     #[pallet::pallet]
-    #[pallet::generate_store(pub(super) trait Store)]
     #[pallet::without_storage_info]
     pub struct Pallet<T>(_);
 
