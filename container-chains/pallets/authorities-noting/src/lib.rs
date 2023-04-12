ca4632543b09aab4dd57e95baeb62653c6763ed3
girazoki <gorka.irazoki@gmail.com>
2023-04-12 12:15:48 +0200

    temp

---
+++ container-chains/pallets/authorities-noting/src/lib.rs
@@ -0,0 +1,209 @@
+//! # Author Noting Pallet
+//!
+//! This pallet notes the author of the different containerChains that have registered:
+//!
+//! The set of container chains is retrieved thanks to the GetContainerChains trait
+//! For each containerChain, we inspect the Header stored in the relayChain as
+//! a generic header. This is the first requirement for containerChains.
+//!
+//! The second requirement is that an Aura digest with the slot number for the containerChains
+//! needs to exist
+//!  
+//! Using those two requirements we can select who the author was based on the collators assigned
+//! to that containerChain, by simply assigning the slot position.
+//!
+#![cfg_attr(not(feature = "std"), no_std)]
+use cumulus_primitives_core::relay_chain::BlakeTwo256;
+use cumulus_primitives_core::relay_chain::BlockNumber;
+use cumulus_primitives_core::relay_chain::HeadData;
+use cumulus_primitives_core::ParaId;
+use frame_support::Hashable;
+use parity_scale_codec::Decode;
+use sp_consensus_aura::inherents::InherentType;
+use sp_inherents::InherentIdentifier;
+use sp_runtime::traits::Header;
+use sp_runtime::DispatchResult;
+use sp_std::prelude::*;
+use tp_authorities_noting_inherent::INHERENT_IDENTIFIER;
+use tp_authorities_noting_inherent::PARAS_HEADS_INDEX;
+use sp_runtime::traits::Hash as HashT;
+
+mod relay_state_snapshot;
+pub use relay_state_snapshot::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod test;
+
+pub use pallet::*;
+
+pub trait GetContainerChains {
+    fn container_chains() -> Vec<ParaId>;
+}
+
+#[frame_support::pallet]
+pub mod pallet {
+    use super::{DispatchResult, *};
+    use frame_support::dispatch::PostDispatchInfo;
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        type OrchestratorParaId: Get<ParaId>;
+        type SelfParaId: Get<ParaId>;
+    }
+
+    pub trait GetAuthorFromSlot<T: Config> {
+        /// Returns current session index.
+        fn author_from_inherent(inherent: InherentType, para_id: ParaId) -> Option<T::AccountId>;
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// The new value for a configuration parameter is invalid.
+        FailedReading,
+        FailedDecodingHeader,
+        AuraDigestFirstItem,
+        AsPreRuntimeError,
+        NonDecodableSlot,
+        AuthorNotFound,
+        NonAuraDigest,
+    }
+
+    #[pallet::pallet]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(PhantomData<T>);
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        #[pallet::call_index(0)]
+        #[pallet::weight((0, DispatchClass::Mandatory))]
+        // TODO: This weight should be corrected.
+        pub fn set_latest_authorities_data(
+            origin: OriginFor<T>,
+            data: tp_authorities_noting_inherent::ContainerChainAuthoritiesInherentData,
+        ) -> DispatchResultWithPostInfo {
+            let total_weight = Weight::zero();
+            ensure_none(origin)?;
+            let tp_authorities_noting_inherent::ContainerChainAuthoritiesInherentData {
+                validation_data: vfp,
+                relay_chain_state,
+                orchestrator_chain_state
+            } = data;
+
+            let para_id = T::OrchestratorParaId::get();
+            let relay_state_proof = AuthoritiesNotingRelayChainStateProof::new(
+                vfp.relay_parent_storage_root,
+                relay_chain_state.clone(),
+            )
+            .expect("Invalid relay chain state proof");
+
+            let orchestrator_root = Self::fetch_orchestrator_header_from_relay_proof(&relay_state_proof, para_id).expect("qed");
+
+            let orchestrator_state_proof = AuthoritiesNotingRelayChainStateProof::new(
+                orchestrator_root,
+                orchestrator_chain_state.clone(),
+            )
+            .expect("Invalid orchestrator chain state proof");
+
+            match Self::fetch_authorities_from_orchestrator_proof(&orchestrator_state_proof, T::SelfParaId::get()) {
+                Ok(authorities) => Authorities::<T>::put(authorities),
+                Err(e) => log::warn!("Author-noting error {:?} found in para {:?}", e, para_id),
+            }
+            
+
+            Ok(PostDispatchInfo {
+                actual_weight: Some(total_weight),
+                pays_fee: Pays::No,
+            })
+        }
+    }
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Latest author changed
+        LatestAuthorChanged {
+            para_id: ParaId,
+            new_author: T::AccountId,
+        },
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn authorities)]
+    pub(super) type Authorities<T: Config> =
+        StorageValue<_, Vec<T::AccountId>, ValueQuery>;
+
+    #[pallet::inherent]
+    impl<T: Config> ProvideInherent for Pallet<T> {
+        type Call = Call<T>;
+        type Error = sp_inherents::MakeFatalError<()>;
+        // TODO, what should we put here
+        const INHERENT_IDENTIFIER: InherentIdentifier =
+            INHERENT_IDENTIFIER;
+
+        fn create_inherent(data: &InherentData) -> Option<Self::Call> {
+            let data: tp_authorities_noting_inherent::ContainerChainAuthoritiesInherentData = data
+                .get_data(&INHERENT_IDENTIFIER)
+                .ok()
+                .flatten()
+                .expect("there is not data to be posted; qed");
+
+            Some(Call::set_latest_authorities_data { data })
+        }
+
+        fn is_inherent(call: &Self::Call) -> bool {
+            matches!(call, Call::set_latest_authorities_data { .. })
+        }
+    }
+}
+
+impl<T: Config> Pallet<T> {
+    /// Fetch author slot from a proof of header
+    fn fetch_orchestrator_header_from_relay_proof(
+        relay_state_proof: &AuthoritiesNotingRelayChainStateProof,
+        para_id: ParaId,
+    ) -> Result<<BlakeTwo256 as HashT>::Output, Error<T>> {
+        let bytes = para_id.twox_64_concat();
+        // CONCAT
+        let key = [PARAS_HEADS_INDEX, bytes.as_slice()].concat();
+        // We might encounter empty vecs
+        // We only note if we can decode
+        // In this process several errors can occur, but we will only log if such errors happen
+        // We first take the HeadData
+        // If the readError was that the key was not provided (identified by the Proof error),
+        // then panic
+        let head_data = relay_state_proof
+            .read_entry::<HeadData>(key.as_slice(), None)
+            .map_err(|e| match e {
+                ReadEntryErr::Proof => panic!("Invalid proof provided for para head key"),
+                _ => Error::<T>::FailedReading,
+            })?;
+
+        // We later take the Header decoded
+        let mut orchestrator_chain_header = sp_runtime::generic::Header::<BlockNumber, BlakeTwo256>::decode(
+            &mut head_data.0.as_slice(),
+        )
+        .map_err(|_| Error::<T>::FailedDecodingHeader)?
+        .clone();
+
+        let orchestrator_chain_storage_root = orchestrator_chain_header.state_root;
+
+        Ok(orchestrator_chain_storage_root)
+    }
+
+     /// Fetch author slot from a proof of header
+     fn fetch_authorities_from_orchestrator_proof(
+        orchestrator_state_proof: &AuthoritiesNotingRelayChainStateProof,
+        para_id: ParaId,
+    ) -> Result<Vec<T::AccountId>, Error<T>> {
+
+        Ok(vec![])
+    }
+}
---
+++ primitives/authorities-noting-inherent/src/lib.rs
@@ -0,0 +1,63 @@
+//! # Author Noting Inherent Primitives
+//!
+//! This crate defines those primitives that should be taken into account when building
+//! the author-noting pallet inherent
+//!
+//! In particular this crate contains:
+//! - The hardcoded relay key that needs to be read
+//! - The Inherent identifier
+//! - The client side trait implementations to introduce the inherent
+//! - The mock version that gets used both in test files and manual seal
+//! - The sproof builder that generates a fake proof that mimics the relay chain sproof
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use cumulus_primitives_core::ParaId;
+use cumulus_primitives_core::PersistedValidationData;
+use parity_scale_codec::{Decode, Encode};
+use scale_info::TypeInfo;
+use sp_inherents::InherentIdentifier;
+use sp_io::hashing::twox_64;
+use sp_std::vec::Vec;
+
+/*
+#[cfg(feature = "std")]
+mod client_side;
+#[cfg(feature = "std")]
+pub use client_side::*;
+#[cfg(feature = "std")]
+mod mock;
+#[cfg(feature = "std")]
+pub use mock::*;
+#[cfg(feature = "std")]
+mod sproof_builder;
+#[cfg(feature = "std")]
+pub use sproof_builder::*;
+#[cfg(test)]
+mod tests;
+*/
+
+#[derive(Encode, Decode, sp_core::RuntimeDebug, Clone, PartialEq, TypeInfo)]
+pub struct ContainerChainAuthoritiesInherentData {
+    pub validation_data: PersistedValidationData,
+    pub relay_chain_state: sp_trie::StorageProof,
+    pub orchestrator_chain_state: sp_trie::StorageProof,
+}
+
+// Identifier of the author-noting inherent
+pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"ccno1337";
+
+// They key to retrieve the para heads
+pub const PARAS_HEADS_INDEX: &[u8] =
+    &hex_literal::hex!["cd710b30bd2eab0352ddcc26417aa1941b3c252fcb29d88eff4f3de5de4476c3"];
+
+// Retrieves the full key representing the para->heads and the paraId
+pub fn para_id_head(para_id: ParaId) -> Vec<u8> {
+    para_id.using_encoded(|para_id: &[u8]| {
+        PARAS_HEADS_INDEX
+            .iter()
+            .chain(twox_64(para_id).iter())
+            .chain(para_id.iter())
+            .cloned()
+            .collect()
+    })
+}
