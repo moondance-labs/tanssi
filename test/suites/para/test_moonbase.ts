b16506d7422c18019d954e3fac09a5be152fc834
girazoki <gorka.irazoki@gmail.com>
2023-04-25 19:02:07 +0200

    Push first zombienet test to ci

--- test/suites/para/test_moonbase.ts
+++ test/suites/para/test_moonbase.ts
@@ -11,14 +11,14 @@ describeSuite({
     let relayApi: ApiPromise;
 
     beforeAll(async () => {
-      paraApi = context.polkadotJs({ type: "polkadotJs" });
-      relayApi = context.polkadotJs({ type: "polkadotJs" });
+      paraApi = context.polkadotJs({ apiName: "Tanssi", type: "polkadotJs" });
+      relayApi = context.polkadotJs({ apiName: "Relay", type: "polkadotJs" });
 
       const relayNetwork = relayApi.consts.system.version.specName.toString();
       expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
 
       const paraNetwork = paraApi.consts.system.version.specName.toString();
-      expect(paraNetwork, "Para API incorrect").to.contain("moonbase");
+      expect(paraNetwork, "Para API incorrect").to.contain("orchestrator-template-parachain");
 
       const currentBlock = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
       expect(currentBlock, "Parachain not producing blocks").to.be.greaterThan(0);
@@ -28,8 +28,8 @@ describeSuite({
       id: "T01",
       title: "Blocks are being produced on parachain",
       test: async function () {
-       // const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
-       // expect(blockNum).to.be.greaterThan(0);
+        const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
+        expect(blockNum).to.be.greaterThan(0);
       },
     });
   },
