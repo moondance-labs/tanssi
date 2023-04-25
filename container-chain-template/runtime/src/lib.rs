bb4cd34eff1837e6f80afc62c6722feeb6345d81
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-04-25 11:20:16 +0200

    Run cargo fmt

--- container-chain-template/runtime/src/lib.rs
+++ container-chain-template/runtime/src/lib.rs
@@ -6,38 +6,42 @@
 #[cfg(feature = "std")]
 include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
 
-use cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases;
-use cumulus_primitives_core::ParaId;
-use frame_support::weights::constants::RocksDbWeight;
-use frame_support::weights::constants::{BlockExecutionWeight, ExtrinsicBaseWeight};
-use smallvec::smallvec;
-use sp_api::impl_runtime_apis;
-use sp_core::{crypto::KeyTypeId, OpaqueMetadata};
-use sp_runtime::{
-    create_runtime_str, generic, impl_opaque_keys,
-    traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, Verify},
-    transaction_validity::{TransactionSource, TransactionValidity},
-    ApplyExtrinsicResult, MultiSignature,
-};
-
-use sp_std::prelude::*;
-#[cfg(feature = "std")]
-use sp_version::NativeVersion;
-use sp_version::RuntimeVersion;
-
-use frame_support::{
-    construct_runtime,
-    dispatch::DispatchClass,
-    parameter_types,
-    traits::{ConstU32, ConstU64, Everything},
-    weights::{
-        constants::WEIGHT_REF_TIME_PER_SECOND, Weight, WeightToFeeCoefficient,
-        WeightToFeeCoefficients, WeightToFeePolynomial,
+use {
+    cumulus_pallet_parachain_system::RelayNumberStrictlyIncreases,
+    cumulus_primitives_core::ParaId,
+    frame_support::weights::constants::{BlockExecutionWeight, ExtrinsicBaseWeight, RocksDbWeight},
+    smallvec::smallvec,
+    sp_api::impl_runtime_apis,
+    sp_core::{crypto::KeyTypeId, OpaqueMetadata},
+    sp_runtime::{
+        create_runtime_str, generic, impl_opaque_keys,
+        traits::{AccountIdLookup, BlakeTwo256, Block as BlockT, IdentifyAccount, Verify},
+        transaction_validity::{TransactionSource, TransactionValidity},
+        ApplyExtrinsicResult, MultiSignature,
     },
 };
-use frame_system::limits::{BlockLength, BlockWeights};
-pub use sp_consensus_aura::sr25519::AuthorityId as AuraId;
-pub use sp_runtime::{MultiAddress, Perbill, Permill};
+
+#[cfg(feature = "std")]
+use sp_version::NativeVersion;
+use {sp_std::prelude::*, sp_version::RuntimeVersion};
+
+use {
+    frame_support::{
+        construct_runtime,
+        dispatch::DispatchClass,
+        parameter_types,
+        traits::{ConstU32, ConstU64, Everything},
+        weights::{
+            constants::WEIGHT_REF_TIME_PER_SECOND, Weight, WeightToFeeCoefficient,
+            WeightToFeeCoefficients, WeightToFeePolynomial,
+        },
+    },
+    frame_system::limits::{BlockLength, BlockWeights},
+};
+pub use {
+    sp_consensus_aura::sr25519::AuthorityId as AuraId,
+    sp_runtime::{MultiAddress, Perbill, Permill},
+};
 
 #[cfg(any(feature = "std", test))]
 pub use sp_runtime::BuildStorage;
@@ -138,8 +142,10 @@ impl WeightToFeePolynomial for WeightToFee {
 /// of data like extrinsics, allowing for them to continue syncing the network through upgrades
 /// to even the core data structures.
 pub mod opaque {
-    use super::*;
-    use sp_runtime::{generic, traits::BlakeTwo256};
+    use {
+        super::*,
+        sp_runtime::{generic, traits::BlakeTwo256},
+    };
 
     pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
     /// Opaque block header type.
