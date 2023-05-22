4c86fb42de21e771cd4ddd06c6c74073c226027e
girazoki <gorka.irazoki@gmail.com>
2023-05-22 15:32:34 +0200

    fix typo

--- test/util/relayInterface.ts
+++ test/util/relayInterface.ts
@@ -3,7 +3,7 @@ import type { Header, ParaId } from '@polkadot/types/interfaces';
 
 export async function getHeaderFromRelay(relayApi: ApiPromise, paraId: ParaId): Promise<Header | null> {
     // Get the latest header from relay storage
-    const encoded = (await relayApi.query.  paras.heads(paraId));
+    const encoded = (await relayApi.query.paras.heads(paraId));
     const header = await relayApi.createType(
         "Header",
         encoded
