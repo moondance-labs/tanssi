6e322e87b24c5320e260abdb1383c667dde129a9
girazoki <gorka.irazoki@gmail.com>
2023-05-22 15:16:11 +0200

    change comment

--- test/util/relayInterface.ts
+++ test/util/relayInterface.ts
@@ -2,7 +2,7 @@ import { ApiPromise } from "@moonwall/cli";
 import type { Header, ParaId } from '@polkadot/types/interfaces';
 
 export async function getHeaderFromRelay(relayApi: ApiPromise, paraId: ParaId): Promise<Header | null> {
-    // Get the latest author from Digest
+    // Get the latest header from relay storage
     const encoded = (await relayApi.query.  paras.heads(paraId));
     const header = await relayApi.createType(
         "Header",
