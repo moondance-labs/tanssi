a147d00d13fdc87bf839a916195684791034329d
girazoki <gorka.irazoki@gmail.com>
2023-06-01 13:16:39 +0200

    Add a few more tests

---
+++ test/suites/dev-frontier-template/test-eth-fee/test-eth-paysFee.ts
@@ -0,0 +1,24 @@
+import { describeSuite, extractInfo, expect, TransactionTypes } from "@moonwall/cli";
+import { BALTATHAR_ADDRESS, GLMR, createRawTransfer } from "@moonwall/util";
+
+// We use ethers library in this test as apparently web3js's types are not fully EIP-1559
+// compliant yet.
+describeSuite({
+  id: "D1002",
+  title: "Ethereum - PaysFee",
+  foundationMethods: "dev",
+  testCases: ({ context, it }) => {
+    it({
+        id: `T01`,
+        title: `should be false for successful ethereum transactions`,
+        test: async function () {
+          const { result } = await context.createBlock(
+            await createRawTransfer(context, BALTATHAR_ADDRESS, GLMR)
+          );
+          const info = extractInfo(result!.events)!;
+          expect(info).to.not.be.empty;
+          expect(info.paysFee.isYes, "Transaction should be marked as paysFees == no").to.be.false;
+        },
+      });
+  },
+});
