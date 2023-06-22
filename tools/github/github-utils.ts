08ec62c7d96fbc66969678477a6de17147f2dce2
girazoki <gorka.irazoki@gmail.com>
2023-06-22 17:20:51 +0200

    start preparing build-runtime job (#127)
    * Start moving things around
    
    * Dancebox introduction
    
    * few cosmetic changesd
    
    * start preparing build-runtime job
    
    * start testing
    
    * Add job
    
    * modify rust toolchain to see if that passes
    
    * Revert "modify rust toolchain to see if that passes"
    
    This reverts commit 0e78b6ac34c0f45e07995f7660227faa177234d0.
    
    * fix
    
    * fmt
    
    * tools to publish wasm
    
    * remove publish runtime
    
    * modify tools
    
    * trigger on push
    
    * fmt
    
    * tsconfig
    
    * github-utils
    
    * wip
    
    * try-with-tags
    
    * revert and put it in correct place
    
    * use new tag
    
    * build runtime remove
    
    * dummycollator for full node
    
    * add back build runtime
    
    * dancebox for orchestrator
    
    * revert back
    
    * get back to dispatch flow
    
    * npm for pnpm
    
    * trigger to see if it works
    
    * Revert "npm for pnpm"
    
    This reverts commit a832dceb1c2b4e5e24fe4d9cf267360ef1e20b52.
    
    * Revert "Revert "npm for pnpm""
    
    This reverts commit c88f3e3fa30d7d0058eec83b896b8cf00c39f8dd.
    
    * Revert "Revert "Revert "npm for pnpm"""
    
    This reverts commit 751421bca1043a1abda4166d148a3c52955472b2.
    
    * workflow dispatch
    
    * fail job if docker image for sr-tool is not found
    
    * Comments on  actions
    
    * trigger on push
    
    * rename
    
    * Update cargo lock
    
    * Revert "trigger on push"
    
    This reverts commit 9fff59f359ca8945870cca08fdca247ce5d5d06e.
    
    * remove pnpm
    
    * fix yml

---
+++ tools/github/github-utils.ts
@@ -0,0 +1,111 @@
+import { Octokit } from "octokit";
+import { execSync } from "child_process";
+
+// Typescript 4 will support it natively, but not yet :(
+type Await<T> = T extends PromiseLike<infer U> ? U : T;
+type Commits = Await<ReturnType<Octokit["rest"]["repos"]["compareCommits"]>>["data"]["commits"];
+
+export function getCompareLink(packageName: string, previousTag: string, newTag: string) {
+  const previousPackage = execSync(
+    `git show ${previousTag}:../Cargo.lock | grep ${packageName}? | head -1 | grep -o '".*"'`
+  ).toString();
+  const previousCommit = /#([0-9a-f]*)/g.exec(previousPackage)[1].slice(0, 8);
+  const previousRepo = /(https:\/\/.*)\?/g.exec(previousPackage)[1];
+
+  const newPackage = execSync(
+    `git show ${newTag}:../Cargo.lock | grep ${packageName}? | head -1 | grep -o '".*"'`
+  ).toString();
+  const newCommit = /#([0-9a-f]*)/g.exec(newPackage)[1].slice(0, 8);
+  const newRepo = /(https:\/\/.*)\?/g.exec(newPackage)[1];
+  const newRepoOrganization = /github.com\/([^\/]*)/g.exec(newRepo)[1];
+
+  const diffLink =
+    previousRepo !== newRepo
+      ? `${previousRepo}/compare/${previousCommit}...${newRepoOrganization}:${newCommit}`
+      : `${previousRepo}/compare/${previousCommit}...${newCommit}`;
+
+  return diffLink;
+}
+
+export async function getCommitAndLabels(
+  octokit: Octokit,
+  owner: string,
+  repo: string,
+  previousTag: string,
+  newTag: string
+): Promise<{ prByLabels: any; commits: any[] }> {
+  let commits: Commits = [];
+  let more = true;
+  let page = 0;
+  while (more) {
+    const compare = await octokit.rest.repos.compareCommitsWithBasehead({
+      owner,
+      repo,
+      basehead: previousTag + "..." + newTag,
+      per_page: 200,
+      page,
+    });
+    commits = commits.concat(compare.data.commits);
+    more = compare.data.commits.length == 200;
+    page++;
+  }
+
+  // Determine commits to exclude
+  // - commits reverted in the same range
+  const excludedCommits: number[] = [];
+  const revertedCommits: number[] = [];
+  for (let i = commits.length - 1; i >= 0; i--) {
+    const commitMessageFirstLine = commits[i].commit.message.split("\n")[0].trim();
+
+    if (revertedCommits[commitMessageFirstLine] != null) {
+      excludedCommits.push(i);
+      excludedCommits.push(revertedCommits[commitMessageFirstLine]);
+    } else {
+      const foundRevertedCommitName = commitMessageFirstLine.match(/Revert \"(.*)\"/);
+      if (foundRevertedCommitName?.length > 0) {
+        revertedCommits[foundRevertedCommitName[1]] = i;
+      }
+    }
+  }
+
+  const prByLabels = {};
+  for (let i = 0; i < commits.length; i++) {
+    const commitMessageFirstLine = commits[i].commit.message.split("\n")[0].trim();
+    if (!excludedCommits.includes(i)) {
+      const foundPrsNumbers = commitMessageFirstLine.match(/\(#([0-9]+)\)$/);
+      if (foundPrsNumbers && foundPrsNumbers.length > 1) {
+        // This will check current repo and if the PR is not found, will try the official repo
+        const repos = [
+          { owner, repo },
+          { owner: "moondance-labs", repo: "tanssi" },
+        ];
+        for (const { owner, repo } of repos) {
+          try {
+            const pr = await octokit.rest.pulls.get({
+              owner,
+              repo,
+              pull_number: parseInt(foundPrsNumbers[1]),
+            });
+
+            if (pr.data.labels && pr.data.labels.length > 0) {
+              for (const label of pr.data.labels) {
+                prByLabels[label.name] = prByLabels[label.name] || [];
+                prByLabels[label.name].push(pr.data);
+              }
+            } else {
+              prByLabels[""] = prByLabels[""] || [];
+              prByLabels[""].push(pr);
+            }
+            break;
+          } catch (e) {
+            // PR not found... let's try the other repo
+          }
+        }
+      }
+    }
+  }
+  return {
+    prByLabels,
+    commits,
+  };
+}
