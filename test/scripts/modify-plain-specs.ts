442c82230fba261328f0641588e429767c219614
girazoki <gorka.irazoki@gmail.com>
2023-07-17 16:00:16 +0200

    start working on zombienbet upgrade tests

---
+++ test/scripts/modify-plain-specs.ts
@@ -0,0 +1,42 @@
+import fs from "fs/promises";
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { convertExponentials } from "@zombienet/utils";
+import jsonBg from "json-bigint";
+const JSONbig = jsonBg({ useNativeBigInt: true });
+
+const ALICE_ADDRESS = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+yargs(hideBin(process.argv))
+  .usage("Usage: $0")
+  .version("2.0.0")
+  .command(
+    `process <inputPath> <outputPath>`,
+    "Overwrites a plainSpec with Alice modifications",
+    (yargs) => {
+      return yargs
+        .positional("inputPath", {
+          describe: "Input path for plainSpecFile to modify",
+          type: "string",
+        })
+        .positional("outputPath", {
+          describe: "Output path for modified file",
+          type: "string",
+        });
+    },
+    async (argv) => {
+      process.stdout.write(`Reading from: ${argv.inputPath} ...`);
+      const plainSpec = JSONbig.parse((await fs.readFile(argv.inputPath!)).toString());
+      process.stdout.write(`Done ✅\n`);
+
+      plainSpec.bootNodes = [];
+      plainSpec.genesis.runtime.collatorSelection.invulnerables = [ALICE_ADDRESS];
+
+      process.stdout.write(`Writing to: ${argv.outputPath} ...`);
+      await fs.writeFile(
+        argv.outputPath!,
+        convertExponentials(JSONbig.stringify(plainSpec, null, 3))
+      );
+      process.stdout.write(`Done ✅\n`);
+    }
+  )
+  .parse();
---
+++ test/suites/rt-upgrade/test-upgrade-chain.ts
@@ -0,0 +1,80 @@
+import { MoonwallContext, beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { BALTATHAR_ADDRESS, KeyringPair, charleth } from "@moonwall/util";
+import { ApiPromise } from "@polkadot/api";
+import { Signer, ethers } from "ethers";
+import fs from "node:fs";
+import { ApiPromise, Keyring } from "@polkadot/api";
+
+const ALICE_ADDRESS = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
+describeSuite({
+  id: "ZAN",
+  title: "Zombie AlphaNet Upgrade Test",
+  foundationMethods: "zombie",
+  testCases: function ({ it, context, log }) {
+    let paraApi: ApiPromise;
+    let relayApi: ApiPromise;
+    let ethersSigner: Signer;
+    let alice: KeyringPair;
+
+    beforeAll(async () => {
+      const keyring = new Keyring({ type: 'sr25519' });
+      alice = keyring.addFromUri('//Alice', { name: 'Alice default' });
+      paraApi = context.polkadotJs({ type: "moon", apiName: "parachain" });
+      relayApi = context.polkadotJs({ type: "polkadotJs", apiName: "relaychain" });
+      ethersSigner = context.ethers();
+
+      const relayNetwork = relayApi.consts.system.version.specName.toString();
+      expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
+
+      const paraNetwork = paraApi.consts.system.version.specName.toString();
+      expect(paraNetwork, "Para API incorrect").to.contain("dancebox");
+
+      const currentBlock = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
+      expect(currentBlock, "Parachain not producing blocks").to.be.greaterThan(0);
+    }, 120000);
+
+    it({
+      id: "T01",
+      title: "Blocks are being produced on parachain",
+      test: async function () {
+        const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
+        expect(blockNum).to.be.greaterThan(0);
+      },
+    });
+
+    it({
+      id: "T02",
+      title: "Chain can be upgraded",
+      timeout: 600000,
+      test: async function () {
+        const blockNumberBefore = (
+          await paraApi.rpc.chain.getBlock()
+        ).block.header.number.toNumber();
+        const currentCode = await paraApi.rpc.state.getStorage(":code");
+        const codeString = currentCode.toString();
+
+        const wasm = fs.readFileSync(MoonwallContext.getContext().rtUpgradePath);
+        const rtHex = `0x${wasm.toString("hex")}`;
+
+        if (rtHex === codeString) {
+          log("Runtime already upgraded, skipping test");
+          return;
+        } else {
+          log("Runtime not upgraded, proceeding with test");
+          log("Current runtime hash: " + rtHex.slice(0, 10) + "..." + rtHex.slice(-10));
+          log("New runtime hash: " + codeString.slice(0, 10) + "..." + codeString.slice(-10));
+        }
+
+        await context.upgradeRuntime({ from: alice, logger: log});
+        await context.waitBlock(2);
+        const blockNumberAfter = (
+          await paraApi.rpc.chain.getBlock()
+        ).block.header.number.toNumber();
+        log(`Before: #${blockNumberBefore}, After: #${blockNumberAfter}`);
+        expect(blockNumberAfter, "Block number did not increase").to.be.greaterThan(
+          blockNumberBefore
+        );
+      },
+    });
+  },
+});
