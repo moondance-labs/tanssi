8b53cf2401517332f1b6a2e5bce3b6cb4af06867
girazoki <gorka.irazoki@gmail.com>
2023-07-24 10:17:41 +0200

    issues to create runtime and client releases

---
+++ tools/github/print-client-release-issue.ts
@@ -0,0 +1,72 @@
+import yargs from "yargs";
+
+async function main() {
+  const argv = yargs(process.argv.slice(2))
+    .usage("Usage: npm run ts-node github/generate-gh-issue-client-release.ts [args]")
+    .version("1.0.0")
+    .options({
+      from: {
+        type: "string",
+        describe: "previous client version",
+        required: true,
+      },
+      to: {
+        type: "string",
+        describe: "next client version",
+        required: true,
+      },
+    })
+    .demandOption(["from", "to"])
+    .help().argv;
+
+  const previousVersion = argv.from;
+  const newVersion = argv.to;
+
+  const commonTemplate = `
+  - [ ] Start the github action Publish Binary Draft with ${previousVersion} => ${newVersion}
+  (master branch).
+  - [ ] Review the generated Draft and clean a bit the messages if needed (keep it draft).
+  - [ ] Update chain-networks stagenet-dancebox config.json to include sha-xxxxx
+  (matching your ${newVersion} tag) and increase the config version + 1.
+  - [ ] Test the new client on stagenet-dancebox.
+  - [ ] Publish the client release draft.
+  - [ ] When everything is ok, publish the new docker image: start github action Publish Docker
+  with ${newVersion}.
+`;
+
+  // Detect if it's a major release or hotfix
+  if (newVersion.endsWith(".0")) {
+    const template = `
+## Requirements
+- [ ] To be manually edited (add pending PRs)
+
+## Pre-Release
+- [ ] Get that PR approved and merged.
+- [ ] Re-run all extrinsics/hooks benchmarks.
+
+## Release
+- [ ] Tag master with ${newVersion} and push to github
+${commonTemplate}
+
+## Post Release
+- [ ] Bump client version to the next one on master
+    `;
+    console.log(template);
+  } else {
+    const template = `
+## Requirements
+- [ ] To be manually edited (add pending PRs)
+
+## Pre-Release
+- [ ] Create branch \`perm-${newVersion}\` against previous client git tag.
+- [ ] In the branch \`perm-${newVersion}\`, bump client version to ${newVersion}.
+
+## Release
+- [ ] Tag \`perm-${newVersion}\` with ${newVersion} and push to github.
+${commonTemplate}
+    `;
+    console.log(template);
+  }
+}
+
+main();
