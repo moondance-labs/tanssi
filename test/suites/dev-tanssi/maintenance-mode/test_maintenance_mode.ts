5ce04453a04984acdc88f352442f74edab5aa94a
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-07-27 18:17:51 +0200

    Add more tests

--- test/suites/dev-tanssi/maintenance-mode/test_maintenance_mode.ts
+++ test/suites/dev-tanssi/maintenance-mode/test_maintenance_mode.ts
@@ -90,17 +90,85 @@ describeSuite({
 
         const balanceBefore = (await polkadotJs.query.system.account(bob.address)).data.free;
 
-        const tx = polkadotJs.tx.balances
-          .transfer(bob.address, 1000);
+        const tx = polkadotJs.tx.balances.transfer(bob.address, 1000);
 
         await context.createBlock([
             await tx.signAsync(alice),
         ]);
         const balanceAfter = (await polkadotJs.query.system.account(bob.address)).data.free;
 
-        console.log("Before, after", balanceBefore, " / ", balanceAfter);
-
         expect(balanceBefore.eq(balanceAfter)).to.be.true;
+      },
+    });
+
+    it({
+      id: "E05",
+      title: "Signed origin cannot disable maintenance mode",
+      test: async function () {
+        await context.createBlock();
+        await context.createBlock();
+
+        const tx = polkadotJs.tx.maintenanceMode.resumeNormalOperation();
+        await context.createBlock([
+          await tx.signAsync(alice),
+        ]);
+
+        const events = await polkadotJs.query.system.events();
+        const ev1 = events.filter(
+          (a) => {
+            return a.event.method == "ExtrinsicFailed";
+        });
+        expect(ev1.length).to.be.equal(1);
+
+        const enabled = (await polkadotJs.query.maintenanceMode.maintenanceMode()).toJSON();
+        expect(enabled).to.be.true;
+      },
+    });
+
+    it({
+      id: "E06",
+      title: "Root origin can disable maintenance mode",
+      test: async function () {
+        await context.createBlock();
+
+        const tx = polkadotJs.tx.maintenanceMode.resumeNormalOperation();
+        await context.createBlock([
+          await polkadotJs.tx.sudo.sudo(tx).signAsync(alice),
+        ]);
+
+        const events = await polkadotJs.query.system.events();
+        const ev1 = events.filter(
+          (a) => {
+            return a.event.method == "NormalOperationResumed";
+        });
+        expect(ev1.length).to.be.equal(1);
+
+        const enabled = (await polkadotJs.query.maintenanceMode.maintenanceMode()).toJSON();
+        expect(enabled).to.be.false;
+      },
+    });
+
+    it({
+      id: "E07",
+      title: "Transfers allowed again after disabling maintenance mode",
+      test: async function () {
+        await context.createBlock();
+        await context.createBlock();
+        await context.createBlock();
+
+        const enabled = (await polkadotJs.query.maintenanceMode.maintenanceMode()).toJSON();
+        expect(enabled).to.be.false;
+
+        const balanceBefore = (await polkadotJs.query.system.account(bob.address)).data.free;
+
+        const tx = polkadotJs.tx.balances.transfer(bob.address, 1000);
+
+        await context.createBlock([
+            await tx.signAsync(alice),
+        ]);
+        const balanceAfter = (await polkadotJs.query.system.account(bob.address)).data.free;
+
+        expect(balanceBefore.lt(balanceAfter)).to.be.true;
       },
     });
     },
