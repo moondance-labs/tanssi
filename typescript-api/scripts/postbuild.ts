765152225f367ad41c19d7d0872f721d72dcdb5c
timbrinded <79199034+timbrinded@users.noreply.github.com>
2023-08-14 15:07:07 +0100

    removed legacy logic

--- typescript-api/scripts/postbuild.ts
+++ typescript-api/scripts/postbuild.ts
@@ -2,21 +2,21 @@ import { writeFileSync, copyFileSync, readFileSync } from "fs";
 import path from "path";
 
 async function main() {
-  console.log("Loading package.json");
+  // console.log("Loading package.json");
 
-  const pck = JSON.parse(readFileSync(path.join(process.cwd(), "package.json"), "utf-8"));
+  // const pck = JSON.parse(readFileSync(path.join(process.cwd(), "package.json"), "utf-8"));
   const buildPath = `${process.env.PWD}/build`;
 
-  pck.scripts = {};
-  pck.private = false;
-  pck.type = "module";
-  pck.files = ["**/*", "!**/tsconfig.tsbuildinfo", "!**/*.tgz"];
+  // pck.scripts = {};
+  // pck.private = false;
+  // pck.type = "module";
+  // pck.files = ["**/*", "!**/tsconfig.tsbuildinfo", "!**/*.tgz"];
 
-  console.log(`Writing ${buildPath}/package.json`);
-  writeFileSync(`${buildPath}/package.json`, JSON.stringify(pck, null, 2));
-  copyFileSync("README.md", `${buildPath}/README.md`);
+  // console.log(`Writing ${buildPath}/package.json`);
+  // writeFileSync(`${buildPath}/package.json`, JSON.stringify(pck, null, 2));
+  // copyFileSync("README.md", `${buildPath}/README.md`);
 
-  console.log(`Copy ${buildPath}/README.md`);
+  // console.log(`Copy ${buildPath}/README.md`);
 
   // Copy empty files for CommonJS modules
   copyFileSync("./src/index.cjs", `${buildPath}/index.cjs`);
