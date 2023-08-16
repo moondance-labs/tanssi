b1ef73502f5a08e422121ccc33303d19dee21348
girazoki <gorka.irazoki@gmail.com>
2023-08-16 12:16:58 +0200

    add account32 hasher

--- container-chains/templates/frontier/runtime/src/xcm_config.rs
+++ container-chains/templates/frontier/runtime/src/xcm_config.rs
@@ -103,6 +103,8 @@ pub type LocationToAccountId = (
     SiblingParachainConvertsVia<polkadot_parachain::primitives::Sibling, AccountId>,
     // If we receive a MultiLocation of type AccountKey20, just generate a native account
     AccountKey20Aliases<RelayNetwork, AccountId>,
+    // Generate remote accounts according to polkadot standards
+	xcm_builder::HashedDescriptionDescribeFamilyAllTerminal<AccountId>,
 );
 
 /// Local origins on this chain are allowed to dispatch XCM sends/executions.
