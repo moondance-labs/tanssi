06a62f510b2c8448960bba6244fec643002b4211
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-08-21 10:38:15 +0200

    Remove container 2001 from warp sync test, and run on ubuntu-latest

--- test/suites/warp-sync/test_warp_sync.ts
+++ test/suites/warp-sync/test_warp_sync.ts
@@ -1,12 +1,9 @@
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
-import { MIN_GAS_PRICE, customWeb3Request, generateKeyringPair } from "@moonwall/util";
-import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
+import { ApiPromise, Keyring } from "@polkadot/api";
 import { u8aToHex, stringToHex } from '@polkadot/util';
 import { decodeAddress } from "@polkadot/util-crypto";
-import { Signer } from "ethers";
-import { getAuthorFromDigest, getAuthorFromDigestRange } from "../../util/author";
+import { getAuthorFromDigest } from "../../util/author";
 import { signAndSendAndInclude, waitSessions } from "../../util/block";
-import { createTransfer, waitUntilEthTxIncluded } from "../../util/ethereum";
 import { getKeyringNimbusIdHex } from "../../util/keys";
 import { getHeaderFromRelay } from "../../util/relayInterface";
 import fs from "fs/promises";
@@ -19,15 +16,11 @@ describeSuite({
     let paraApi: ApiPromise;
     let relayApi: ApiPromise;
     let container2000Api: ApiPromise;
-    let container2001Api: ApiPromise;
-    let ethersSigner: Signer;
 
     beforeAll(async () => {
       paraApi = context.polkadotJs("Tanssi");
       relayApi = context.polkadotJs("Relay");
       container2000Api = context.polkadotJs("Container2000");
-      container2001Api = context.polkadotJs("Container2001");
-      ethersSigner = context.ethers();
 
       const relayNetwork = relayApi.consts.system.version.specName.toString();
       expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
@@ -42,17 +35,10 @@ describeSuite({
       expect(container2000Network, "Container2000 API incorrect").to.contain("container-chain-template");
       expect(paraId2000, "Container2000 API incorrect").to.be.equal("2000");
 
-      const container2001Network = container2001Api.consts.system.version.specName.toString();
-      const paraId2001 = (await container2001Api.query.parachainInfo.parachainId()).toString();
-      expect(container2001Network, "Container2001 API incorrect").to.contain("frontier-template");
-      expect(paraId2001, "Container2001 API incorrect").to.be.equal("2001");
-
       // Test block numbers in relay are 0 yet
       const header2000 = await getHeaderFromRelay(relayApi, 2000);
-      const header2001 = await getHeaderFromRelay(relayApi, 2001);
 
       expect(header2000.number.toNumber()).to.be.equal(0);
-      expect(header2001.number.toNumber()).to.be.equal(0);
     }, 120000);
 
     it({
@@ -79,7 +65,6 @@ describeSuite({
           ],
           containerChains: {
             "2000": [getKeyringNimbusIdHex("Collator2000-01"), getKeyringNimbusIdHex("Collator2000-02")],
-            "2001": [getKeyringNimbusIdHex("Collator2001-01"), getKeyringNimbusIdHex("Collator2001-02")],
           },
         };
 
@@ -93,17 +78,6 @@ describeSuite({
       test: async function () {
         const blockNum = (await container2000Api.rpc.chain.getBlock()).block.header.number.toNumber();
         expect(blockNum).to.be.greaterThan(0);
-      },
-    });
-
-    it({
-      id: "T05",
-      title: "Blocks are being produced on container 2001",
-      test: async function () {
-        const blockNum = (await container2001Api.rpc.chain.getBlock()).block.header.number.toNumber();
-
-        expect(blockNum).to.be.greaterThan(0);
-        expect(await ethersSigner.provider.getBlockNumber(), "Safe tag is not present").to.be.greaterThan(0);
       },
     });
 
@@ -125,40 +99,20 @@ describeSuite({
     });
 
     it({
-      id: "T07",
-      title: "Test container chain 2001 assignation is correct",
-      test: async function () {
-        const currentSession = (await paraApi.query.session.currentIndex()).toNumber();
-        const paraId = (await container2001Api.query.parachainInfo.parachainId()).toString();
-        const containerChainCollators = (
-          await paraApi.query.authorityAssignment.collatorContainerChain(currentSession)
-        ).toJSON().containerChains[paraId];
-
-        const writtenCollators = (await container2001Api.query.authoritiesNoting.authorities()).toJSON();
-
-        expect(containerChainCollators).to.deep.equal(writtenCollators);
-      },
-    });
-
-    it({
       id: "T08",
       title: "Test author noting is correct for both containers",
       timeout: 60000,
       test: async function () {
         const assignment = await paraApi.query.collatorAssignment.collatorContainerChain();
         const paraId2000 = await container2000Api.query.parachainInfo.parachainId();
-        const paraId2001 = await container2001Api.query.parachainInfo.parachainId();
 
         // TODO: fix once we have types
         const containerChainCollators2000 = assignment.containerChains.toJSON()[paraId2000.toString()];
-        const containerChainCollators2001 = assignment.containerChains.toJSON()[paraId2001.toString()];
 
         await context.waitBlock(3, "Tanssi");
         const author2000 = await paraApi.query.authorNoting.latestAuthor(paraId2000);
-        const author2001 = await paraApi.query.authorNoting.latestAuthor(paraId2001);
 
         expect(containerChainCollators2000.includes(author2000.toJSON().author)).to.be.true;
-        expect(containerChainCollators2001.includes(author2001.toJSON().author)).to.be.true;
       },
     });
 
@@ -181,20 +135,6 @@ describeSuite({
         // TODO: fix once we have types
         const genesisData2000 = await paraApi.query.registrar.paraGenesisData(2000);
         expect(genesisData2000.toJSON().properties.isEthereum).to.be.false;
-        const genesisData2001 = await paraApi.query.registrar.paraGenesisData(2001);
-        expect(genesisData2001.toJSON().properties.isEthereum).to.be.true;
-      },
-    });
-    it({
-      id: "T11",
-      title: "Transactions can be made with ethers",
-      timeout: 30000,
-      test: async function () {
-        const randomAccount = generateKeyringPair();
-        let tx = await createTransfer(context, randomAccount.address, 1_000_000_000_000, { gasPrice: MIN_GAS_PRICE });
-        let txHash = await customWeb3Request(context.web3(), "eth_sendRawTransaction", [tx]);
-        await waitUntilEthTxIncluded(() => context.waitBlock(1, "Container2001"), context.web3(), txHash.result);
-        expect(Number(await context.web3().eth.getBalance(randomAccount.address))).to.be.greaterThan(0);
       },
     });
 
@@ -238,7 +178,6 @@ describeSuite({
           ],
           containerChains: {
             "2000": [getKeyringNimbusIdHex("Collator2000-01"), getKeyringNimbusIdHex("Collator1000-03")],
-            "2001": [getKeyringNimbusIdHex("Collator2001-01"), getKeyringNimbusIdHex("Collator2001-02")],
           },
         };
 
