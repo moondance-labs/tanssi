be2c68145b54e5b4ad6a61589c2352bafdebef3e
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2023-08-22 11:58:54 +0200

    Fix tests

--- test/suites/dev-tanssi/fees/test_fee_balance_transfer.ts
+++ test/suites/dev-tanssi/fees/test_fee_balance_transfer.ts
@@ -2,8 +2,6 @@ import "@tanssi/api-augment";
 import { describeSuite, expect, beforeAll } from "@moonwall/cli";
 import { KeyringPair, extractFee } from "@moonwall/util";
 import { ApiPromise, Keyring } from "@polkadot/api";
-import { jumpSessions } from "../../../util/block";
-import { u8aToHex, stringToHex } from "@polkadot/util";
 
 describeSuite({
   id: "D14",
@@ -16,62 +14,54 @@ describeSuite({
     let charlie: KeyringPair;
     let dave: KeyringPair;
 
-    beforeAll(() => {
+    beforeAll(async () => {
       const keyring = new Keyring({ type: "sr25519" });
       alice = keyring.addFromUri("//Alice", { name: "Alice default" });
       bob = keyring.addFromUri("//Bob", { name: "Bob default" });
       charlie = keyring.addFromUri("//Charlie", { name: "Charlie default" });
       dave = keyring.addFromUri("//Dave", { name: "Dave default" });
       polkadotJs = context.polkadotJs();
+
+      // We must create an empty block before any tests, otherwise the fee is 2630822
+      // despite the weight being the same:
+      //   inclusionFee: { baseFee: 1000000, lenFee: 144, adjustedWeightFee: 1630678 }
+      await context.createBlock([]);
     });
 
     it({
-        id: "E01",
-        title: "Fee of balances.transfer matches expected",
-        test: async function () {
-        // Dave has multiple proxy types, but none of them allows to call balances.transfer
-        const balanceBefore = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
-        const tx = polkadotJs.tx.balances.transfer(bob.address, 200_000);
+      id: "E01",
+      title: "Fee of balances.transfer can be estimated using paymentInfo",
+      test: async function () {
+      const balanceBefore = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
+      const tx = polkadotJs.tx.balances.transfer(bob.address, 200_000);
+      const info = await tx.paymentInfo(alice.address);
+      const signedTx = await tx.signAsync(alice);
+      await context.createBlock([
+        signedTx
+      ]);
 
-        // Estimate the fees as RuntimeDispatchInfo using the signer
-        const info = await tx.paymentInfo(alice.address);
-        console.log("INFO: ", info.toJSON());
-        console.log("tx len: ", tx.encodedLength);
-        const signedTx = await tx.signAsync(alice);
-        console.log("signed tx len: ", signedTx.encodedLength);
+      const events = await polkadotJs.query.system.events();
+      const fee = extractFee(events).amount.toBigInt();
+      const expectedFee = 1000144n;
+      expect(fee).to.equal(expectedFee);
 
-        await context.createBlock([
-          signedTx
-        ]);
+      const tip = 0n;
+      expect(fee).to.equal(info.partialFee.toBigInt() + tip);
 
-        const events = await polkadotJs.query.system.events();
-        const fee = extractFee(events).amount.toBigInt();
-        const expectedFee = 2630822n;
-
-        expect(fee).to.equal(expectedFee);
-
-        const balanceAfter = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
-
-        // Balance must be old balance minus fee minus transfered value
-        expect(balanceBefore - fee - 200_000n).to.equal(balanceAfter);
-        },
+      const balanceAfter = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
+      // Balance must be old balance minus fee minus transfered value
+      expect(balanceBefore - fee - 200_000n).to.equal(balanceAfter);
+      },
     });
 
     it({
       id: "E02",
-      title: "Fee of balances.transfer can be estimated in advance",
+      title: "Fee of balances.transfer can be estimated using transactionPaymentApi.queryFeeDetails",
       test: async function () {
-      // Dave has multiple proxy types, but none of them allows to call balances.transfer
       const balanceBefore = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
       const tx = polkadotJs.tx.balances.transfer(bob.address, 200_000);
-
-      // Estimate the fees as RuntimeDispatchInfo using the signer
-      const info = await tx.paymentInfo(alice.address);
-      
-      console.log("INFO: ", info.toJSON());
-      console.log("tx len: ", tx.encodedLength);
       const signedTx = await tx.signAsync(alice);
-      console.log("signed tx len: ", signedTx.encodedLength);
+      const feeDetails = await polkadotJs.call.transactionPaymentApi.queryFeeDetails(tx, signedTx.encodedLength);
 
       await context.createBlock([
         signedTx
@@ -80,47 +70,17 @@ describeSuite({
       const events = await polkadotJs.query.system.events();
       const fee = extractFee(events).amount.toBigInt();
       const expectedFee = 1000144n;
-
       expect(fee).to.equal(expectedFee);
+
+      const inclusionFee = feeDetails.inclusionFee.unwrapOrDefault();
+      const tip = 0n;
+      expect(fee).to.equal(inclusionFee.baseFee.toBigInt() + inclusionFee.lenFee.toBigInt() + inclusionFee.adjustedWeightFee.toBigInt() + tip);
 
       const balanceAfter = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
 
       // Balance must be old balance minus fee minus transfered value
       expect(balanceBefore - fee - 200_000n).to.equal(balanceAfter);
       },
-    });
-
-    it({
-      id: "E03",
-      title: "Fee of balances.transfer can be estimated in advance 2",
-      test: async function () {
-      // Dave has multiple proxy types, but none of them allows to call balances.transfer
-      const balanceBefore = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
-      const tx = polkadotJs.tx.balances.transfer(bob.address, 200_000);
-
-      // Estimate the fees as RuntimeDispatchInfo using the signer
-      const info = await tx.paymentInfo(alice.address);
-      
-      console.log("INFO: ", info.toJSON());
-      console.log("tx len: ", tx.encodedLength);
-      const signedTx = await tx.signAsync(alice);
-      console.log("signed tx len: ", signedTx.encodedLength);
-
-      await context.createBlock([
-        signedTx
-      ]);
-
-      const events = await polkadotJs.query.system.events();
-      const fee = extractFee(events).amount.toBigInt();
-      const expectedFee = 1000144n;
-
-      expect(fee).to.equal(expectedFee);
-
-      const balanceAfter = (await polkadotJs.query.system.account(alice.address)).data.free.toBigInt();
-
-      // Balance must be old balance minus fee minus transfered value
-      expect(balanceBefore - fee - 200_000n).to.equal(balanceAfter);
-      },
-    });
+  });
   },
 });
