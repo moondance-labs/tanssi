9557d1df78f4d1eaa615e7807e4dda7f14d0049c
girazoki <gorka.irazoki@gmail.com>
2023-08-24 10:28:52 +0200

    increment spec versions

--- container-chains/templates/simple/runtime/src/lib.rs
+++ container-chains/templates/simple/runtime/src/lib.rs
@@ -184,7 +184,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("container-chain-template"),
     impl_name: create_runtime_str!("container-chain-template"),
     authoring_version: 1,
-    spec_version: 100,
+    spec_version: 101,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
