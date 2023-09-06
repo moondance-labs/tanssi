66cdb8cb3e2052eb14721d60a6e2fb9233cc43f4
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2023-09-06 09:25:35 +0200

    Staking integration tests (#242)
    * basic structure + pool logic
    
    * sorted candidate list + factor out candidate from key
    
    * call to request delegate
    
    * bounded eligible candidate list
    
    * joining requests
    
    * keep track of held funds
    
    * mock and few tests
    
    * joining events + tests
    
    * multi test + wip rebalance hold
    
    * hold rebalancing works
    
    * improve tests structure
    
    * full delegation test helper
    
    * operations deal with shares
    
    * test delegating too much
    
    * undelegate call
    
    * undelegate + more events
    
    * fix edge case in mul_div
    
    * feedback
    
    * remove CollatorSet storage (sorted list is enough)
    
    * typo + enum order
    
    * mul_div tests
    
    * ensure_origin + consistency_check
    
    * consistent naming
    
    * test roundings
    
    * fix EligibleCandidate ordering
    
    * .is_zero()
    
    * leaving shares don't contribute to election
    
    * test executing operations too soon
    
    * workspace deps
    
    * use Contains trait + eligible candidate filter
    
    * fix and test manual rewards on share amount changes
    
    * test pending_rewards
    
    * renalme + test amounts in shares
    
    * fix and test rebalance_hold while undelegating
    
    * change Shares/Stake type param
    
    * fmt
    
    * more readable tests
    
    * test self-delegation
    
    * test eligible candidates list
    
    * add pallet to dancebox runtime
    
    * fix clippy
    
    * update interfaces
    
    * FMT
    
    * pooled staking
    
    * first benchmark tests
    
    * manual rewards benchmark
    
    * WIP
    
    * add staking first benchmarks, all of them working
    
    * Update docs
    
    Co-authored-by: tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
    
    * Initial joining/leaving shares should have a value of 1.
    
    Co-authored-by: tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
    
    * last benchmark
    
    * Fix integration tests
    
    * left overs and clippy
    
    * Add leaving tests
    
    * Initialize staking typescript tests
    
    * Remove unused imports
    
    * Add EligibleCandidatesFilter to pallet_session and tests
    
    * Fix private imports
    
    * generic timer for operations to not be forced to use block numbers
    
    * Use invulnerables and pallet_staking candidates as collators
    
    * skip_to_elapsed
    
    * IsCandidateEligible trait
    
    * toml_sort
    
    * remove associated types for joining/leaving shares value
    
    * More typescript tests
    
    * Update typescript-api
    
    * Commit missing files
    
    * docs
    
    * typo
    
    * update filters
    
    * Add events to ts tests
    
    * Bring benchmarks to pallet staking (#240)
    
    * add weights
    
    * fix unitests
    
    * fmt
    
    * make parametric execute_pending_operations
    
    * take into account that execute pending actions and request undelegate can incurr into more operations
    
    * update request_delegate to worst case scenario
    
    * join filter with session
    
    * updated weights
    
    * EligibleCandidate trait
    
    * weights updated
    
    * remove key if eligible is false
    
    * make candidates eligible
    
    * fix
    
    * updated weights
    
    * fix clippy
    
    * fmt
    
    * clippy happy
    
    * updated benchmarks and weights
    
    * FMT
    
    * add back filter negated
    
    * typescript api
    
    * rename + document events
    
    * fmt and pr comments
    
    * api augment
    
    * update typescript api
    
    * update rpc stagebox
    
    * remove unused imports
    
    ---------
    
    Co-authored-by: nanocryk <6422796+nanocryk@users.noreply.github.com>
    Co-authored-by: girazoki <gorka.irazoki@gmail.com>

---
+++ test/suites/dev-tanssi/staking/test_staking_session.ts
@@ -0,0 +1,87 @@
+import "@tanssi/api-augment";
+import { describeSuite, beforeAll, expect, isExtrinsicSuccessful } from "@moonwall/cli";
+import { KeyringPair, generateKeyringPair } from "@moonwall/util";
+import { ApiPromise } from "@polkadot/api";
+import { numberToHex } from "@polkadot/util";
+import { jumpToBlock } from "../../../util/block";
+
+describeSuite({
+    id: "DT3302",
+    title: "Fee test suite",
+    foundationMethods: "dev",
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+        let alice: KeyringPair;
+        // TODO: don't hardcode the period here
+        const sessionPeriod = 5;
+
+        beforeAll(async () => {
+            alice = context.keyring.alice;
+            polkadotJs = context.polkadotJs();
+        });
+
+        it({
+            id: "E01",
+            title: "It takes 2 sessions to update pallet_session collators",
+            test: async function () {
+                const initialValidators = await polkadotJs.query.session.validators();
+
+                const randomAccount = generateKeyringPair("sr25519");
+
+                const tx = polkadotJs.tx.balances.transfer(randomAccount.address, 2n * 10000000000000000n);
+                await context.createBlock([await tx.signAsync(alice)]);
+                expect(isExtrinsicSuccessful(await polkadotJs.query.system.events())).to.be.true;
+
+                // Register keys in pallet_session
+                const newKey = await polkadotJs.rpc.author.rotateKeys();
+                const tx2 = polkadotJs.tx.session.setKeys(newKey, []);
+                await context.createBlock([await tx2.signAsync(randomAccount)]);
+                expect(isExtrinsicSuccessful(await polkadotJs.query.system.events())).to.be.true;
+
+                // Self-delegate in pallet_pooled_staking
+                const tx3 = polkadotJs.tx.pooledStaking.requestDelegate(
+                    randomAccount.address,
+                    "AutoCompounding",
+                    10000000000000000n
+                );
+                await context.createBlock([await tx3.signAsync(randomAccount)]);
+                const events = await polkadotJs.query.system.events();
+                const ev1 = events.filter((a) => {
+                    return a.event.method == "IncreasedStake";
+                });
+                expect(ev1.length).to.be.equal(1);
+                const ev2 = events.filter((a) => {
+                    return a.event.method == "UpdatedCandidatePosition";
+                });
+                expect(ev2.length).to.be.equal(1);
+                const ev3 = events.filter((a) => {
+                    return a.event.method == "RequestedDelegate";
+                });
+                expect(ev3.length).to.be.equal(1);
+
+                const stakingCandidates = await polkadotJs.query.pooledStaking.sortedEligibleCandidates();
+                expect(stakingCandidates.toJSON()).to.deep.equal([
+                    {
+                        candidate: randomAccount.address,
+                        stake: numberToHex(10000000000000000, 128),
+                    },
+                ]);
+
+                // Jump to block 9
+                await jumpToBlock(context, 2 * sessionPeriod - 1);
+
+                // Now pallet_session validators should not include the new one from staking
+                const validators9 = await polkadotJs.query.session.validators();
+                expect(validators9.toJSON()).to.deep.equal(initialValidators.toJSON());
+
+                await context.createBlock();
+                // We are now in block 10 but this block cannot include any transactions, so go to 11
+                await context.createBlock();
+
+                // Block 11: candidates that joined pallet_pooled_staking in session 0 are now eligible candidates
+                const validators11 = await polkadotJs.query.session.validators();
+                expect(validators11.toJSON()).to.deep.equal([...initialValidators.toJSON(), randomAccount.address]);
+            },
+        });
+    },
+});
