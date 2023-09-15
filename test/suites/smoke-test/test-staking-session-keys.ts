516442e16df453c82bfbcdc7b412c3af585e265a
girazoki <gorka.irazoki@gmail.com>
2023-09-15 13:09:15 +0200

    remove unused varS

--- test/suites/smoke-test/test-staking-session-keys.ts
+++ test/suites/smoke-test/test-staking-session-keys.ts
@@ -22,8 +22,8 @@ describeSuite({
                     return;
                 }
                 const allEntries = await api.query.session.keyOwner.entries();
-                const accounts = allEntries.map(([_key, account]) => account.toHuman());
-                const keys = allEntries.map(([key, _account]) => key.toHuman());
+                const accounts = allEntries.map(([_, account]) => account.toHuman());
+                const keys = allEntries.map(([key, _]) => key.toHuman());
 
                 const eligibleCandidates = await api.query.pooledStaking.sortedEligibleCandidates();
 
@@ -32,7 +32,7 @@ describeSuite({
 
                     expect(index, `Candidate ${c.candidate.toHuman()} should have session keys`).not.toBe(-1);
 
-                    const allCandidateKeyTypes = keys[index].map(([keyType, _key]) => keyType.toString());
+                    const allCandidateKeyTypes = keys[index].map(([keyType, _]) => keyType.toString());
                     expect(
                         allCandidateKeyTypes.indexOf("nmbs"),
                         `Candidate ${c.candidate.toHuman()} should have nimbus keys`
