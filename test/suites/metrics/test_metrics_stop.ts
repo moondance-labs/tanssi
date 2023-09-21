bd38d9989153d932eaf6682a73fc60dfa03f7871
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2023-09-21 17:48:36 +0200

    Fix metrics server not stopping if there are open connections (#270)

---
+++ test/suites/metrics/test_metrics_stop.ts
@@ -0,0 +1,220 @@
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { ApiPromise, Keyring } from "@polkadot/api";
+import { getAuthorFromDigest } from "../../util/author";
+import { signAndSendAndInclude, waitSessions } from "../../util/block";
+import { getKeyringNimbusIdHex } from "../../util/keys";
+import { getHeaderFromRelay } from "../../util/relayInterface";
+import net from "net";
+
+describeSuite({
+    id: "ZM01",
+    title: "Zombie Tanssi Metrics Test",
+    foundationMethods: "zombie",
+    testCases: function ({ it, context }) {
+        let paraApi: ApiPromise;
+        let relayApi: ApiPromise;
+        let container2000Api: ApiPromise;
+
+        beforeAll(async () => {
+            paraApi = context.polkadotJs("Tanssi");
+            relayApi = context.polkadotJs("Relay");
+            container2000Api = context.polkadotJs("Container2000");
+
+            const relayNetwork = relayApi.consts.system.version.specName.toString();
+            expect(relayNetwork, "Relay API incorrect").to.contain("rococo");
+
+            const paraNetwork = paraApi.consts.system.version.specName.toString();
+            const paraId1000 = (await paraApi.query.parachainInfo.parachainId()).toString();
+            expect(paraNetwork, "Para API incorrect").to.contain("dancebox");
+            expect(paraId1000, "Para API incorrect").to.be.equal("1000");
+
+            const container2000Network = container2000Api.consts.system.version.specName.toString();
+            const paraId2000 = (await container2000Api.query.parachainInfo.parachainId()).toString();
+            expect(container2000Network, "Container2000 API incorrect").to.contain("container-chain-template");
+            expect(paraId2000, "Container2000 API incorrect").to.be.equal("2000");
+
+            // Test block numbers in relay are 0 yet
+            const header2000 = await getHeaderFromRelay(relayApi, 2000);
+
+            expect(header2000.number.toNumber()).to.be.equal(0);
+        }, 120000);
+
+        it({
+            id: "T01",
+            title: "Blocks are being produced on parachain",
+            test: async function () {
+                const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
+                expect(blockNum).to.be.greaterThan(0);
+            },
+        });
+
+        it({
+            id: "T03",
+            title: "Test assignation did not change",
+            test: async function () {
+                const currentSession = (await paraApi.query.session.currentIndex()).toNumber();
+                // TODO: fix once we have types
+                const allCollators = (
+                    await paraApi.query.authorityAssignment.collatorContainerChain(currentSession)
+                ).toJSON();
+                const expectedAllCollators = {
+                    orchestratorChain: [
+                        getKeyringNimbusIdHex("Collator1000-01"),
+                        getKeyringNimbusIdHex("Collator1000-02"),
+                        getKeyringNimbusIdHex("Collator1000-03"),
+                    ],
+                    containerChains: {
+                        "2000": [getKeyringNimbusIdHex("Collator2000-01"), getKeyringNimbusIdHex("Collator2000-02")],
+                    },
+                };
+
+                expect(allCollators).to.deep.equal(expectedAllCollators);
+            },
+        });
+
+        it({
+            id: "T04",
+            title: "Blocks are being produced on container 2000",
+            test: async function () {
+                const blockNum = (await container2000Api.rpc.chain.getBlock()).block.header.number.toNumber();
+                expect(blockNum).to.be.greaterThan(0);
+            },
+        });
+
+        it({
+            id: "T06",
+            title: "Test container chain 2000 assignation is correct",
+            test: async function () {
+                const currentSession = (await paraApi.query.session.currentIndex()).toNumber();
+                const paraId = (await container2000Api.query.parachainInfo.parachainId()).toString();
+                const containerChainCollators = (
+                    await paraApi.query.authorityAssignment.collatorContainerChain(currentSession)
+                ).toJSON().containerChains[paraId];
+
+                // TODO: fix once we have types
+                const writtenCollators = (await container2000Api.query.authoritiesNoting.authorities()).toJSON();
+
+                expect(containerChainCollators).to.deep.equal(writtenCollators);
+            },
+        });
+
+        it({
+            id: "T08",
+            title: "Test author noting is correct for both containers",
+            timeout: 60000,
+            test: async function () {
+                const assignment = await paraApi.query.collatorAssignment.collatorContainerChain();
+                const paraId2000 = await container2000Api.query.parachainInfo.parachainId();
+
+                // TODO: fix once we have types
+                const containerChainCollators2000 = assignment.containerChains.toJSON()[paraId2000.toString()];
+
+                await context.waitBlock(3, "Tanssi");
+                const author2000 = await paraApi.query.authorNoting.latestAuthor(paraId2000);
+
+                expect(containerChainCollators2000.includes(author2000.toJSON().author)).to.be.true;
+            },
+        });
+
+        it({
+            id: "T09",
+            title: "Test author is correct in Orchestrator",
+            test: async function () {
+                const sessionIndex = (await paraApi.query.session.currentIndex()).toNumber();
+                const authorities = await paraApi.query.authorityAssignment.collatorContainerChain(sessionIndex);
+                const author = await getAuthorFromDigest(paraApi);
+                // TODO: fix once we have types
+                expect(authorities.toJSON().orchestratorChain.includes(author.toString())).to.be.true;
+            },
+        });
+
+        it({
+            id: "T10",
+            title: "Test frontier template isEthereum",
+            test: async function () {
+                // TODO: fix once we have types
+                const genesisData2000 = await paraApi.query.registrar.paraGenesisData(2000);
+                expect(genesisData2000.toJSON().properties.isEthereum).to.be.false;
+            },
+        });
+
+        it({
+            id: "T12",
+            title: "Test metrics: deregister container chain and metrics should stop",
+            timeout: 300000,
+            test: async function () {
+                const keyring = new Keyring({ type: "sr25519" });
+                const alice = keyring.addFromUri("//Alice", { name: "Alice default" });
+
+                // Begin sending GET /metrics requests in a loop to try to prevent the server from closing
+                const connectionHandle = sendMetricsRequestLoop("127.0.0.1", 27124, 1000);
+                expect(isServerAlive(connectionHandle)).to.be.true;
+
+                const registered1 = await paraApi.query.registrar.registeredParaIds();
+                // TODO: fix once we have types
+                expect(registered1.toJSON().includes(2000)).to.be.true;
+
+                const tx = paraApi.tx.registrar.deregister(2000);
+                await signAndSendAndInclude(paraApi.tx.sudo.sudo(tx), alice);
+                await waitSessions(context, paraApi, 2);
+
+                // Check that pending para ids removes 2000
+                const registered = await paraApi.query.registrar.registeredParaIds();
+                // TODO: fix once we have types
+                expect(registered.toJSON().includes(2000)).to.be.false;
+                expect(isServerAlive(connectionHandle)).to.be.false;
+            },
+        });
+    },
+});
+
+// Send periodic "GET /metrics" requests using the same socket every time.
+// This is to reproduce a bug where the metrics server would not close if there are any open connections.
+function sendMetricsRequestLoop(hostname: string, port: number, period: number) {
+    // Use a TCP client instead of an HTTP client because I was unable to configure the HTTP client to use only
+    // one socket
+    const client = new net.Socket();
+
+    // Connect to the server
+    client.connect(port, hostname, () => {
+        console.log(`Connected to ${hostname}:${port}`);
+
+        // Define the function to send the metrics request
+        const sendMetrics = () => {
+            if (!client.destroyed) {
+                const request = "GET /metrics HTTP/1.1\r\n\r\n";
+                client.write(request);
+                console.log(`Sent request: ${request}`);
+            }
+        };
+
+        // Initially send the request
+        sendMetrics();
+
+        // Set up periodic sending of the request
+        const intervalId = setInterval(sendMetrics, period);
+
+        // Handle data received from the server
+        client.on("data", (data) => {
+            console.log(`Received data: ${data}`);
+        });
+
+        // Handle errors
+        client.on("error", (error) => {
+            console.error(`Error: ${error}`);
+        });
+
+        // Handle connection close
+        client.on("close", () => {
+            console.log("Connection closed");
+            clearInterval(intervalId);
+        });
+    });
+
+    return client;
+}
+
+// Check if the connection is still alive
+function isServerAlive(socket: net.Socket): boolean {
+    return !socket.destroyed && !socket.closed;
+}
