c26cad283bd1eea04508a609af8beb264642c4cb
girazoki <gorka.irazoki@gmail.com>
2023-09-28 19:02:08 +0200

    reuse new moonwall and for now download polkadot binary with wget

--- test/scripts/download-polkadot.sh
+++ test/scripts/download-polkadot.sh
@@ -7,7 +7,6 @@ set -e
 branch=$(egrep -o '/polkadot.*#([^\"]*)' $(dirname $0)/../../Cargo.lock | head -1 | sed 's/.*release-//#')
 polkadot_release=$(echo $branch | sed 's/#.*//' | sed 's/\/polkadot-sdk?branch=tanssi-polkadot-v//')
 
-echo $polkadot_release
 # Always run the commands from the "test" dir
 cd $(dirname $0)/..
 
@@ -17,15 +16,25 @@ if [[ -f tmp/polkadot ]]; then
 		exit 0
 	else
 		echo "Updating polkadot binary..."
-		pnpm moonwall download polkadot $polkadot_release tmp
-		pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
-		pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
-		
-		
+
+		wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v$polkadot_release/polkadot -P tmp
 		chmod +x tmp/polkadot
+
+		pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+		chmod +x tmp/polkadot-execute-worker
+
+		pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+		chmod +x tmp/polkadot-prepare-worker
+
 	fi
 else
 	echo "Polkadot binary not found, downloading..."
-	pnpm moonwall download polkadot $polkadot_release tmp
+	wget https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-v$polkadot_release/polkadot -P tmp
 	chmod +x tmp/polkadot
+
+	pnpm moonwall download polkadot-execute-worker $polkadot_release tmp
+	chmod +x tmp/polkadot-execute-worker
+
+	pnpm moonwall download polkadot-prepare-worker $polkadot_release tmp
+	chmod +x tmp/polkadot-prepare-worker
 fi
