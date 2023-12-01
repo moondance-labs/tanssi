93d32ca5cc66759ce155e46ced08947dd417a3c2
nanocryk <6422796+nanocryk@users.noreply.github.com>
2023-12-01 11:37:24 +0100

    Add dev service to simple template (#343)
    * dev service
    
    * fix tests to work with simple template
    
    * fmt
    
    * add simple template dev tests to CI
    
    * fix CI file + remove commented code
    
    * add simple template to coverage
    
    ---------
    
    Co-authored-by: girazoki <gorka.irazoki@gmail.com>

--- container-chains/templates/simple/node/src/chain_spec.rs
+++ container-chains/templates/simple/node/src/chain_spec.rs
@@ -41,7 +41,7 @@ pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Pu
 }
 
 /// Orcherstrator's parachain id
-const ORCHESTRATOR: ParaId = ParaId::new(1000);
+pub const ORCHESTRATOR: ParaId = ParaId::new(1000);
 
 /// The extensions for the [`ChainSpec`].
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize, ChainSpecGroup, ChainSpecExtension)]
--- container-chains/templates/simple/node/src/service.rs
+++ container-chains/templates/simple/node/src/service.rs
@@ -23,10 +23,19 @@ use {
     cumulus_client_consensus_common::ParachainBlockImport as TParachainBlockImport,
     cumulus_client_service::prepare_node_config,
     cumulus_primitives_core::ParaId,
+    cumulus_primitives_parachain_inherent::MockValidationDataInherentDataProvider,
+    cumulus_primitives_parachain_inherent::MockXcmConfig,
+    nimbus_primitives::NimbusId,
+    node_common::service::ManualSealConfiguration,
+    node_common::service::Sealing,
     node_common::service::{NodeBuilder, NodeBuilderConfig},
     sc_consensus::BasicQueue,
     sc_executor::NativeElseWasmExecutor,
     sc_service::{Configuration, TFullBackend, TFullClient, TaskManager},
+    sp_blockchain::HeaderBackend,
+    sp_consensus_aura::SlotDuration,
+    sp_core::Pair,
+    sp_core::H256,
     std::{sync::Arc, time::Duration},
 };
 
@@ -55,6 +64,33 @@ impl NodeBuilderConfig for NodeConfig {
     type Block = Block;
     type RuntimeApi = RuntimeApi;
     type ParachainNativeExecutor = ParachainNativeExecutor;
+}
+
+thread_local!(static TIMESTAMP: std::cell::RefCell<u64> = std::cell::RefCell::new(0));
+
+/// Provide a mock duration starting at 0 in millisecond for timestamp inherent.
+/// Each call will increment timestamp by slot_duration making Aura think time has passed.
+struct MockTimestampInherentDataProvider;
+#[async_trait::async_trait]
+impl sp_inherents::InherentDataProvider for MockTimestampInherentDataProvider {
+    async fn provide_inherent_data(
+        &self,
+        inherent_data: &mut sp_inherents::InherentData,
+    ) -> Result<(), sp_inherents::Error> {
+        TIMESTAMP.with(|x| {
+            *x.borrow_mut() += container_chain_template_simple_runtime::SLOT_DURATION;
+            inherent_data.put_data(sp_timestamp::INHERENT_IDENTIFIER, &*x.borrow())
+        })
+    }
+
+    async fn try_handle_error(
+        &self,
+        _identifier: &sp_inherents::InherentIdentifier,
+        _error: &[u8],
+    ) -> Option<Result<(), sp_inherents::Error>> {
+        // The pallet never reports error.
+        None
+    }
 }
 
 pub fn import_queue(
@@ -126,6 +162,8 @@ pub async fn start_parachain_node(
                 client: client.clone(),
                 pool: transaction_pool.clone(),
                 deny_unsafe,
+                command_sink: None,
+                xcm_senders: None,
             };
 
             crate::rpc::create_full(deps).map_err(Into::into)
@@ -144,4 +182,133 @@ pub async fn start_parachain_node(
     node_builder.network.start_network.start_network();
 
     Ok((node_builder.task_manager, node_builder.client))
+}
+
+/// Helper function to generate a crypto pair from seed
+fn get_aura_id_from_seed(seed: &str) -> NimbusId {
+    sp_core::sr25519::Pair::from_string(&format!("//{}", seed), None)
+        .expect("static values are valid; qed")
+        .public()
+        .into()
+}
+
+/// Start a node with the given parachain `Configuration` and relay chain `Configuration`.
+///
+/// This is the actual implementation that is abstract over the executor and the runtime api.
+#[sc_tracing::logging::prefix_logs_with("Parachain Dev Node")]
+pub async fn start_dev_node(
+    parachain_config: Configuration,
+    sealing: Sealing,
+    para_id: ParaId,
+    hwbench: Option<sc_sysinfo::HwBench>,
+) -> sc_service::error::Result<TaskManager> {
+    let parachain_config = prepare_node_config(parachain_config);
+
+    // Create a `NodeBuilder` which helps setup parachain nodes common systems.
+    let node_builder = NodeConfig::new_builder(&parachain_config, hwbench.clone())?;
+
+    let (parachain_block_import, import_queue) = import_queue(&parachain_config, &node_builder);
+
+    // Build a Substrate Network. (not cumulus since it is a dev node, it mocks
+    // the relaychain)
+    let mut node_builder = node_builder.build_substrate_network(&parachain_config, import_queue)?;
+
+    let mut command_sink = None;
+    let mut xcm_senders = None;
+
+    if parachain_config.role.is_authority() {
+        let client = node_builder.client.clone();
+        let (downward_xcm_sender, downward_xcm_receiver) = flume::bounded::<Vec<u8>>(100);
+        let (hrmp_xcm_sender, hrmp_xcm_receiver) = flume::bounded::<(ParaId, Vec<u8>)>(100);
+        xcm_senders = Some((downward_xcm_sender, hrmp_xcm_sender));
+
+        let authorities = vec![get_aura_id_from_seed("alice")];
+
+        command_sink = node_builder.install_manual_seal(ManualSealConfiguration {
+            block_import: parachain_block_import,
+            sealing,
+            soft_deadline: None,
+            select_chain: sc_consensus::LongestChain::new(node_builder.backend.clone()),
+            consensus_data_provider: Some(Box::new(
+                tc_consensus::ContainerManualSealAuraConsensusDataProvider::new(
+                    client.clone(),
+                    node_builder.keystore_container.keystore(),
+                    SlotDuration::from_millis(
+                        container_chain_template_simple_runtime::SLOT_DURATION,
+                    ),
+                    authorities.clone(),
+                ),
+            )),
+            create_inherent_data_providers: move |block: H256, ()| {
+                let current_para_block = client
+                    .number(block)
+                    .expect("Header lookup should succeed")
+                    .expect("Header passed in as parent should be present in backend.");
+
+                let client_for_xcm = client.clone();
+                let authorities_for_cidp = authorities.clone();
+
+                let downward_xcm_receiver = downward_xcm_receiver.clone();
+                let hrmp_xcm_receiver = hrmp_xcm_receiver.clone();
+
+                async move {
+                    let mocked_authorities_noting =
+                        ccp_authorities_noting_inherent::MockAuthoritiesNotingInherentDataProvider {
+                            current_para_block,
+                            relay_offset: 1000,
+                            relay_blocks_per_para_block: 2,
+                            orchestrator_para_id: crate::chain_spec::ORCHESTRATOR,
+                            container_para_id: para_id,
+                            authorities: authorities_for_cidp
+                    };
+
+                    let time = MockTimestampInherentDataProvider;
+                    let mocked_parachain = MockValidationDataInherentDataProvider {
+                        current_para_block,
+                        relay_offset: 1000,
+                        relay_blocks_per_para_block: 2,
+                        // TODO: Recheck
+                        para_blocks_per_relay_epoch: 10,
+                        relay_randomness_config: (),
+                        xcm_config: MockXcmConfig::new(
+                            &*client_for_xcm,
+                            block,
+                            para_id,
+                            Default::default(),
+                        ),
+                        raw_downward_messages: downward_xcm_receiver.drain().collect(),
+                        raw_horizontal_messages: hrmp_xcm_receiver.drain().collect(),
+                        additional_key_values: Some(mocked_authorities_noting.get_key_values()),
+                    };
+
+                    Ok((time, mocked_parachain, mocked_authorities_noting))
+                }
+            },
+        })?;
+    }
+
+    let rpc_builder = {
+        let client = node_builder.client.clone();
+        let transaction_pool = node_builder.transaction_pool.clone();
+
+        Box::new(move |deny_unsafe, _| {
+            let deps = crate::rpc::FullDeps {
+                client: client.clone(),
+                pool: transaction_pool.clone(),
+                deny_unsafe,
+                command_sink: command_sink.clone(),
+                xcm_senders: xcm_senders.clone(),
+            };
+
+            crate::rpc::create_full(deps).map_err(Into::into)
+        })
+    };
+
+    let node_builder = node_builder.spawn_common_tasks(parachain_config, rpc_builder)?;
+
+    log::info!("Development Service Ready");
+
+    node_builder.network.start_network.start_network();
+
+    Ok(node_builder.task_manager)
 }
