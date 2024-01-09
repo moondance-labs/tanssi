34cd4db373cae547e062a68e38949e1e2901c2f4
Agusrodri <agusrodriguez2456@gmail.com>
2024-01-09 06:52:39 -0800

    remove dummy comment

--- test/suites/dev-frontier-template/test-balance/test-balance-existential.ts
+++ test/suites/dev-frontier-template/test-balance/test-balance-existential.ts
@@ -2,7 +2,7 @@ import { TransactionTypes, beforeEach, describeSuite, expect } from "@moonwall/c
 import { ALITH_ADDRESS, BALTATHAR_ADDRESS, MIN_GAS_PRICE, createRawTransfer } from "@moonwall/util";
 import { PrivateKeyAccount } from "viem";
 import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
-//
+
 describeSuite({
     id: "DF0101",
     title: "Existential Deposit disabled",
