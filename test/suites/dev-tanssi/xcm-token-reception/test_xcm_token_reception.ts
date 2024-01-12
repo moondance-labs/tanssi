8612f130ee31cfdce7ed5e82a2e91f947838d681
girazoki <gorka.irazoki@gmail.com>
2024-01-12 11:34:46 +0100

    first test proving reception

---
+++ test/suites/dev-tanssi/xcm-token-reception/test_xcm_token_reception.ts
@@ -0,0 +1,114 @@
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { KeyringPair, alith } from "@moonwall/util";
+import { generateKeyringPair } from "@moonwall/util";
+import { ApiPromise, Keyring } from "@polkadot/api";
+import { BN, hexToU8a, stringToU8a, u8aToHex } from "@polkadot/util";
+
+import {
+    RawXcmMessage,
+    XcmFragment,
+    descendParentOriginForAddress20,
+    descendParentOriginFromAddress32,
+    injectDmpMessageAndSeal,
+} from "../../../util/xcm.ts";
+import {
+    RELAY_SOURCE_LOCATION
+} from "../../../util/constants.ts";
+
+describeSuite({
+    id: "TX0101",
+    title: "Mock XCM - Succeeds receiving tokens",
+    foundationMethods: "dev",
+    testCases: ({ context, it }) => {
+        let polkadotJs: ApiPromise;
+        let transferredBalance;
+        let sendingAddress;
+        let alice: KeyringPair;
+        let chain;
+
+        beforeAll(async function () {
+            polkadotJs = context.polkadotJs();
+            chain = polkadotJs.consts.system.version.specName.toString();
+            // since in the future is likely that we are going to add this to containers, I leave it here
+            alice =
+                chain == "frontier-template"
+                    ? alith
+                    : new Keyring({ type: "sr25519" }).addFromUri("//Alice", {
+                          name: "Alice default",
+                      });
+
+            transferredBalance = 10_000_000_000_000n;
+
+            // We register the token
+            const txSigned = polkadotJs.tx.sudo.sudo(
+                polkadotJs.tx.utility.batch([
+                    polkadotJs.tx.foreignAssetsCreator.createForeignAsset(
+                        RELAY_SOURCE_LOCATION,
+                        1,
+                        alice.address,
+                        true,
+                        1
+                    ),
+                    polkadotJs.tx.assetRate.create(
+                        1,
+                        // this defines how much the asset costs with respect to the 
+                        // new asset
+                        // in this case, asset*2=native
+                        // that means that we will charge 0.5 of the native balance
+                        2000000000000000000n
+                    )]
+                ));
+
+            await context.createBlock(await txSigned.signAsync(alice), {
+                allowFailures: false,
+            });
+        });
+
+        it({
+            id: "T01",
+            title: "Should succeed receiving tokens",
+            test: async function () {
+                // Send an XCM and create block to execute it
+                // By default we send tokens from the relay
+                const xcmMessage = new XcmFragment({
+                    assets: [
+                      {
+                        multilocation: {
+                          parents: 1,
+                          interior: { Here: null },
+                        },
+                        fungible: 10000000000000n,
+                      },
+                    ],
+                    weight_limit: {
+                      refTime: 4000000000n,
+                      proofSize: 80000n,
+                    } as any,
+                    beneficiary: u8aToHex(alice.addressRaw),
+                  })
+                    .reserve_asset_deposited()
+                    .clear_origin()
+                    .buy_execution()
+                    .deposit_asset()
+                    .as_v3();
+                
+                // Send an XCM and create block to execute it
+                await injectDmpMessageAndSeal(context, {
+                    type: "XcmVersionedXcm",
+                    payload: xcmMessage,
+                } as RawXcmMessage);
+
+                // Create a block in which the XCM will be executed
+                await context.createBlock();
+
+                // Make sure the state has ALITH's to DOT tokens
+                const alice_dot_balance = (
+                    await context.polkadotJs().query.foreignAssets.account(1, alice.address)
+                )
+                .unwrap()
+                .balance.toBigInt();
+                expect(alice_dot_balance > 0n).to.be.true;
+            },
+        });
+    },
+});
