c9f66d21b4899e4e336fe25497ec05e5cca6c401
girazoki <gorka.irazoki@gmail.com>
2024-01-30 08:56:04 +0100

    clippy

--- runtime/flashbox/src/lib.rs
+++ runtime/flashbox/src/lib.rs
@@ -76,7 +76,7 @@ use {
     sp_core::{crypto::KeyTypeId, Decode, Encode, Get, MaxEncodedLen, OpaqueMetadata},
     sp_runtime::{
         create_runtime_str, generic, impl_opaque_keys,
-        traits::{AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT, Zero},
+        traits::{AccountIdConversion, AccountIdLookup, BlakeTwo256, Block as BlockT},
         transaction_validity::{TransactionSource, TransactionValidity},
         AccountId32, ApplyExtrinsicResult,
     },
@@ -582,11 +582,13 @@ impl RemoveParaIdsWithNoCredits for RemoveParaIdsWithNoCreditsImpl {
             let free_credits = pallet_services_payment::BlockProductionCredits::<Runtime>::get(para_id)
                 .unwrap_or_default();
 
-            let remaining_credits = credits_for_2_sessions.saturating_sub(free_credits);
             // Return if we can survive with free credits
-            if remaining_credits.is_zero() {
+            if free_credits >= credits_for_2_sessions {
                 return true
             }
+
+            let remaining_credits = credits_for_2_sessions.saturating_sub(free_credits);
+
             let (block_production_costs, _) = <Runtime as pallet_services_payment::Config>::ProvideBlockProductionCost::block_cost(para_id);
             // let's check if we can withdraw
             let remaining_to_pay = (remaining_credits as u128).saturating_mul(block_production_costs);
