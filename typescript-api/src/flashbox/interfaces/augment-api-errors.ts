a77f1e5c44115a0e60c49c88b7f2ace185412651
augustocollado <56117494+augustocollado@users.noreply.github.com>
2024-02-07 08:14:39 -0300

    MD 306 - add pallet treasury (#400)
    * pallet treasury added & compiling
    
    * 20% of Fees & Tips go to treasury - working
    
    * Test fees and tips and reserves on proposal creation
    
    * slash on rejection
    
    * Approval tests
    
    * Added pallet to flashbox
    
    * prettier
    
    * even prettier
    
    * lint
    
    * missing references in flashbox runtime
    
    * pallet added to flashbox
    
    * api augment
    
    * Transaction payment - added missing fees policy
    
    * fmt
    
    * clippy
    
    * undo fmt
    
    * assignment.rs - doesn't pass fmt check as it is in master
    
    * api augment
    
    * TODO: enable different assets in the treasury
    
    * Added treasury to the benchmarks list

--- typescript-api/src/flashbox/interfaces/augment-api-errors.ts
+++ typescript-api/src/flashbox/interfaces/augment-api-errors.ts
@@ -250,6 +250,34 @@ declare module "@polkadot/api-base/types/errors" {
             /** Generic error */
             [key: string]: AugmentedError<ApiType>;
         };
+        treasury: {
+            /** The payment has already been attempted. */
+            AlreadyAttempted: AugmentedError<ApiType>;
+            /** The spend is not yet eligible for payout. */
+            EarlyPayout: AugmentedError<ApiType>;
+            /** The balance of the asset kind is not convertible to the balance of the native asset. */
+            FailedToConvertBalance: AugmentedError<ApiType>;
+            /** The payment has neither failed nor succeeded yet. */
+            Inconclusive: AugmentedError<ApiType>;
+            /** The spend origin is valid but the amount it is allowed to spend is lower than the amount to be spent. */
+            InsufficientPermission: AugmentedError<ApiType>;
+            /** Proposer's balance is too low. */
+            InsufficientProposersBalance: AugmentedError<ApiType>;
+            /** No proposal, bounty or spend at that index. */
+            InvalidIndex: AugmentedError<ApiType>;
+            /** The payout was not yet attempted/claimed. */
+            NotAttempted: AugmentedError<ApiType>;
+            /** There was some issue with the mechanism of payment. */
+            PayoutError: AugmentedError<ApiType>;
+            /** Proposal has not been approved. */
+            ProposalNotApproved: AugmentedError<ApiType>;
+            /** The spend has expired and cannot be claimed. */
+            SpendExpired: AugmentedError<ApiType>;
+            /** Too many approvals in the queue. */
+            TooManyApprovals: AugmentedError<ApiType>;
+            /** Generic error */
+            [key: string]: AugmentedError<ApiType>;
+        };
         txPause: {
             /** The call is paused. */
             IsPaused: AugmentedError<ApiType>;
