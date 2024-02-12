cf2207d83fd2530b9c8d8a75cf5f823a5a412429
girazoki <gorka.irazoki@gmail.com>
2024-02-12 10:24:20 +0100

    fmt

--- primitives/traits/src/lib.rs
+++ primitives/traits/src/lib.rs
@@ -31,18 +31,6 @@ use {
     sp_std::vec::Vec,
 };
 
-/// The author-noting hook to react to container chains authoring.
-pub trait AuthorNotingHook<AccountId> {
-    /// This hook is called partway through the `set_latest_author_data` inherent in author-noting.
-    ///
-    /// The hook should never panic and is required to return the weight consumed.
-    fn on_container_author_noted(
-        author: &AccountId,
-        block_number: BlockNumber,
-        para_id: ParaId,
-    ) -> Weight;
-}
-
 /// The collator-assignment hook to react to collators beign assigned to container chains.
 pub trait CollatorAssignmentHook {
     /// This hook is called when collators are assigned to a container
@@ -58,6 +46,18 @@ impl CollatorAssignmentHook for Tuple {
         for_tuples!( #( weight.saturating_accrue(Tuple::on_collators_assigned(p)); )* );
         weight
     }
+}
+
+/// The author-noting hook to react to container chains authoring.
+pub trait AuthorNotingHook<AccountId> {
+    /// This hook is called partway through the `set_latest_author_data` inherent in author-noting.
+    ///
+    /// The hook should never panic and is required to return the weight consumed.
+    fn on_container_author_noted(
+        author: &AccountId,
+        block_number: BlockNumber,
+        para_id: ParaId,
+    ) -> Weight;
 }
 
 #[impl_trait_for_tuples::impl_for_tuples(5)]
