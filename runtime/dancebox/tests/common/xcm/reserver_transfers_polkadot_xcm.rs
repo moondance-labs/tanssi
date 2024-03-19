43354ec96feabeb03b9c948f92d10b2a62f5f563
girazoki <gorka.irazoki@gmail.com>
2024-03-19 12:33:52 +0100

    failing test

--- runtime/dancebox/tests/common/xcm/reserver_transfers_polkadot_xcm.rs
+++ runtime/dancebox/tests/common/xcm/reserver_transfers_polkadot_xcm.rs
@@ -589,3 +589,230 @@ fn transfer_assets_container_token_tanssi() {
         );
     });
 }
+
+#[allow(unused_assignments)]
+#[test]
+fn transfer_asset_relay_token_across_tanssi_container() {
+    // XcmPallet reserve transfer arguments
+    let alice_dancebox_origin = <Dancebox as Chain>::RuntimeOrigin::signed(DanceboxSender::get());
+    let alice_relay_origin = <Westend as Chain>::RuntimeOrigin::signed(WestendSender::get());
+
+    // Parents 1 this time
+    let simple_template_dest: VersionedMultiLocation = MultiLocation {
+        parents: 1,
+        interior: X1(Parachain(2002u32)),
+    }
+    .into();
+
+    let simple_template_beneficiary: VersionedMultiLocation = MultiLocation {
+        parents: 0,
+        interior: X1(AccountId32 {
+            network: None,
+            id: SimpleTemplateEmptyReceiver::get().into(),
+        }),
+    }
+    .into();
+
+    let dancebox_dest: VersionedMultiLocation = MultiLocation {
+        parents: 0,
+        interior: X1(Parachain(2000u32)),
+    }
+    .into();
+
+    let dancebox_beneficiary: VersionedMultiLocation = MultiLocation {
+        parents: 0,
+        interior: X1(AccountId32 {
+            network: None,
+            id: DanceboxSender::get().into(),
+        }),
+    }
+    .into();
+
+    let relay_amount_to_send: crate::Balance = westend_runtime::ExistentialDeposit::get() * 1000;
+
+    let relay_assets: MultiAssets = (Here, relay_amount_to_send).into();
+
+    let fee_asset_item = 0;
+    let westend_token_asset_id = 1u16;
+
+    // Register the relay asset first
+    SimpleTemplate::execute_with(|| {
+        let root_origin = <SimpleTemplate as Chain>::RuntimeOrigin::root();
+
+        assert_ok!(
+            <SimpleTemplate as SimpleTemplateParaPallet>::ForeignAssetsCreator::create_foreign_asset(
+                root_origin.clone(),
+                MultiLocation::parent(),
+                westend_token_asset_id,
+                SimpleTemplateEmptyReceiver::get(),
+                true,
+                1
+            )
+        );
+
+        assert_ok!(
+            <SimpleTemplate as SimpleTemplateParaPallet>::AssetRate::create(
+                root_origin,
+                bx!(westend_token_asset_id),
+                FixedU128::from_u32(1)
+            )
+        );
+    });
+
+    // Register the relay asset first
+    Dancebox::execute_with(|| {
+        let root_origin = <Dancebox as Chain>::RuntimeOrigin::root();
+
+        assert_ok!(
+            <Dancebox as DanceboxParaPallet>::ForeignAssetsCreator::create_foreign_asset(
+                root_origin.clone(),
+                MultiLocation::parent(),
+                westend_token_asset_id,
+                DanceboxSender::get(),
+                true,
+                1
+            )
+        );
+
+        assert_ok!(<Dancebox as DanceboxParaPallet>::AssetRate::create(
+            root_origin,
+            bx!(westend_token_asset_id),
+            FixedU128::from_u32(1)
+        ));
+    });
+
+    // Relay sends to dancebox first
+    Westend::execute_with(|| {
+        assert_ok!(
+            <Westend as WestendRelayPallet>::XcmPallet::limited_reserve_transfer_assets(
+                alice_relay_origin,
+                bx!(dancebox_dest),
+                bx!(dancebox_beneficiary),
+                bx!(relay_assets.into()),
+                fee_asset_item,
+                WeightLimit::Unlimited,
+            )
+        );
+    });
+
+    // We should have received the tokens
+    let mut native_balance = 0u128;
+    Dancebox::execute_with(|| {
+        type RuntimeEvent = <Dancebox as Chain>::RuntimeEvent;
+        let mut outcome_weight = Weight::default();
+        assert_expected_events!(
+            Dancebox,
+            vec![
+                RuntimeEvent::MessageQueue(
+                    pallet_message_queue::Event::Processed {
+                        success: true,
+                        weight_used,
+                        ..
+                    }) => {
+                        weight_used: {
+                            outcome_weight = *weight_used;
+                            weight_used.all_gte(Weight::from_parts(0,0))
+                        },
+                    },
+            ]
+        );
+        type ForeignAssets = <Dancebox as DanceboxParaPallet>::ForeignAssets;
+
+        // We should have charged an amount of tokens that is identical to the weight spent
+        native_balance = dancebox_runtime::WeightToFee::weight_to_fee(&outcome_weight);
+
+        // Assert empty receiver received funds
+        assert_eq!(
+            <ForeignAssets as frame_support::traits::fungibles::Inspect<_>>::balance(
+                westend_token_asset_id,
+                &DanceboxSender::get(),
+            ),
+            relay_amount_to_send - native_balance
+        );
+    });
+
+    let relay_tokens_to_send_simple_template = (relay_amount_to_send - native_balance) / 2;
+    // We just send half of the DOT received
+    let relay_assets_to_send: MultiAssets = vec![(
+        MultiLocation::parent(),
+        relay_tokens_to_send_simple_template,
+    )
+        .into()]
+    .into();
+
+    // Now we need to send the relay asset to simple template
+    // Send XCM message from Dancebox
+    // Let's try to use dot as the fee
+    // This should work as we are trying to send a single asset
+    Dancebox::execute_with(|| {
+        assert_ok!(
+            <Dancebox as DanceboxParaPallet>::PolkadotXcm::transfer_assets(
+                alice_dancebox_origin.clone(),
+                bx!(simple_template_dest.clone()),
+                bx!(simple_template_beneficiary.clone()),
+                bx!(relay_assets_to_send.into()),
+                0,
+                WeightLimit::Unlimited,
+            )
+        );
+    });
+
+    // Relay execute with to create a block
+    Westend::execute_with(|| {
+        type RuntimeEvent = <Westend as Chain>::RuntimeEvent;
+        let mut outcome_weight = Weight::default();
+        assert_expected_events!(
+            Westend,
+            vec![
+                RuntimeEvent::MessageQueue(
+                    pallet_message_queue::Event::Processed {
+                        success: true,
+                        weight_used,
+                        ..
+                    }) => {
+                        weight_used: {
+                            outcome_weight = *weight_used;
+                            weight_used.all_gte(Weight::from_parts(0,0))
+                        },
+                    },
+            ]
+        );
+
+    });
+
+    // Let's assert we received them
+    SimpleTemplate::execute_with(|| {
+        type RuntimeEvent = <SimpleTemplate as Chain>::RuntimeEvent;
+        let mut outcome_weight = Weight::default();
+        assert_expected_events!(
+            SimpleTemplate,
+            vec![
+                RuntimeEvent::MessageQueue(
+                    pallet_message_queue::Event::Processed {
+                        success: true,
+                        weight_used,
+                        ..
+                    }) => {
+                        weight_used: {
+                            outcome_weight = *weight_used;
+                            weight_used.all_gte(Weight::from_parts(0,0))
+                        },
+                    },
+            ]
+        );
+        type ForeignAssets = <SimpleTemplate as SimpleTemplateParaPallet>::ForeignAssets;
+
+        // We should have charged an amount of tokens that is identical to the weight spent
+        let charged_tokens =
+            container_chain_template_simple_runtime::WeightToFee::weight_to_fee(&outcome_weight);
+
+        // Assert empty receiver received funds for relay asset
+        assert_eq!(
+            <ForeignAssets as frame_support::traits::fungibles::Inspect<_>>::balance(
+                westend_token_asset_id,
+                &SimpleTemplateEmptyReceiver::get(),
+            ),
+            relay_tokens_to_send_simple_template - charged_tokens
+        );
+    });
+}
