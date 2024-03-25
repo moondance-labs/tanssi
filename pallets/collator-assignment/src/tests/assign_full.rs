893d07495d5a6937dde23341356aaba5cf540545
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-03-25 13:52:07 +0100

    Shuffle only top N collators to respect staking priority

--- pallets/collator-assignment/src/tests/assign_full.rs
+++ pallets/collator-assignment/src/tests/assign_full.rs
@@ -19,8 +19,20 @@ use {
         assignment::{Assignment, AssignmentError},
         tests::Test,
     },
+    rand::{seq::SliceRandom, SeedableRng},
+    rand_chacha::ChaCha20Rng,
     sp_std::collections::btree_map::BTreeMap,
 };
+
+fn no_shuffle() -> Option<impl FnOnce(&mut Vec<u64>)> {
+    let mut shuffle = Some(move |collators: &mut Vec<u64>| {
+        // Create an empty closure to allow the compiler to infer the return type
+    });
+    // But don't return that closure, instead return `None`
+    shuffle = None;
+
+    shuffle
+}
 
 #[test]
 fn assign_full_old_assigned_priority() {
@@ -30,7 +42,8 @@ fn assign_full_old_assigned_priority() {
     let old_assigned = BTreeMap::from_iter(vec![(1000.into(), vec![3, 4])]);
 
     let new_assigned =
-        Assignment::<Test>::assign_full(collators, container_chains, old_assigned).unwrap();
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle())
+            .unwrap();
     let expected = BTreeMap::from_iter(vec![(1000.into(), vec![3, 4, 1, 2, 5])]);
     assert_eq!(new_assigned, expected);
 }
@@ -43,7 +56,8 @@ fn assign_full_invalid_old_assigned_collators_removed() {
     let old_assigned = BTreeMap::from_iter(vec![(1000.into(), vec![20, 21])]);
 
     let new_assigned =
-        Assignment::<Test>::assign_full(collators, container_chains, old_assigned).unwrap();
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle())
+            .unwrap();
     let expected = BTreeMap::from_iter(vec![(1000.into(), vec![1, 2, 3, 4, 5])]);
     assert_eq!(new_assigned, expected);
 }
@@ -56,7 +70,8 @@ fn assign_full_invalid_chains_removed() {
     let old_assigned = BTreeMap::from_iter(vec![(1001.into(), vec![1, 2, 3, 4, 5])]);
 
     let new_assigned =
-        Assignment::<Test>::assign_full(collators, container_chains, old_assigned).unwrap();
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle())
+            .unwrap();
     let expected = BTreeMap::from_iter(vec![(1000.into(), vec![1, 2, 3, 4, 5])]);
     assert_eq!(new_assigned, expected);
 }
@@ -72,7 +87,8 @@ fn assign_full_truncates_collators() {
     ]);
 
     let new_assigned =
-        Assignment::<Test>::assign_full(collators, container_chains, old_assigned).unwrap();
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle())
+            .unwrap();
     let expected = BTreeMap::from_iter(vec![(1000.into(), vec![1, 2]), (2000.into(), vec![6, 7])]);
     assert_eq!(new_assigned, expected);
 }
@@ -84,9 +100,49 @@ fn assign_full_old_assigned_error_if_not_enough_collators() {
     let collators = vec![1, 2];
     let container_chains = vec![(1000.into(), 2), (2000.into(), 2)];
     let old_assigned = BTreeMap::from_iter(vec![(2000.into(), vec![1, 2])]);
-    let new_assigned = Assignment::<Test>::assign_full(collators, container_chains, old_assigned);
+    let new_assigned =
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle());
     assert_eq!(
         new_assigned.unwrap_err(),
         AssignmentError::NotEnoughCollators
     );
+}
+
+#[test]
+fn assign_full_list_priority() {
+    // The order in the collators list is priority
+    let collators = vec![
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+    let container_chains = vec![(1000.into(), 2), (2000.into(), 2)];
+    let old_assigned = BTreeMap::from_iter(vec![]);
+
+    let new_assigned =
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, no_shuffle())
+            .unwrap();
+    let expected = BTreeMap::from_iter(vec![(1000.into(), vec![1, 2]), (2000.into(), vec![3, 4])]);
+    assert_eq!(new_assigned, expected);
+}
+
+#[test]
+fn assign_full_list_priority_shuffle() {
+    // The order in the collators list is priority
+    let collators = vec![
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+    ];
+    let container_chains = vec![(1000.into(), 2), (2000.into(), 2)];
+    let old_assigned = BTreeMap::from_iter(vec![]);
+    let shuffle = Some(move |collators: &mut Vec<u64>| {
+        // Shuffle with a fixed seed, we do not need randomness in a unit test
+        let seed = [1; 32];
+        let mut rng: ChaCha20Rng = SeedableRng::from_seed(seed);
+        collators.shuffle(&mut rng);
+    });
+
+    let new_assigned =
+        Assignment::<Test>::assign_full(collators, container_chains, old_assigned, shuffle)
+            .unwrap();
+    // Expect only [1, 2, 3, 4] to be assigned, in random order
+    let expected = BTreeMap::from_iter(vec![(1000.into(), vec![3, 2]), (2000.into(), vec![1, 4])]);
+    assert_eq!(new_assigned, expected);
 }
