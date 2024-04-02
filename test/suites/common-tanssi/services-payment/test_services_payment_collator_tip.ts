66fab4504e9030a6018f4143caffd6afc5c2ddab
Francisco Gamundi <francisco@moonsonglabs.com>
2024-04-02 18:17:01 +0200

    Lint

--- test/suites/common-tanssi/services-payment/test_services_payment_collator_tip.ts
+++ test/suites/common-tanssi/services-payment/test_services_payment_collator_tip.ts
@@ -1,9 +1,8 @@
 import "@tanssi/api-augment";
 import { describeSuite, expect, beforeAll } from "@moonwall/cli";
 import { ApiPromise } from "@polkadot/api";
-import { generateKeyringPair, KeyringPair } from "@moonwall/util";
+import { KeyringPair } from "@moonwall/util";
 import { fetchCollatorAssignmentTip, jumpSessions } from "util/block";
-import { paraIdTank } from "util/payment";
 
 describeSuite({
     id: "CT0608",
@@ -12,7 +11,6 @@ describeSuite({
     testCases: ({ it, context }) => {
         let polkadotJs: ApiPromise;
         let alice: KeyringPair;
-        const startingCredits = 100n;
 
         beforeAll(async () => {
             polkadotJs = context.polkadotJs();
@@ -26,12 +24,12 @@ describeSuite({
 
                 const paraId = 2001n;
 
-                let tip = 123;
+                const tip = 123;
 
                 const tx = polkadotJs.tx.servicesPayment.purchaseCredits(paraId, 1_000_000_000_000_000);
                 await context.createBlock([await tx.signAsync(alice)]);
 
-                let txMaxTip = polkadotJs.tx.servicesPayment.setMaxTip(paraId, tip);
+                const txMaxTip = polkadotJs.tx.servicesPayment.setMaxTip(paraId, tip);
                 await context.createBlock([await polkadotJs.tx.sudo.sudo(txMaxTip).signAsync(alice)]);
                 await jumpSessions(context, 2);
 
