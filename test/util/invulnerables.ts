df35ff693d5d7053b87829281a194f18a7a99666
augustocollado <augustocollado@gmail.com>
2024-04-16 11:10:42 -0300

    Fixes map syntax

--- test/util/invulnerables.ts
+++ test/util/invulnerables.ts
@@ -5,13 +5,12 @@ export async function createBlockAndRemoveInvulnerables(context: DevModeContext,
     let nonce = (await context.polkadotJs().rpc.system.accountNextIndex(sudoKey.address)).toNumber();
     const invulnerables = await context.polkadotJs().query.invulnerables.invulnerables();
 
-    const txs = invulnerables.map((invulnerable) => {
-            context
-                .polkadotJs()
-                .tx.sudo.sudo(context.polkadotJs().tx.invulnerables.removeInvulnerable(invulnerable))
-                .signAsync(sudoKey, { nonce: nonce++ })
-        );
-    });
+    const txs = invulnerables.map((invulnerable) =>
+        context
+            .polkadotJs()
+            .tx.sudo.sudo(context.polkadotJs().tx.invulnerables.removeInvulnerable(invulnerable))
+            .signAsync(sudoKey, { nonce: nonce++ })
+    );
 
     await context.createBlock(txs);
 }
