c72bd61ff47e922a432d519a4952214fe61e646a
tmpolaczyk <44604217+tmpolaczyk@users.noreply.github.com>
2024-04-17 16:31:25 +0200

    Store tanssi slot number in author noting pallet (#500)

--- runtime/flashbox/tests/integration_test.rs
+++ runtime/flashbox/tests/integration_test.rs
@@ -1794,7 +1794,7 @@ fn test_author_noting_not_self_para() {
                 Some(ContainerChainBlockInfo {
                     block_number: 1,
                     author: AccountId::from(DAVE),
-                    latest_slot_number: 5.into(),
+                    latest_slot_number: 0.into(),
                 })
             );
         });
@@ -1943,7 +1943,7 @@ fn test_author_noting_runtime_api() {
                 Some(ContainerChainBlockInfo {
                     block_number: 1,
                     author: AccountId::from(DAVE),
-                    latest_slot_number: 5.into(),
+                    latest_slot_number: 0.into(),
                 })
             );
 
