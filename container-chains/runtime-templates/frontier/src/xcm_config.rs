5545e9e37e5f40f862f913894f173afca70eba7e
girazoki <gorka.irazoki@gmail.com>
2024-04-25 17:17:58 +0200

    forbid teleports and fix command in templates (#530)
    * forbid teleports and fix command
    
    * fix white spece
    
    * tests
    
    * test token reception

--- container-chains/runtime-templates/frontier/src/xcm_config.rs
+++ container-chains/runtime-templates/frontier/src/xcm_config.rs
@@ -447,13 +447,14 @@ impl pallet_asset_rate::Config for Runtime {
 parameter_types! {
     pub const TrustPolicyMaxAssets: u32 = 1000;
     pub const AllNativeTrustPolicy: DefaultTrustPolicy = DefaultTrustPolicy::AllNative;
+    pub const AllNeverTrustPolicy: DefaultTrustPolicy = DefaultTrustPolicy::Never;
 }
 impl pallet_xcm_executor_utils::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type TrustPolicyMaxAssets = TrustPolicyMaxAssets;
     type ReserveDefaultTrustPolicy = AllNativeTrustPolicy;
     type SetReserveTrustOrigin = EnsureRoot<AccountId>;
-    type TeleportDefaultTrustPolicy = AllNativeTrustPolicy;
+    type TeleportDefaultTrustPolicy = AllNeverTrustPolicy;
     type SetTeleportTrustOrigin = EnsureRoot<AccountId>;
     type WeightInfo = weights::pallet_xcm_executor_utils::SubstrateWeight<Runtime>;
 }
