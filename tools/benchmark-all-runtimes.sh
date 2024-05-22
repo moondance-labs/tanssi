ea19a8b98be4b09e0499d1b61d1855de9de08c9c
Francisco Gamundi <52399794+fgamundi@users.noreply.github.com>
2024-05-22 18:04:09 +0200

    Script benchmarking all runtimes (#557)
    * Script benchmarking all runtimes
    
    * *
    
    * Review and rename
    
    * chmod

---
+++ tools/benchmark-all-runtimes.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Exit on any error
+set -e
+
+# Always run the commands from the project dir
+cd "$(dirname "$0")/.."
+
+# mkdir just in case as the benchmarking fails if they don't exist
+mkdir -p tmp/dancebox_weights tmp/flashbox_weights tmp/simple_template_weights tmp/frontier_template_weights
+
+# Empty directories
+rm -rf tmp/*_weights/*
+
+# Dancebox weights
+TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
+    OUTPUT_PATH=tmp/dancebox_weights \
+    tools/benchmarking.sh "*" "*"
+
+# Flashbox weights
+TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
+    CHAIN=flashbox_dev \
+    OUTPUT_PATH=tmp/flashbox_weights \
+    tools/benchmarking.sh "*" "*"
+
+# Simple template weights
+BINARY=target/release/container-chain-template-simple-node \
+    TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
+    OUTPUT_PATH=tmp/simple_template_weights \
+    tools/benchmarking.sh "*" "*"
+
+# Frontier template weights
+BINARY=target/release/container-chain-template-frontier-node \
+    TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
+    OUTPUT_PATH=tmp/frontier_template_weights \
+    tools/benchmarking.sh "*" "*"
