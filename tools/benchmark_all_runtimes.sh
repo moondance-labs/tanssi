fca6804cda7444655db2c2b6bd145ed29e675921
Francisco Gamundi <francisco@moonsonglabs.com>
2024-05-22 11:54:53 +0200

    *

--- tools/benchmark_all_runtimes.sh
+++ tools/benchmark_all_runtimes.sh
@@ -9,22 +9,22 @@ rm -rf tmp/*_weights/*
 # Dancebox weights
 TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
     OUTPUT_PATH=tmp/dancebox_weights \
-    tools/benchmarking.sh "frame_system" "remark"
+    tools/benchmarking.sh "*" "*"
 
 # Flashbox weights
 TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
     CHAIN=flashbox_dev \
     OUTPUT_PATH=tmp/flashbox_weights \
-    tools/benchmarking.sh "frame_system" "remark"
+    tools/benchmarking.sh "*" "*"
 
 # Simple template weights
 BINARY=target/release/container-chain-template-simple-node \
     TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
     OUTPUT_PATH=tmp/simple_template_weights \
-    tools/benchmarking.sh "frame_system" "remark"
+    tools/benchmarking.sh "*" "*"
 
 # Frontier template weights
 BINARY=target/release/container-chain-template-frontier-node \
     TEMPLATE_PATH=benchmarking/frame-weight-runtime-template.hbs \
     OUTPUT_PATH=tmp/frontier_template_weights \
-    tools/benchmarking.sh "frame_system" "remark"
+    tools/benchmarking.sh "*" "*"
