a70372387dfd68b32e6c486bbe444f69abc1580b
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-06-14 15:40:26 +0200

    Add runtime-api to typescript-api

--- test/suites/common-xcm/xcm/test-xcm-payment-api.ts
+++ test/suites/common-xcm/xcm/test-xcm-payment-api.ts
@@ -9,63 +9,6 @@ import {
 } from "../../../util/xcm";
 import { ApiPromise, Keyring, WsProvider } from "@polkadot/api";
 
-const runtimeApi = {
-    runtime: {
-        XcmPaymentApi: [
-            {
-                methods: {
-                    query_acceptable_payment_assets: {
-                        description: "The API to query acceptable payment assets",
-                        params: [
-                            {
-                                name: "version",
-                                type: "u32",
-                            },
-                        ],
-                        type: "Result<Vec<XcmVersionedAssetId>, XcmPaymentApiError>",
-                    },
-                    query_weight_to_asset_fee: {
-                        description: "",
-                        params: [
-                            {
-                                name: "weight",
-                                type: "WeightV2",
-                            },
-                            {
-                                name: "asset",
-                                type: "XcmVersionedAssetId",
-                            },
-                        ],
-                        type: "Result<u128, XcmPaymentApiError>",
-                    },
-                    query_xcm_weight: {
-                        description: "",
-                        params: [
-                            {
-                                name: "message",
-                                type: "XcmVersionedXcm",
-                            },
-                        ],
-                        type: "Result<WeightV2, XcmPaymentApiError>",
-                    },
-                },
-                version: 1,
-            },
-        ],
-    },
-    types: {
-        XcmPaymentApiError: {
-            _enum: {
-                Unimplemented: "Null",
-                VersionedConversionFailed: "Null",
-                WeightNotComputable: "Null",
-                UnhandledXcmVersion: "Null",
-                AssetNotFound: "Null",
-            },
-        },
-    },
-};
-
 describeSuite({
     id: "CX0207",
     title: "XCM - XcmPaymentApi",
@@ -79,65 +22,7 @@ describeSuite({
         const txByteFee = 1n;
 
         beforeAll(async function () {
-            // Not using context.polkadotJs() because we need to add the runtime api
-            polkadotJs = await ApiPromise.create({
-                provider: new WsProvider(`ws://localhost:${process.env.MOONWALL_RPC_PORT}/`),
-                runtime: {
-                    XcmPaymentApi: [
-                        {
-                            methods: {
-                                query_acceptable_payment_assets: {
-                                    description: "The API to query acceptable payment assets",
-                                    params: [
-                                        {
-                                            name: "version",
-                                            type: "u32",
-                                        },
-                                    ],
-                                    type: "Result<Vec<XcmVersionedAssetId>, XcmPaymentApiError>",
-                                },
-                                query_weight_to_asset_fee: {
-                                    description: "",
-                                    params: [
-                                        {
-                                            name: "weight",
-                                            type: "WeightV2",
-                                        },
-                                        {
-                                            name: "asset",
-                                            type: "XcmVersionedAssetId",
-                                        },
-                                    ],
-                                    type: "Result<u128, XcmPaymentApiError>",
-                                },
-                                query_xcm_weight: {
-                                    description: "",
-                                    params: [
-                                        {
-                                            name: "message",
-                                            type: "XcmVersionedXcm",
-                                        },
-                                    ],
-                                    type: "Result<WeightV2, XcmPaymentApiError>",
-                                },
-                            },
-                            version: 1,
-                        },
-                    ],
-                },
-                types: {
-                    XcmPaymentApiError: {
-                        _enum: {
-                            Unimplemented: "Null",
-                            VersionedConversionFailed: "Null",
-                            WeightNotComputable: "Null",
-                            UnhandledXcmVersion: "Null",
-                            AssetNotFound: "Null",
-                        },
-                    },
-                },
-            });
-            //
+            polkadotJs = context.polkadotJs();
             chain = polkadotJs.consts.system.version.specName.toString();
             alice =
                 chain == "frontier-template"
@@ -160,6 +45,7 @@ describeSuite({
                     .index.toNumber();
 
                 console.log(chainInfo.toHuman());
+                console.log(api.call.xcmPaymentApi);
 
                 const assets = await api.call.xcmPaymentApi.queryAcceptablePaymentAssets(3);
                 const weightToNativeAssets = await api.call.xcmPaymentApi.queryWeightToAssetFee(
--- typescript-api/src/dancebox/interfaces/tanssi/definitions.ts
+++ typescript-api/src/dancebox/interfaces/tanssi/definitions.ts
@@ -1,3 +1,56 @@
 export default {
-    types: {},
+    runtime: {
+        XcmPaymentApi: [
+            {
+                methods: {
+                    query_acceptable_payment_assets: {
+                        description: "The API to query acceptable payment assets",
+                        params: [
+                            {
+                                name: "version",
+                                type: "u32",
+                            },
+                        ],
+                        type: "Result<Vec<XcmVersionedAssetId>, XcmPaymentApiError>",
+                    },
+                    query_weight_to_asset_fee: {
+                        description: "",
+                        params: [
+                            {
+                                name: "weight",
+                                type: "WeightV2",
+                            },
+                            {
+                                name: "asset",
+                                type: "XcmVersionedAssetId",
+                            },
+                        ],
+                        type: "Result<u128, XcmPaymentApiError>",
+                    },
+                    query_xcm_weight: {
+                        description: "",
+                        params: [
+                            {
+                                name: "message",
+                                type: "XcmVersionedXcm",
+                            },
+                        ],
+                        type: "Result<WeightV2, XcmPaymentApiError>",
+                    },
+                },
+                version: 1,
+            },
+        ],
+    },
+    types: {
+        XcmPaymentApiError: {
+            _enum: {
+                Unimplemented: "Null",
+                VersionedConversionFailed: "Null",
+                WeightNotComputable: "Null",
+                UnhandledXcmVersion: "Null",
+                AssetNotFound: "Null",
+            },
+        },
+    },
 };
