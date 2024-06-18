dec99452f94995a0865ae885284cac085c22104a
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-06-18 12:56:16 +0200

    Add a few expects to test

--- test/suites/common-xcm/xcm/test-xcm-payment-api.ts
+++ test/suites/common-xcm/xcm/test-xcm-payment-api.ts
@@ -80,64 +80,11 @@ describeSuite({
 
         beforeAll(async function () {
             // Not using context.polkadotJs() because we need to add the runtime api
+            // This won't be needed after @polkadot/api adds the XcmPaymentApi
             polkadotJs = await ApiPromise.create({
                 provider: new WsProvider(`ws://localhost:${process.env.MOONWALL_RPC_PORT}/`),
-                runtime: {
-                    XcmPaymentApi: [
-                        {
-                            methods: {
-                                query_acceptable_payment_assets: {
-                                    description: "The API to query acceptable payment assets",
-                                    params: [
-                                        {
-                                            name: "version",
-                                            type: "u32",
-                                        },
-                                    ],
-                                    type: "Result<Vec<XcmVersionedAssetId>, XcmPaymentApiError>",
-                                },
-                                query_weight_to_asset_fee: {
-                                    description: "",
-                                    params: [
-                                        {
-                                            name: "weight",
-                                            type: "WeightV2",
-                                        },
-                                        {
-                                            name: "asset",
-                                            type: "XcmVersionedAssetId",
-                                        },
-                                    ],
-                                    type: "Result<u128, XcmPaymentApiError>",
-                                },
-                                query_xcm_weight: {
-                                    description: "",
-                                    params: [
-                                        {
-                                            name: "message",
-                                            type: "XcmVersionedXcm",
-                                        },
-                                    ],
-                                    type: "Result<WeightV2, XcmPaymentApiError>",
-                                },
-                            },
-                            version: 1,
-                        },
-                    ],
-                },
-                types: {
-                    XcmPaymentApiError: {
-                        _enum: {
-                            Unimplemented: "Null",
-                            VersionedConversionFailed: "Null",
-                            WeightNotComputable: "Null",
-                            UnhandledXcmVersion: "Null",
-                            AssetNotFound: "Null",
-                        },
-                    },
-                },
+                ...runtimeApi,
             });
-            //
             chain = polkadotJs.consts.system.version.specName.toString();
             alice =
                 chain == "frontier-template"
@@ -198,15 +145,16 @@ describeSuite({
                     }
                 );
 
+                const transactWeightAtMost = {
+                    refTime: 200000000n,
+                    proofSize: 3000n,
+                };
                 const xcmToWeight = await api.call.xcmPaymentApi.queryXcmWeight({
                     V3: [
                         {
                             Transact: {
                                 originKind: "Superuser",
-                                requireWeightAtMost: {
-                                    refTime: 200000000n,
-                                    proofSize: 3000n,
-                                },
+                                requireWeightAtMost: transactWeightAtMost,
                                 call: {
                                     encoded:
                                         "0x0408001cbd2d43530a44705ad088af313e18f80b53ef16b36177cd4b77b846f2a5f07c0284d717",
@@ -225,6 +173,13 @@ describeSuite({
                     "\nxcmToWeight: ",
                     xcmToWeight.toHuman()
                 );
+
+                expect(assets.isOk).to.be.true;
+                expect(assets.asOk.toJSON().length).to.be.equal(1);
+                expect(xcmToWeight.isOk).to.be.true;
+                // Weight estimated by queryXcmWeight will always be greater than the weight passed to the transact call as requireWeightAtMost
+                expect(xcmToWeight.asOk.refTime.toBigInt() > transactWeightAtMost.refTime).to.be.true;
+                expect(xcmToWeight.asOk.proofSize.toBigInt() > transactWeightAtMost.proofSize).to.be.true;
 
                 // Output of console.log:
                 // TODO: add expects?
