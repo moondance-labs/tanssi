5575e990d9dc785868a4c6d2bf934a71556a9b08
Akram Hussein <akram@moondancelabs.com>
2024-06-19 12:48:37 -0300

    Add simple chain spec test for dev-tanssi-relay moonwall environment

---
+++ test/suites/dev-tanssi-relay/chain-spec/test_chain_spec.ts
@@ -0,0 +1,29 @@
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { ApiPromise } from "@polkadot/api";
+
+describeSuite({
+    id: "DTR0101",
+    title: "Tanssi Relay Chain Spec",
+    foundationMethods: "dev",
+    testCases: function ({ it, context }) {
+        let polkadotJs: ApiPromise;
+
+        beforeAll(() => {
+            polkadotJs = context.polkadotJs("Relay");
+        });
+
+        it({
+            id: "E01",
+            title: "Checking spec name and version",
+            test: async function () {
+                const relayNetwork = polkadotJs.consts.system;
+
+                const specName = relayNetwork.version.specName.toString();
+                expect(specName, "Relay API incorrect").to.contain("mozart");
+
+                const specVersion = relayNetwork.version.specVersion.toString();
+                expect(specVersion, "Relay API incorrect").to.contain("0.1.0");
+            },
+        });
+    }
+});
