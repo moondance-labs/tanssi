796d0dc2170d833b4223192b98da2927f72aa693
Francisco Gamundi <francisco@moonsonglabs.com>
2024-07-03 15:14:19 +0200

    allow dead code

--- solo-chains/runtime/starlight/tests/common/mod.rs
+++ solo-chains/runtime/starlight/tests/common/mod.rs
@@ -13,7 +13,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
-
+#![allow(dead_code)]
 use {
     crate::UNIT,
     babe_primitives::{
