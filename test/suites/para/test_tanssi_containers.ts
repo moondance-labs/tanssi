aa5a3cb39357067e91cb457d8127d22556fddad3
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-08-05 15:06:13 +0200

    undo test

--- test/suites/para/test_tanssi_containers.ts
+++ test/suites/para/test_tanssi_containers.ts
@@ -74,7 +74,6 @@ describeSuite({
             test: async function () {
                 const blockNum = (await paraApi.rpc.chain.getBlock()).block.header.number.toNumber();
                 expect(blockNum).to.be.greaterThan(0);
-                expect(true).to.equal(false);
             },
         });
 
