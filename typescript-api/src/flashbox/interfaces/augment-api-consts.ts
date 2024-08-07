9fed916d95c9cd4ab85b5b675466881f049cfbd5
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-08-07 11:55:07 +0200

    typescript-api

--- typescript-api/src/flashbox/interfaces/augment-api-consts.ts
+++ typescript-api/src/flashbox/interfaces/augment-api-consts.ts
@@ -6,7 +6,7 @@
 import "@polkadot/api-base/types/consts";
 
 import type { ApiTypes, AugmentedConst } from "@polkadot/api-base/types";
-import type { Option, u128, u16, u32, u64, u8 } from "@polkadot/types-codec";
+import type { u128, u16, u32, u64, u8 } from "@polkadot/types-codec";
 import type { Codec } from "@polkadot/types-codec/types";
 import type { AccountId32, Perbill, Permill } from "@polkadot/types/interfaces/runtime";
 import type {
@@ -275,15 +275,6 @@ declare module "@polkadot/api-base/types/consts" {
             palletId: FrameSupportPalletId & AugmentedConst<ApiType>;
             /** The period during which an approved treasury spend has to be claimed. */
             payoutPeriod: u32 & AugmentedConst<ApiType>;
-            /**
-             * Fraction of a proposal's value that should be bonded in order to place the proposal. An accepted proposal gets
-             * these back. A rejected proposal does not.
-             */
-            proposalBond: Permill & AugmentedConst<ApiType>;
-            /** Maximum amount of funds that should be placed in a deposit for making a proposal. */
-            proposalBondMaximum: Option<u128> & AugmentedConst<ApiType>;
-            /** Minimum amount of funds that should be placed in a deposit for making a proposal. */
-            proposalBondMinimum: u128 & AugmentedConst<ApiType>;
             /** Period between successive spends. */
             spendPeriod: u32 & AugmentedConst<ApiType>;
             /** Generic const */
