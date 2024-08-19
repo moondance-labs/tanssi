6ca605d0bef1d441817ec0924a048d6311911c5f
girazoki <gorka.irazoki@gmail.com>
2024-08-19 18:39:54 +0200

    fix upgrade jobs by using flashbox insteadç

--- test/scripts/build-spec-single-container.sh
+++ test/scripts/build-spec-single-container.sh
@@ -18,6 +18,12 @@ else
     CONTAINER_BINARY="${2}"
 fi
 
+if [[ -z "${3}" || ${3} == "undefined" ]]; then
+    TANSSI_CHAIN="dancebox-local"
+else
+    TANSSI_CHAIN="${3}"
+fi
+
 mkdir -p specs
 $BINARY_FOLDER/$CONTAINER_BINARY build-spec --disable-default-bootnode --add-bootnode "/ip4/127.0.0.1/tcp/33049/ws/p2p/12D3KooWHVMhQDHBpj9vQmssgyfspYecgV6e3hH1dQVDUkUbCYC9" --parachain-id 2000 --raw > specs/single-container-template-container-2000.json
-$BINARY_FOLDER/tanssi-node build-spec --chain dancebox-local --parachain-id 1000 --add-container-chain specs/single-container-template-container-2000.json --invulnerable "Collator1000-01" --invulnerable "Collator1000-02" --invulnerable "Collator1000-03" --invulnerable "Collator2000-01" --invulnerable "Collator2000-02" --raw > specs/single-container-tanssi-1000.json
+$BINARY_FOLDER/tanssi-node build-spec --chain $TANSSI_CHAIN --parachain-id 1000 --add-container-chain specs/single-container-template-container-2000.json --invulnerable "Collator1000-01" --invulnerable "Collator1000-02" --invulnerable "Collator1000-03" --invulnerable "Collator2000-01" --invulnerable "Collator2000-02" --raw > specs/single-container-tanssi-1000.json
