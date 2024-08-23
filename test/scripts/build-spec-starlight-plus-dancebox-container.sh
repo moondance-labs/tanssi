edee82d289b82b2eea13bfb1c25a29bb2dc3a767
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-08-23 18:21:14 +0200

    More zombienet cleanup

--- test/scripts/build-spec-starlight-plus-dancebox-container.sh
+++ test/scripts/build-spec-starlight-plus-dancebox-container.sh
@@ -19,9 +19,8 @@ else
 fi
 
 mkdir -p specs
-$BINARY_FOLDER/$CONTAINER_BINARY build-spec --chain dancebox-local --parachain-id 1000 --invulnerable "Collator1000-01" --invulnerable "Collator1000-02" --raw > specs/single-dancebox-container-1000.json
 $BINARY_FOLDER/container-chain-simple-node build-spec --disable-default-bootnode --add-bootnode "/ip4/127.0.0.1/tcp/33049/ws/p2p/12D3KooWHVMhQDHBpj9vQmssgyfspYecgV6e3hH1dQVDUkUbCYC9" --parachain-id 2000 --raw > specs/single-container-template-container-2000.json
 $BINARY_FOLDER/container-chain-frontier-node build-spec --disable-default-bootnode --add-bootnode "/ip4/127.0.0.1/tcp/33050/ws/p2p/12D3KooWFGaw1rxB6MSuN3ucuBm7hMq5pBFJbEoqTyth4cG483Cc" --parachain-id 2001 --raw > specs/single-container-template-container-2001.json
 #$BINARY_FOLDER/tanssi-relay build-spec --chain starlight-local --add-container-chain specs/single-dancebox-container-1000.json --add-container-chain specs/single-container-template-container-2000.json --add-container-chain specs/single-container-template-container-2001.json --invulnerable "Alice" --invulnerable "Bob" > specs/tanssi-relay.json
-$BINARY_FOLDER/tanssi-relay build-spec --chain starlight-local --add-container-chain specs/single-dancebox-container-1000.json --add-container-chain specs/single-container-template-container-2000.json --add-container-chain specs/single-container-template-container-2001.json --invulnerable "Collator1000-01" --invulnerable "Collator1000-02" --invulnerable "Collator1000-03" --invulnerable "Collator1000-04" --invulnerable "Collator2000-01" --invulnerable "Collator2000-02" > specs/tanssi-relay.json
+$BINARY_FOLDER/tanssi-relay build-spec --chain starlight-local --add-container-chain specs/single-container-template-container-2000.json --add-container-chain specs/single-container-template-container-2001.json --invulnerable "Collator1000-01" --invulnerable "Collator1000-02" --invulnerable "Collator1000-03" --invulnerable "Collator1000-04" --invulnerable "Collator2000-01" --invulnerable "Collator2000-02" > specs/tanssi-relay.json
 ## Here we need to modify the chain-spec. Zombienet is not able to do this
