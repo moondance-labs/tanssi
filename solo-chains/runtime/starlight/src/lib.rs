6046e8bfc57361b5320742b70000a4bde8f6568d
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-09-10 11:26:47 +0200

    Remove should_end_session check, it always returns false

--- solo-chains/runtime/starlight/src/lib.rs
+++ solo-chains/runtime/starlight/src/lib.rs
@@ -2314,16 +2314,7 @@ sp_api::impl_runtime_apis! {
         /// Return the registered para ids
         fn registered_paras() -> Vec<ParaId> {
             // We should return the container-chains for the session in which we are kicking in
-            let parent_number = System::block_number();
-            let should_end_session = <Runtime as pallet_session::Config>::ShouldEndSession::should_end_session(parent_number + 1);
-
-            let session_index = if should_end_session {
-                Session::current_index() +1
-            }
-            else {
-                Session::current_index()
-            };
-
+            let session_index = Session::current_index();
             let container_chains = ContainerRegistrar::session_container_chains(session_index);
             let mut para_ids = vec![];
             para_ids.extend(container_chains.parachains);
@@ -2383,17 +2374,7 @@ sp_api::impl_runtime_apis! {
     impl dp_consensus::TanssiAuthorityAssignmentApi<Block, NimbusId> for Runtime {
         /// Return the current authorities assigned to a given paraId
         fn para_id_authorities(para_id: ParaId) -> Option<Vec<NimbusId>> {
-            let parent_number = System::block_number();
-
-            let should_end_session = <Runtime as pallet_session::Config>::ShouldEndSession::should_end_session(parent_number + 1);
-
-            let session_index = if should_end_session {
-                Session::current_index() +1
-            }
-            else {
-                Session::current_index()
-            };
-
+            let session_index = Session::current_index();
             let assigned_authorities = TanssiAuthorityAssignment::collator_container_chain(session_index)?;
 
             assigned_authorities.container_chains.get(&para_id).cloned()
@@ -2401,15 +2382,7 @@ sp_api::impl_runtime_apis! {
 
         /// Return the paraId assigned to a given authority
         fn check_para_id_assignment(authority: NimbusId) -> Option<ParaId> {
-            let parent_number = System::block_number();
-            let should_end_session = <Runtime as pallet_session::Config>::ShouldEndSession::should_end_session(parent_number + 1);
-
-            let session_index = if should_end_session {
-                Session::current_index() +1
-            }
-            else {
-                Session::current_index()
-            };
+            let session_index = Session::current_index();
             let assigned_authorities = TanssiAuthorityAssignment::collator_container_chain(session_index)?;
             // This self_para_id is used to detect assignments to orchestrator, in this runtime the
             // orchestrator will always be empty so we can set it to any value
