4ea28322e501a40ac2edae12b8ee623a0e5a65e7
girazoki <gorka.irazoki@gmail.com>
2024-10-09 18:59:03 +0200

    final tests

--- test/suites/dev-tanssi-relay/eth-client/test-eth-ciient-submit-update-same-period.ts
+++ test/suites/dev-tanssi-relay/eth-client/test-eth-ciient-submit-update-same-period.ts
@@ -5,7 +5,7 @@ import { readFileSync } from "fs";
 import { KeyringPair } from "@moonwall/util";
 
 describeSuite({
-    id: "DTR1201",
+    id: "DTR1203",
     title: "Ethereum Beacon Client tests",
     foundationMethods: "dev",
 
@@ -16,27 +16,34 @@ describeSuite({
         beforeAll(async () => {
             polkadotJs = context.polkadotJs();
             alice = context.keyring.alice;
+
+            let initialCheckpoint = JSON.parse(
+                readFileSync("tmp/ethereum_client_test/initial-checkpoint.json").toString()
+            );
+            const tx = polkadotJs.tx.ethereumBeaconClient.forceCheckpoint(initialCheckpoint);
+            const signedTx = await polkadotJs.tx.sudo.sudo(tx).signAsync(alice);
+            await context.createBlock([signedTx]);
         });
 
         it({
-            id: "E01",
-            title: "Ethreum client should accept an intiial checkpoint",
+            id: "E02",
+            title: "Ethreum client should be able to receive an update within the same period by same committee",
             test: async function () {
-                let initialCheckpoint = JSON.parse(
-                    readFileSync("tmp/ethereum_client_test/initial-checkpoint.json").toString()
+                let samePeriodUpdate = JSON.parse(
+                    readFileSync("tmp/ethereum_client_test/finalized-header-update.json").toString()
                 );
-                const tx = polkadotJs.tx.ethereumBeaconClient.forceCheckpoint(initialCheckpoint);
-                const signedTx = await polkadotJs.tx.sudo.sudo(tx).signAsync(alice);
+                const tx = polkadotJs.tx.ethereumBeaconClient.submit(samePeriodUpdate);
+                const signedTx = await tx.signAsync(alice);
                 await context.createBlock([signedTx]);
-                const checkpointRoot = await polkadotJs.query.ethereumBeaconClient.validatorsRoot();
-                expect(checkpointRoot.toHuman()).to.equal(initialCheckpoint["validators_root"]);
 
                 const latestFinalizedBlockRoot = await polkadotJs.query.ethereumBeaconClient.latestFinalizedBlockRoot();
                 const latestFinalizedSlot = await polkadotJs.query.ethereumBeaconClient.finalizedBeaconState(
                     latestFinalizedBlockRoot
                 );
 
-                expect(latestFinalizedSlot.toHuman().slot).to.equal(initialCheckpoint["header"]["slot"].toString());
+                expect(latestFinalizedSlot.toHuman().slot).to.equal(
+                    samePeriodUpdate["finalized_header"]["slot"].toString()
+                );
             },
         });
     },
---
+++ test/suites/dev-tanssi-relay/eth-client/test-eth-client-submit-update-next-period-with-proper-committees.ts
@@ -0,0 +1,85 @@
+import "@tanssi/api-augment";
+import { describeSuite, expect, beforeAll } from "@moonwall/cli";
+import { ApiPromise } from "@polkadot/api";
+import { readFileSync } from "fs";
+import { KeyringPair } from "@moonwall/util";
+
+describeSuite({
+    id: "DTR1205",
+    title: "Ethereum Beacon Client tests",
+    foundationMethods: "dev",
+
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+        let alice: KeyringPair;
+        let initialSlot;
+        String;
+
+        beforeAll(async () => {
+            polkadotJs = context.polkadotJs();
+            alice = context.keyring.alice;
+
+            let initialCheckpoint = JSON.parse(
+                readFileSync("tmp/ethereum_client_test/initial-checkpoint.json").toString()
+            );
+            initialSlot = initialCheckpoint["header"]["slot"].toString();
+            const tx = polkadotJs.tx.ethereumBeaconClient.forceCheckpoint(initialCheckpoint);
+            const signedTx = await polkadotJs.tx.sudo.sudo(tx).signAsync(alice);
+            await context.createBlock([signedTx]);
+        });
+
+        it({
+            id: "E01",
+            title: "Ethreum client should be able to receive an update for the next period when pushing all committee info",
+            test: async function () {
+                // Next sync committee shold give us the default values
+                const nextSyncCommitteeBeforeUpdate = await polkadotJs.query.ethereumBeaconClient.nextSyncCommittee();
+                expect(nextSyncCommitteeBeforeUpdate.root.toHuman()).to.be.eq(
+                    "0x0000000000000000000000000000000000000000000000000000000000000000"
+                );
+
+                let thisPeriodNextSyncCommitteeUpdate = JSON.parse(
+                    readFileSync("tmp/ethereum_client_test/sync-committee-update.json").toString()
+                );
+                await context.createBlock([
+                    await polkadotJs.tx.ethereumBeaconClient.submit(thisPeriodNextSyncCommitteeUpdate).signAsync(alice),
+                ]);
+
+                // Now the next sync committee should have been populated
+                const nextSyncCommittee = await polkadotJs.query.ethereumBeaconClient.nextSyncCommittee();
+                expect(nextSyncCommittee.root.toHuman()).to.not.be.eq(
+                    "0x0000000000000000000000000000000000000000000000000000000000000000"
+                );
+
+                // Now we are injecting the first update of the next period
+                // this should contain the next sync committee
+                let nextPeriodSyncCommitteeUpdate = JSON.parse(
+                    readFileSync("tmp/ethereum_client_test/next-sync-committee-update.json").toString()
+                );
+                await context.createBlock([
+                    await polkadotJs.tx.ethereumBeaconClient.submit(nextPeriodSyncCommitteeUpdate).signAsync(alice),
+                ]);
+
+                // Now we are injecting an update for the period 'intial period +1' for which
+                // we have already pushed the sync committee update. Since this needs to be done
+                // only once per period, we shoudl be good
+                let nextPeriodUpdate = JSON.parse(
+                    readFileSync("tmp/ethereum_client_test/next-finalized-header-update.json").toString()
+                );
+
+                await context.createBlock([
+                    await polkadotJs.tx.ethereumBeaconClient.submit(nextPeriodUpdate).signAsync(alice),
+                ]);
+
+                const latestFinalizedBlockRoot = await polkadotJs.query.ethereumBeaconClient.latestFinalizedBlockRoot();
+                const latestFinalizedSlot = await polkadotJs.query.ethereumBeaconClient.finalizedBeaconState(
+                    latestFinalizedBlockRoot
+                );
+
+                // The update did go through, we should have the latest state
+                let expectedSlot = nextPeriodUpdate["finalized_header"]["slot"];
+                expect(latestFinalizedSlot.toHuman().slot.replace(/,/g, "")).to.equal(expectedSlot.toString());
+            },
+        });
+    },
+});
