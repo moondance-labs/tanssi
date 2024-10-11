d09dff2c52bf359f8960a67fbedfac38f59026e4
girazoki <gorka.irazoki@gmail.com>
2024-10-11 15:03:27 +0200

    Add stagelight upgrade job with chopsticks (#719)
    * add stagelight and increase runtime numbers
    
    * add new chopsticks
    
    * remove spec version check from test
    
    * FMT
    
    * update logs
    
    * keep testing
    
    * release
    
    * downgrade chopsticks just in case
    
    * test with latest moonwall
    
    * moonwall timeout inc
    
    * try with localhost
    
    * try again but with 0.16.1
    
    * wip
    
    * remove thing
    
    * update to use self runners
    
    * update chopsticks now
    
    * try passing address

--- solo-chains/runtime/dancelight/src/lib.rs
+++ solo-chains/runtime/dancelight/src/lib.rs
@@ -182,7 +182,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dancelight"),
     impl_name: create_runtime_str!("tanssi-dancelight-v2.0"),
     authoring_version: 0,
-    spec_version: 1_011_000,
+    spec_version: 1000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 25,
