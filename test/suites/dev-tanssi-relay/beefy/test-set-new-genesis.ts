f1fb004fff5bca1ab86460381198827a5ec88c09
Agusrodri <agusrodriguez2456@gmail.com>
2024-10-16 17:29:16 -0700

    add TS test for beefy genesis

---
+++ test/suites/dev-tanssi-relay/beefy/test-set-new-genesis.ts
@@ -0,0 +1,40 @@
+import "@tanssi/api-augment";
+import { describeSuite, expect, beforeAll } from "@moonwall/cli";
+import { ApiPromise } from "@polkadot/api";
+import { jumpToSession } from "../../../util/block";
+
+describeSuite({
+    id: "DTR1202",
+    title: "BEEFY - Set new genesis",
+    foundationMethods: "dev",
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+        beforeAll(() => {
+            polkadotJs = context.polkadotJs();
+        });
+
+        it({
+            id: "E01",
+            title: "Should be able to update the genesis BEEFY block",
+            test: async function () {
+                await jumpToSession(context, 1);
+
+                const genesisDelayBefore = (await polkadotJs.query.beefy.genesisBlock()).toHuman();
+                expect(genesisDelayBefore).to.eq(1);
+
+                const newGenesisDelay: number = 5;
+                const { result } = await context.createBlock(
+                        polkadotJs
+                        .tx.sudo.sudo(polkadotJs.tx.beefy.setNewGenesis(newGenesisDelay))
+                        .signAsync(context.keyring.alice),
+                    {allowFailures: false}
+                );
+                expect(result!.successful, result!.error?.name).to.be.true;
+
+                const currentBlockNumber = (await polkadotJs.rpc.chain.getHeader()).number.toNumber();
+                const genesisDelayAfter = (await polkadotJs.query.beefy.genesisBlock()).toHuman();
+                expect(genesisDelayAfter).to.eq((currentBlockNumber + newGenesisDelay).toString());
+            },
+        });
+    },
+});
