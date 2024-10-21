2775e39362e23970dcf2b0804c34b7246f51c182
girazoki <gorka.irazoki@gmail.com>
2024-10-21 10:16:13 +0200

    only download files when current version does not match

--- test/scripts/download-ethereum-client-test-files.sh
+++ test/scripts/download-ethereum-client-test-files.sh
@@ -9,8 +9,16 @@ cd $(dirname $0)/..
 # Grab Polkadot version
 branch=$(egrep -o '/polkadot-sdk.*#([^\"]*)' ../Cargo.lock | head -1)
 polkadot_release=$(echo $branch | sed 's/.*branch=//' | sed 's/#.*//')
+if [ -f tmp/ethereum_client_test/latest_version.txt ]; then
+    stored_version=$(< tmp/ethereum_client_test/latest_version.txt)
+    if [[ "$VAR1" == "$VAR2" ]]; then
+        echo "Stored version is latest, nothing to do"
+        exit 0;
+    fi
+fi
 mkdir -p tmp
 wget -O - tmp/ethereum_client_test https://github.com/moondance-labs/polkadot-sdk/archive/$polkadot_release.tar.gz | tar -xz --strip=6 "polkadot-sdk-$polkadot_release/bridges/snowbridge/pallets/ethereum-client/tests/fixtures"
 # remove for a clean move
 rm -rf  tmp/ethereum_client_test
 mv  -u  fixtures tmp/ethereum_client_test
+echo $polkadot_release > tmp/ethereum_client_test/latest_version.txt
