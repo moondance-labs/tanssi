f465490de5d4aa829ef71c0ca443595b2c504804
girazoki <gorka.irazoki@gmail.com>
2024-10-22 17:12:03 +0200

    remove comments

--- pallets/external-validator-slashes/src/lib.rs
+++ pallets/external-validator-slashes/src/lib.rs
@@ -14,20 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
 
-//! # Author Noting Pallet
-//!
-//! This pallet notes the author of the different containerChains that have registered:
-//!
-//! The set of container chains is retrieved thanks to the GetContainerChains trait
-//! For each containerChain, we inspect the Header stored in the relayChain as
-//! a generic header. This is the first requirement for containerChains.
-//!
-//! The second requirement is that an Aura digest with the slot number for the containerChains
-//! needs to exist
-//!  
-//! Using those two requirements we can select who the author was based on the collators assigned
-//! to that containerChain, by simply assigning the slot position.
-
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use {
