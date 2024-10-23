0d53f5bd0b1625fbf9829f4c2f2b350811c32dab
girazoki <gorka.irazoki@gmail.com>
2024-10-23 11:15:32 +0200

    add offence handler

--- solo-chains/runtime/dancelight/src/lib.rs
+++ solo-chains/runtime/dancelight/src/lib.rs
@@ -565,7 +565,7 @@ impl pallet_treasury::Config for Runtime {
 impl pallet_offences::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type IdentificationTuple = pallet_session::historical::IdentificationTuple<Self>;
-    type OnOffenceHandler = ();
+    type OnOffenceHandler = ExternalValidatorSlashes;
 }
 
 impl pallet_authority_discovery::Config for Runtime {
