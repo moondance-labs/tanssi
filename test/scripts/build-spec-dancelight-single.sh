0e890958837de992f3c513f297df843a4f774b88
girazoki <gorka.irazoki@gmail.com>
2024-10-25 12:52:13 +0200

    add first job for testing starliht ugrade through zombie (#731)
    * add first job for testing relay ugrade through zombie
    
    * add to CI
    
    * fix
    
    * try to fix
    
    * use current node
    
    * and workers
    
    * add script correctly

---
+++ test/scripts/build-spec-dancelight-single.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+# Always run the commands from the "test" dir
+cd $(dirname $0)/..
+
+if [[ -z "${1}" || ${1} == "undefined" ]]; then
+    BINARY_FOLDER="../target/release"
+else
+    BINARY_FOLDER="${1}"
+fi
+
+mkdir -p specs
+$BINARY_FOLDER/tanssi-relay build-spec --chain dancelight-local > specs/tanssi-relay.json
