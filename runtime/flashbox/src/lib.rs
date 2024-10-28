fd99db15322a6e4fd975d62ef7f5d859e0207a5c
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2024-10-28 16:11:35 +0100

    Add starlight support to version bump script
    And fix flashbox outdated spec version

--- runtime/flashbox/src/lib.rs
+++ runtime/flashbox/src/lib.rs
@@ -223,7 +223,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("flashbox"),
     impl_name: create_runtime_str!("flashbox"),
     authoring_version: 1,
-    spec_version: 900,
+    spec_version: 1000,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
