a9e7e9742283433759330e72193bdafee2aa3b79
Parth Desai <desaiparth08@gmail.com>
2024-10-28 19:34:56 +0400

    increment spec version to 1010

--- solo-chains/runtime/dancelight/src/lib.rs
+++ solo-chains/runtime/dancelight/src/lib.rs
@@ -184,7 +184,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("dancelight"),
     impl_name: create_runtime_str!("tanssi-dancelight-v2.0"),
     authoring_version: 0,
-    spec_version: 1000,
+    spec_version: 1010,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 26,
