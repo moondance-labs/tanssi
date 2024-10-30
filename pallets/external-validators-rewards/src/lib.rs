357ac0140b5239ac421a31a4c672ed4499ff5b21
nanocryk <6422796+nanocryk@users.noreply.github.com>
2024-10-30 15:07:29 +0100

    pallet prototype

---
+++ pallets/external-validators-rewards/src/lib.rs
@@ -0,0 +1,146 @@
+// Tanssi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Tanssi is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
+
+//! TODO: Crate docs
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use {
+    frame_support::traits::{Defensive, ValidatorSet},
+    polkadot_primitives::ValidatorIndex,
+    runtime_parachains::session_info,
+    sp_staking::SessionIndex,
+    sp_std::collections::btree_set::BTreeSet,
+};
+
+#[frame_support::pallet]
+pub mod pallet {
+    use {frame_support::pallet_prelude::*, sp_std::collections::btree_map::BTreeMap};
+
+    /// The current storage version.
+    const STORAGE_VERSION: StorageVersion = StorageVersion::new(0);
+
+    pub type RewardPoints = u32;
+    pub type EraIndex = u32;
+
+    #[pallet::config]
+    pub trait Config: frame_system::Config {
+        type CurrentEra: Get<Option<EraIndex>>;
+    }
+
+    #[pallet::pallet]
+    #[pallet::storage_version(STORAGE_VERSION)]
+    pub struct Pallet<T>(_);
+
+    /// TODO: Docs
+    #[derive(RuntimeDebug, Encode, Decode, PartialEq, Eq, TypeInfo)]
+    pub struct EraRewardPoints<AccountId> {
+        pub total: RewardPoints,
+        pub individual: BTreeMap<AccountId, RewardPoints>,
+    }
+
+    impl<AccountId> Default for EraRewardPoints<AccountId> {
+        fn default() -> Self {
+            EraRewardPoints {
+                total: Default::default(),
+                individual: BTreeMap::new(),
+            }
+        }
+    }
+
+    /// TODO: Docs
+    #[pallet::storage]
+    #[pallet::unbounded]
+    pub type ErasRewardPoints<T: Config> =
+        StorageMap<_, Twox64Concat, EraIndex, EraRewardPoints<T::AccountId>, ValueQuery>;
+
+    impl<T: Config> Pallet<T> {
+        pub fn reward_by_ids(points: impl IntoIterator<Item = (T::AccountId, RewardPoints)>) {
+            let Some(active_era) = T::CurrentEra::get() else {
+                return;
+            };
+
+            ErasRewardPoints::<T>::mutate(active_era, |era_rewards| {
+                for (validator, points) in points.into_iter() {
+                    *era_rewards.individual.entry(validator).or_default() += points;
+                    era_rewards.total += points;
+                }
+            })
+        }
+    }
+}
+
+/// The amount of era points given by backing a candidate that is included.
+pub const BACKING_POINTS: u32 = 20;
+/// The amount of era points given by dispute voting on a candidate.
+pub const DISPUTE_STATEMENT_POINTS: u32 = 20;
+
+/// Rewards validators for participating in parachains with era points in pallet-staking.
+pub struct RewardValidatorsWithEraPoints<C>(core::marker::PhantomData<C>);
+
+impl<C> RewardValidatorsWithEraPoints<C>
+where
+    C: pallet::Config + session_info::Config,
+    C::ValidatorSet: ValidatorSet<C::AccountId, ValidatorId = C::AccountId>,
+{
+    /// Reward validators in session with points, but only if they are in the active set.
+    fn reward_only_active(
+        session_index: SessionIndex,
+        indices: impl IntoIterator<Item = ValidatorIndex>,
+        points: u32,
+    ) {
+        let validators = session_info::AccountKeys::<C>::get(&session_index);
+        let validators = match validators
+            .defensive_proof("account_keys are present for dispute_period sessions")
+        {
+            Some(validators) => validators,
+            None => return,
+        };
+        // limit rewards to the active validator set
+        let active_set: BTreeSet<_> = C::ValidatorSet::validators().into_iter().collect();
+
+        let rewards = indices
+            .into_iter()
+            .filter_map(|i| validators.get(i.0 as usize).cloned())
+            .filter(|v| active_set.contains(v))
+            .map(|v| (v, points));
+
+        pallet::Pallet::<C>::reward_by_ids(rewards);
+    }
+}
+
+impl<C> runtime_parachains::inclusion::RewardValidators for RewardValidatorsWithEraPoints<C>
+where
+    C: pallet::Config + runtime_parachains::shared::Config + session_info::Config,
+    C::ValidatorSet: ValidatorSet<C::AccountId, ValidatorId = C::AccountId>,
+{
+    fn reward_backing(indices: impl IntoIterator<Item = ValidatorIndex>) {
+        let session_index = runtime_parachains::shared::CurrentSessionIndex::<C>::get();
+        Self::reward_only_active(session_index, indices, BACKING_POINTS);
+    }
+
+    fn reward_bitfields(_validators: impl IntoIterator<Item = ValidatorIndex>) {}
+}
+
+impl<C> runtime_parachains::disputes::RewardValidators for RewardValidatorsWithEraPoints<C>
+where
+    C: pallet::Config + session_info::Config,
+    C::ValidatorSet: ValidatorSet<C::AccountId, ValidatorId = C::AccountId>,
+{
+    fn reward_dispute_statement(
+        session: SessionIndex,
+        validators: impl IntoIterator<Item = ValidatorIndex>,
+    ) {
+        Self::reward_only_active(session, validators, DISPUTE_STATEMENT_POINTS);
+    }
+}
