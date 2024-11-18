3c32ffc31bbadada4f082be98b2de0609a137abc
girazoki <gorka.irazoki@gmail.com>
2024-11-18 12:47:37 +0100

    test related to validator rewards

---
+++ test/suites/dev-tanssi-relay/external-validators-rewards/test_external_validator_rewards.ts
@@ -0,0 +1,68 @@
+import "@tanssi/api-augment";
+import { describeSuite, customDevRpcRequest, expect, beforeAll } from "@moonwall/cli";
+import { ApiPromise, Keyring } from "@polkadot/api";
+import { jumpToSession } from "util/block";
+import { before } from "node:test";
+
+describeSuite({
+    id: "DTR1601",
+    title: "Paras inherent tests",
+    foundationMethods: "dev",
+
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+
+        beforeAll(async () => {
+            polkadotJs = context.polkadotJs();
+        });
+
+        it({
+            id: "E01",
+            title: "para candidates should trigger reward info",
+            test: async function () {
+                const keyring = new Keyring({ type: "sr25519" });
+                const aliceStash = keyring.addFromUri("//Alice//stash");
+                await context.createBlock();
+                // Send RPC call to enable para inherent candidate generation
+                await customDevRpcRequest("mock_enableParaInherentCandidate", []);
+                // Since collators are not assigned until session 2, we need to go till session 2 to actually see heads being injected
+                await jumpToSession(context, 3);
+                await context.createBlock();
+
+                // we are still in era 0
+                const validatorRewards = await context
+                    .polkadotJs()
+                    .query.externalValidatorsRewards.rewardPointsForEra(0);
+                const totalRewards = validatorRewards.total.toBigInt();
+
+                expect(totalRewards).to.be.greaterThan(0n);
+                // All of them come from alice as she is the only one validating candidates
+                expect(validatorRewards.individual.toHuman()[aliceStash.address]).to.be.eq(totalRewards.toString());
+            },
+        });
+
+        it({
+            id: "E02",
+            title: "Check rewards storage clears after historyDepth",
+            test: async function () {
+                const sessionsPerEra = await polkadotJs.consts.externalValidators.sessionsPerEra;
+                const historyDepth = await polkadotJs.consts.externalValidatorsRewards.historyDepth;
+
+                const currentIndex = await polkadotJs.query.session.currentIndex();
+
+                const targetSession =
+                    currentIndex.toNumber() + sessionsPerEra.toNumber() * (historyDepth.toNumber() + 1);
+
+                await jumpToSession(context, targetSession);
+
+                const validatorRewards = await context
+                    .polkadotJs()
+                    .query.externalValidatorsRewards.rewardPointsForEra(0);
+                const totalRewards = validatorRewards.total.toBigInt();
+
+                // rewards should have expired
+                expect(totalRewards).to.be.equal(0n);
+            },
+        });
+    },
+});
--- test/util/relayInterface.ts
+++ test/util/relayInterface.ts
@@ -1,6 +1,6 @@
 import { ApiPromise } from "@polkadot/api";
 import type { Header, ParaId, HeadData } from "@polkadot/types/interfaces";
-import { u8aToHex, u8aToU8a, isU8a, hexToU8a, compactToU8a, compactFromU8a, compactFromU8aLim } from "@polkadot/util"
+import { u8aToHex, u8aToU8a, isU8a, hexToU8a, compactToU8a, compactFromU8a, compactFromU8aLim } from "@polkadot/util";
 import { bool, u32, u8, Bytes } from "@polkadot/types-codec";
 import { TypeRegistry } from "@polkadot/types";
 
@@ -9,7 +9,7 @@ export async function getHeaderFromRelay(relayApi: ApiPromise, paraId: ParaId): 
     const encoded = await relayApi.query.paras.heads(paraId);
     const registry = new TypeRegistry();
     const headerEncoded: HeadData = await relayApi.createType("HeadData", encoded.toHex());
-    const nonEncodedHeader = new Bytes(registry, headerEncoded.toU8a(true)).toHex()
+    const nonEncodedHeader = new Bytes(registry, headerEncoded.toU8a(true)).toHex();
 
     const header = await relayApi.createType("SpRuntimeHeader", nonEncodedHeader);
     return header;
