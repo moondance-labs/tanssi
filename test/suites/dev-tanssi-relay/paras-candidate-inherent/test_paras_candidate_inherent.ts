aefd1c6ef057a806294e13a16c0ee5a1b2c22f9d
girazoki <gorka.irazoki@gmail.com>
2024-11-18 13:39:44 +0100

    more lint

--- test/suites/dev-tanssi-relay/paras-candidate-inherent/test_paras_candidate_inherent.ts
+++ test/suites/dev-tanssi-relay/paras-candidate-inherent/test_paras_candidate_inherent.ts
@@ -1,8 +1,7 @@
 import "@tanssi/api-augment";
-import { describeSuite, customDevRpcRequest, expect } from "@moonwall/cli";
+import { describeSuite, customDevRpcRequest, expect, beforeAll } from "@moonwall/cli";
 import { ApiPromise } from "@polkadot/api";
 import { jumpToSession } from "util/block";
-import { before } from "node:test";
 import { getHeaderFromRelay } from "util/relayInterface.ts";
 
 describeSuite({
@@ -13,7 +12,7 @@ describeSuite({
     testCases: ({ it, context }) => {
         let polkadotJs: ApiPromise;
 
-        before(async () => {
+        beforeAll(async () => {
             polkadotJs = context.polkadotJs();
         });
 
@@ -21,18 +20,18 @@ describeSuite({
             id: "E01",
             title: "Paras heads should be updated every block",
             test: async function () {
-                const parasHeadGenesis = await context.polkadotJs().query.paras.heads(2000);
+                const parasHeadGenesis = await polkadotJs.query.paras.heads(2000);
                 await context.createBlock();
                 // Send RPC call to enable para inherent candidate generation
                 await customDevRpcRequest("mock_enableParaInherentCandidate", []);
                 // Since collators are not assigned until session 2, we need to go till session 2 to actually see heads being injected
                 await jumpToSession(context, 3);
                 await context.createBlock();
-                const parasHeadAfterOneBlock = await context.polkadotJs().query.paras.heads(2000);
+                const parasHeadAfterOneBlock = await polkadotJs.query.paras.heads(2000);
                 expect(parasHeadAfterOneBlock).to.not.be.eq(parasHeadGenesis);
                 await context.createBlock();
                 // we create one more block to test we are persisting candidates every block
-                const parasHeadAfterTwoBlocks = await context.polkadotJs().query.paras.heads(2000);
+                const parasHeadAfterTwoBlocks = await polkadotJs.query.paras.heads(2000);
                 expect(parasHeadAfterOneBlock).to.not.be.eq(parasHeadAfterTwoBlocks);
                 const header2000 = await getHeaderFromRelay(context.polkadotJs(), 2000);
                 expect(header2000.number.toBigInt()).to.be.equal(31n);
