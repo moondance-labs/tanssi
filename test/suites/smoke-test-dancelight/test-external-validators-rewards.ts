0447e399d23e67db1e9f0e249da29d463697e14f
nanocryk <6422796+nanocryk@users.noreply.github.com>
2024-11-25 16:56:45 +0100

    check points sum is consistent

---
+++ test/suites/smoke-test-dancelight/test-external-validators-rewards.ts
@@ -0,0 +1,31 @@
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import { ApiPromise } from "@polkadot/api";
+
+describeSuite({
+    id: "S22",
+    title: "Smoke tests for external validators rewards pallet",
+    foundationMethods: "read_only",
+    testCases: ({ it, context }) => {
+        let api: ApiPromise;
+
+        beforeAll(async () => {
+            api = context.polkadotJs();
+        });
+
+        it({
+            id: "C01",
+            title: "Total points matches sum of individual points",
+            test: async function () {
+                const entries = await api.query.externalValidatorsRewards.rewardPointsForEra.entries();
+
+                for (const [key, entry] of entries) {
+                    let sum = 0;
+                    for (const [, points] of entry.individual.entries()) {
+                        sum += points.toNumber();
+                    }
+                    expect(sum).to.be.eq(entry.total.toNumber(), `inconsistency at key ${key}`);
+                }
+            },
+        });
+    },
+});
