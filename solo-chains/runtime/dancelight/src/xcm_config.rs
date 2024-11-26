c67674e4eaee705eaf1f9add4d04a217fe9a8d36
Diego <diego2737@gmail.com>
2024-11-27 01:30:46 +0900

    Accept tokens from other parachains

--- solo-chains/runtime/dancelight/src/xcm_config.rs
+++ solo-chains/runtime/dancelight/src/xcm_config.rs
@@ -117,6 +117,27 @@ pub type XcmRouter = WithUniqueTopic<
     ChildParachainRouter<Runtime, XcmPallet, PriceForChildParachainDelivery>,
 >;
 
+pub struct NativeAssetReserve;
+impl frame_support::traits::ContainsPair<Asset, Location> for NativeAssetReserve {
+    fn contains(asset: &Asset, origin: &Location) -> bool {
+        log::trace!(target: "xcm::contains", "NativeAssetReserve asset: {:?}, origin: {:?}", asset, origin);
+        let reserve = if asset.id.0.parents == 0
+            && !matches!(asset.id.0.first_interior(), Some(Parachain(_)))
+        {
+            Some(Location::here())
+        } else {
+            asset.id.0.chain_part()
+        };
+
+        if let Some(ref reserve) = reserve {
+            if reserve == origin {
+                return true;
+            }
+        }
+        false
+    }
+}
+
 parameter_types! {
     pub Star: AssetFilter = Wild(AllOf { fun: WildFungible, id: AssetId(TokenLocation::get()) });
     pub AssetHub: Location = Parachain(ASSET_HUB_ID).into_location();
