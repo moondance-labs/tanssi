32c65b9f023fb1ad212248f8f251c6ad2cfb237a
Diego <diego2737@gmail.com>
2024-12-11 01:09:19 +0900

    Add missing copyright

--- primitives/xcm-commons/src/lib.rs
+++ primitives/xcm-commons/src/lib.rs
@@ -1,3 +1,8 @@
+// Copyright (C) Moondance Labs Ltd.
+// This file is part of Tanssi.
+
+// Tanssi is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 
