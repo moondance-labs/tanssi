f26441a21b036cca105839cca9f0b1c5c1ecc286
Diego <diego2737@gmail.com>
2024-12-23 14:23:06 -0300

    Use simple mv since we dont need the -u flag (#781)

--- test/scripts/download-ethereum-client-test-files.sh
+++ test/scripts/download-ethereum-client-test-files.sh
@@ -19,6 +19,6 @@ fi
 mkdir -p tmp
 wget -O - tmp/ethereum_client_test https://github.com/moondance-labs/polkadot-sdk/archive/$polkadot_release.tar.gz | tar -xz --strip=6 "polkadot-sdk-$polkadot_release/bridges/snowbridge/pallets/ethereum-client/tests/fixtures"
 # remove for a clean move
-rm -rf  tmp/ethereum_client_test
-mv  -u  fixtures tmp/ethereum_client_test
+rm -rf tmp/ethereum_client_test
+mv fixtures tmp/ethereum_client_test
 echo $polkadot_release > tmp/ethereum_client_test/latest_version.txt
