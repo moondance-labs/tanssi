6aa3fd1114a04bc55443efa7c792cd42ebdaccf9
girazoki <gorka.irazoki@gmail.com>
2025-01-03 10:57:20 +0100

    add collator block credits

--- test/suites/common-tanssi-parachain/services-payment/test_services_payment_collator_credits.ts
+++ test/suites/common-tanssi-parachain/services-payment/test_services_payment_collator_credits.ts
@@ -6,7 +6,7 @@ import { jumpSessions } from "util/block";
 import { paraIdTank } from "util/payment";
 
 describeSuite({
-    id: "CPT0601",
+    id: "CPT0602",
     title: "Services payment test suite",
     foundationMethods: "dev",
     testCases: ({ it, context }) => {
