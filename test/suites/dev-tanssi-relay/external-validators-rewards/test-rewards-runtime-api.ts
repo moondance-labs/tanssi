a4dd2618aec30873330065d0132ec84169c1db0d
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-01-07 16:58:00 +0100

    WIP, need to create a set_external_validators extrinsic

--- test/suites/dev-tanssi-relay/external-validators-rewards/test-rewards-runtime-api.ts
+++ test/suites/dev-tanssi-relay/external-validators-rewards/test-rewards-runtime-api.ts
@@ -1,6 +1,7 @@
 import { beforeAll, customDevRpcRequest, describeSuite, expect } from "@moonwall/cli";
 import { ApiPromise, Keyring } from "@polkadot/api";
 import { jumpToSession } from "util/block";
+import {DANCE} from "../../../util/constants.ts";
 
 describeSuite({
     id: "DTR0820",
@@ -8,9 +9,11 @@ describeSuite({
     foundationMethods: "dev",
     testCases: ({ it, context }) => {
         let polkadotJs: ApiPromise;
+        let alice;
 
         beforeAll(async function () {
             polkadotJs = context.polkadotJs();
+            alice = context.keyring.alice;
         });
 
         it({
@@ -22,7 +25,22 @@ describeSuite({
                 await context.createBlock();
                 // Send RPC call to enable para inherent candidate generation
                 await customDevRpcRequest("mock_enableParaInherentCandidate", []);
-                // Since collators are not assigned until session 2, we need to go till session 2 to actually see heads being injected
+
+                // Register Alice as an external validator, because it starts as a whitelisted validator and whitelisted
+                // validators don't get rewards.
+                let aliceNonce = (await polkadotJs.rpc.system.accountNextIndex(alice.address)).toNumber();
+
+                await context.createBlock([
+                    await polkadotJs.tx.sudo.sudo(polkadotJs.tx.externalValidators
+                        .removeWhitelisted(alice.address))
+                        .signAsync(context.keyring.alice, { nonce: aliceNonce++ }),
+                    await polkadotJs.tx.sudo.sudo(polkadotJs.tx.externalValidators
+                        .set(alice.address))
+                        .signAsync(context.keyring.alice, { nonce: aliceNonce++ }),
+                ]);
+
+
+                    // Since collators are not assigned until session 2, we need to go till session 2 to actually see heads being injected
                 await jumpToSession(context, 3);
                 await context.createBlock();
 
