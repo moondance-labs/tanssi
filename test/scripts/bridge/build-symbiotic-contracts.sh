7c0aa88aafe08e39d6c44571c6daa2ad77640ff7
girazoki <gorka.irazoki@gmail.com>
2025-01-15 10:51:49 +0100

    leftover

--- test/scripts/bridge/build-symbiotic-contracts.sh
+++ test/scripts/bridge/build-symbiotic-contracts.sh
@@ -9,21 +9,6 @@ source $scripts_path/set-env.sh
 
 echo "Building symbiotic contracts"
 
-echo "Checkout Symbiotic contract repository"
-
-if [ -d "$symbiotic_contracts_dir" ];
-then
-  pushd $symbiotic_contracts_dir
-  git fetch && git checkout 142d523cd5c62f2f2b62541b6cb68de75a2404e3
-  popd
-  echo "Symbiotic contract repository seems to be already setup. Skipping git clone"
-else
-  git clone https://github.com/moondance-labs/tanssi-symbiotic $symbiotic_contracts_dir
-  pushd $symbiotic_contracts_dir
-  git fetch && git checkout 142d523cd5c62f2f2b62541b6cb68de75a2404e3
-  popd
-fi
-
 pushd $symbiotic_contracts_dir
 forge build
 popd
