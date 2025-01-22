bc4ffbd0eb9e9d3a8e4ffb5b5d7f6148226b2946
nanocryk <6422796+nanocryk@users.noreply.github.com>
2025-01-22 14:04:29 +0100

    ts test inflation is minted in correct account

--- chains/orchestrator-relays/runtime/dancelight/src/lib.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/lib.rs
@@ -1409,6 +1409,7 @@ impl Get<u64> for TimestampProvider {
 
 parameter_types! {
     // Chain ID of Holesky.
+    // Output is: 34cdd3f84040fb44d70e83b892797846a8c0a556ce08cd470bf6d4cf7b94ff77
     pub RewardsEthereumSovereignAccount: AccountId =
         tp_bridge::EthereumLocationsConverterFor::<AccountId>::convert_location(
             &EthereumLocation::get()
