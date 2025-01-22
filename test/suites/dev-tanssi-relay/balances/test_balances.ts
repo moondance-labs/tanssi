b91a19389e26b876d04dff11c313f5af64c26b56
nanocryk <6422796+nanocryk@users.noreply.github.com>
2025-01-22 15:27:41 +0100

    update test about total issuance

--- test/suites/dev-tanssi-relay/balances/test_balances.ts
+++ test/suites/dev-tanssi-relay/balances/test_balances.ts
@@ -20,7 +20,7 @@ describeSuite({
             title: "Checking total issuance is correct on genesis",
             test: async function () {
                 const totalIssuance = (await polkadotJs.query.balances.totalIssuance()).toBigInt();
-                expect(totalIssuance).toBe(12_000_000_000_133_333_332n);
+                expect(totalIssuance).toBe(13_000_000_000_133_333_332n);
             },
         });
 
