70da8e30b7f168d5c7341fb0760823bdeeb28563
timbrinded <79199034+timbrinded@users.noreply.github.com>
2025-02-07 11:11:52 +0000

    refactor: ♻️ Update authority smoke test

--- test/suites/smoke-test-dancelight/test-authority-consistency-assignation.ts
+++ test/suites/smoke-test-dancelight/test-authority-consistency-assignation.ts
@@ -1,5 +1,5 @@
+import "@tanssi/api-augment/dancelight";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
-
 import type { ApiPromise } from "@polkadot/api";
 
 describeSuite({
@@ -17,22 +17,32 @@ describeSuite({
             id: "C01",
             title: "Collator assignation and authority assignation should match with observed mapping in containers",
             test: async () => {
-                const assignmentCollatorAccount = (
-                    await api.query.tanssiCollatorAssignment.collatorContainerChain()
-                ).toJSON();
+                const assignmentCollatorAccount = await api.query.tanssiCollatorAssignment.collatorContainerChain();
                 const sessionIndex = (await api.query.session.currentIndex()).toNumber();
                 const assignmentCollatorKey = (
                     await api.query.tanssiAuthorityAssignment.collatorContainerChain(sessionIndex)
-                ).toJSON();
+                ).unwrap();
                 const authorityKeyMapping = (
                     await api.query.tanssiAuthorityMapping.authorityIdMapping(sessionIndex)
-                ).toJSON();
-                for (const container of Object.keys(assignmentCollatorKey.containerChains)) {
-                    for (const key of assignmentCollatorKey.containerChains[container]) {
-                        const assignedAccount = authorityKeyMapping[key.toString()];
-                        expect(
-                            assignmentCollatorAccount.containerChains[container].includes(assignedAccount.toString())
-                        ).to.be.true;
+                ).unwrap();
+
+                const remappedAssignmentCollatorAccount = Object.fromEntries(
+                    [...assignmentCollatorAccount.containerChains.entries()].map(([key, value]) => [
+                        key.toString(),
+                        value.map((v) => v.toHuman()),
+                    ])
+                );
+
+                const remappedAuthorityKeyMapping = Object.fromEntries(
+                    [...authorityKeyMapping.entries()].map(([key, value]) => [key.toHex(), value.toHuman()])
+                );
+
+                const containerChains = assignmentCollatorKey.containerChains.keys();
+
+                for (const container of containerChains) {
+                    for (const key of assignmentCollatorKey.containerChains.get(container)) {
+                        const assignedAccount = remappedAuthorityKeyMapping[key.toHex()];
+                        expect(remappedAssignmentCollatorAccount[container.toString()]).toContain(assignedAccount);
                     }
                 }
             },
