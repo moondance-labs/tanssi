a82ecfa48514b47666a90559a787baf54cc328cd
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-02-10 10:19:14 +0000

    update validator rewards smoke

--- test/suites/smoke-test-dancelight/test-external-validators-rewards.ts
+++ test/suites/smoke-test-dancelight/test-external-validators-rewards.ts
@@ -1,3 +1,4 @@
+import "@tanssi/api-augment/dancelight";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 import type { ApiPromise } from "@polkadot/api";
 
@@ -5,7 +6,7 @@ describeSuite({
     id: "SMOK05",
     title: "Smoke tests for external validators rewards pallet",
     foundationMethods: "read_only",
-    testCases: ({ it, context }) => {
+    testCases: ({ it, context, log }) => {
         let api: ApiPromise;
 
         beforeAll(async () => {
@@ -18,13 +19,23 @@ describeSuite({
             test: async () => {
                 const entries = await api.query.externalValidatorsRewards.rewardPointsForEra.entries();
 
-                for (const [key, entry] of entries) {
-                    let sum = 0;
-                    for (const [, points] of entry.individual.entries()) {
-                        sum += points.toNumber();
-                    }
-                    expect(sum).to.be.eq(entry.total.toNumber(), `inconsistency at key ${key}`);
+                log(`Checking rewards for ${entries.length} validators...`);
+                const failures = entries
+                    .map(([key, entry]) => {
+                        const sum = [...entry.individual.entries()].reduce(
+                            (acc, [key, points]) => acc + points.toNumber(),
+                            0
+                        );
+                        const failed = sum !== entry.total.toNumber();
+                        return { failed, key: key.toHex() };
+                    })
+                    .filter(({ failed }) => failed);
+
+                for (const failed of failures) {
+                    console.error(`inconsistency at key ${failed.key}`);
                 }
+
+                expect(failures.length).to.be.eq(0);
             },
         });
     },
