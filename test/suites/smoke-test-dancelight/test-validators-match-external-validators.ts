d8b24e275387d7c78df21aedc69becc8ca555c55
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-02-10 11:19:20 +0000

    update session/ext validators test

--- test/suites/smoke-test-dancelight/test-validators-match-external-validators.ts
+++ test/suites/smoke-test-dancelight/test-validators-match-external-validators.ts
@@ -1,13 +1,12 @@
+import "@tanssi/api-augment/dancelight";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 import type { ApiPromise } from "@polkadot/api";
-import type { U32, Vec } from "@polkadot/types-codec";
-import type { AccountId32 } from "@polkadot/types/interfaces";
 
 describeSuite({
     id: "SMOK08",
     title: "Smoke tests for validators matching external validators",
     foundationMethods: "read_only",
-    testCases: ({ it, context }) => {
+    testCases: ({ it, context, log }) => {
         let api: ApiPromise;
 
         beforeAll(async () => {
@@ -18,43 +17,72 @@ describeSuite({
             id: "C01",
             title: "Validators should match external validators",
 
-            test: async () => {
+            test: async (vitest) => {
                 // Find the last block in which the era changed
-                const currentEra = await api.query.externalValidators.currentEra<U32>();
-                let blockToCheck = (await api.query.babe.epochStart()).toJSON()[1];
+                const currentEra = await api.query.externalValidators.currentEra();
+                if (currentEra.isNone) {
+                    expect.fail("No external validators found");
+                }
+
+                let blockToCheck = (await api.query.babe.epochStart())[1].toNumber();
                 let apiBeforeLatestNewSession = await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1));
 
-                while (currentEra === (await apiBeforeLatestNewSession.query.externalValidators.currentEra<U32>())) {
-                    blockToCheck = (await apiBeforeLatestNewSession.query.babe.epochStart()).toJSON()[1];
+                while (
+                    currentEra.unwrap().toBigInt() ===
+                    (await apiBeforeLatestNewSession.query.externalValidators.currentEra()).unwrap().toBigInt()
+                ) {
+                    blockToCheck = (await apiBeforeLatestNewSession.query.babe.epochStart())[1].toNumber();
                     apiBeforeLatestNewSession = await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1));
                 }
 
-                const externalValidatorsList = await (
-                    await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1))
-                ).query.externalValidators.externalValidators<Vec<AccountId32>>();
-                const whitelistedValidatorsList = await (
-                    await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1))
-                ).query.externalValidators.whitelistedValidators<Vec<AccountId32>>();
+                const externalValidatorsList = (
+                    await (
+                        await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1))
+                    ).query.externalValidators.externalValidators()
+                ).map((validator) => validator.toHuman());
+                const whitelistedValidatorsList = (
+                    await (
+                        await api.at(await api.rpc.chain.getBlockHash(blockToCheck - 1))
+                    ).query.externalValidators.whitelistedValidators()
+                ).map((validator) => validator.toHuman());
 
-                const sessionValidators = await api.query.session.validators();
-                const externalValidators = externalValidatorsList.toArray().concat(whitelistedValidatorsList.toArray());
+                const sessionValidators = (await api.query.session.validators()).map((acc) => acc.toHuman());
+                const externalValidators = [...externalValidatorsList, ...whitelistedValidatorsList];
 
                 if (externalValidators.length <= sessionValidators.length) {
-                    // Less external validators than session validators: all external validators must be session validators
-                    for (const externalValidator of externalValidators) {
-                        expect(
-                            sessionValidators.toString().includes(externalValidator.toString()),
-                            `External validator should be in validators list: ${externalValidator.toString()}`
-                        ).to.be.true;
+                    const failures = externalValidators.filter(
+                        (externalValidator) => !sessionValidators.includes(externalValidator)
+                    );
+
+                    if (failures.length > 0) {
+                        for (const failure of failures) {
+                            log(`External validator ${failure} should be in session validators list`);
+                        }
+
+                        log(`Session validators list: [ ${sessionValidators.join(", ")} ]`);
                     }
+
+                    expect(
+                        failures.length,
+                        "Equal/Fewer ext than session validators: all external validators must be session validators"
+                    ).toBe(0);
                 } else {
-                    // More external validators than session validators: all session validators must be external validators
-                    for (const validator of sessionValidators) {
-                        expect(
-                            externalValidators.toString().includes(validator.toString()),
-                            `Validator should be in external validators list: ${validator.toString()}`
-                        ).to.be.true;
+                    const failures = sessionValidators.filter(
+                        (sessionValidator) => !externalValidators.includes(sessionValidator)
+                    );
+
+                    if (failures.length > 0) {
+                        for (const failure of failures) {
+                            log(`Session validator ${failure} should be in external validators list`);
+                        }
+
+                        log(`External validators list: [ ${externalValidators.join(", ")} ]`);
                     }
+
+                    expect(
+                        failures.length,
+                        "More ext than session validators: all session validators must be external validators"
+                    ).toBe(0);
                 }
             },
         });
