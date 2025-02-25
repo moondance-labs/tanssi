12fd54d9b7e2de3b9acd73f9174ffc7efc432ae4
Evgeny Svirsky <e.svirsky@tut.by>
2025-02-25 13:25:53 +0100

    - Added smoke test: Check if message with rewards is sent in the end of the era and nonce is incremented

--- test/suites/smoke-test-dancelight/test-external-validators-rewards.ts
+++ test/suites/smoke-test-dancelight/test-external-validators-rewards.ts
@@ -1,6 +1,7 @@
 import "@tanssi/api-augment/dancelight";
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
 import type { ApiPromise } from "@polkadot/api";
+import { PRIMARY_GOVERNANCE_CHANNEL_ID } from "../../util/constants.ts";
 
 describeSuite({
     id: "SMOK05",
@@ -36,6 +37,39 @@ describeSuite({
                 }
 
                 expect(failures.length).to.be.eq(0);
+            },
+        });
+
+        it({
+            id: "C02",
+            title: "Check if message with rewards is sent in the end of the era and nonce is incremented",
+            test: async () => {
+                // Checkpoint B - the block number of current epoch start
+                const currentEpochStartBlockNumber = (await api.query.babe.epochStart())[0].toNumber();
+                // Checkpoint A - the block number before Checkpoint B
+                const beforeCurrentEpochStartBlockNumber = currentEpochStartBlockNumber - 1;
+
+                const apiAtCheckpointA = await api.at(await api.rpc.chain.getBlockHash(beforeCurrentEpochStartBlockNumber));
+                const apiAtCheckpointB = await api.at(await api.rpc.chain.getBlockHash(currentEpochStartBlockNumber));
+
+                const event = (await apiAtCheckpointB.query.system.events()).find(event => event.event.method === 'RewardsMessageSent');
+
+                const checkpointAPrimaryChannelNonce =
+                    await apiAtCheckpointA.query.ethereumOutboundQueue.nonce(PRIMARY_GOVERNANCE_CHANNEL_ID);
+
+                const checkpointBPrimaryChannelNonce =
+                    await api.query.ethereumOutboundQueue.nonce(PRIMARY_GOVERNANCE_CHANNEL_ID);
+
+                const nonceDiff = checkpointBPrimaryChannelNonce.toNumber()  - checkpointAPrimaryChannelNonce.toNumber();
+
+                // The event is triggered, nonce should be incremented
+                if (event) {
+                    expect(nonceDiff).toEqual(1);
+
+                // The event is not triggered, nonce should be the same
+                } else {
+                    expect(nonceDiff).toEqual(0);
+                }
             },
         });
     },
