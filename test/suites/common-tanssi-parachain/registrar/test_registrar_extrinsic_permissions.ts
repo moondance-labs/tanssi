5f7bb122e0d1e7857821df2c4efd687a6fc58b49
Evgeny Svirsky <e.svirsky@tut.by>
2025-03-06 16:29:15 +0100

    - Fixed PR comment - Added e2e test

---
+++ test/suites/common-tanssi-parachain/registrar/test_registrar_extrinsic_permissions.ts
@@ -0,0 +1,54 @@
+import "@tanssi/api-augment";
+
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import type { KeyringPair } from "@moonwall/util";
+import type { ApiPromise } from "@polkadot/api";
+
+describeSuite({
+    id: "COMMO1107",
+    title: "Registrar extrinsics permissions",
+    foundationMethods: "dev",
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+        let alice: KeyringPair;
+        let bob: KeyringPair;
+        const paraId = 2001;
+
+        beforeAll(() => {
+            alice = context.keyring.alice;
+            bob = context.keyring.bob;
+            polkadotJs = context.polkadotJs();
+        });
+
+        it({
+            id: "E01",
+            title: "Para manager can execute registrar pallet extrinsics",
+            test: async () => {
+                await context.createBlock();
+
+                // Bob is not a manager, extrinsic requiring RegistrarOrigin should fail with BadOrigin error
+                const { result: pauseContainerResultAttempt1 } = await context.createBlock(
+                    await polkadotJs.tx.registrar.pauseContainerChain(paraId).signAsync(bob)
+                );
+                expect(pauseContainerResultAttempt1.successful).to.be.false;
+                expect(pauseContainerResultAttempt1.error.name).toEqual("BadOrigin");
+
+                // Set bob as manager
+                const tx = polkadotJs.tx.registrar.setParaManager(paraId, bob.address);
+
+                const { result: sudoResult } = await context.createBlock(
+                    await polkadotJs.tx.sudo.sudo(tx).signAsync(alice)
+                );
+
+                expect(sudoResult.successful).to.be.true;
+
+                // Now it should show ParaIdNotRegistered error but not the BadOrigin
+                const { result: pauseContainerResultAttempt2 } = await context.createBlock(
+                    await polkadotJs.tx.registrar.pauseContainerChain(paraId).signAsync(bob)
+                );
+
+                expect(pauseContainerResultAttempt2.successful).toEqual(true);
+            },
+        });
+    },
+});
