f05547ba71710d8175b398bae42d0a7bfaecb25c
nanocryk <6422796+nanocryk@users.noreply.github.com>
2025-03-14 17:11:12 +0100

    ts api

--- primitives/data-preservers-common/src/lib.rs
+++ primitives/data-preservers-common/src/lib.rs
@@ -14,6 +14,8 @@
 // You should have received a copy of the GNU General Public License
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
 
+#![cfg_attr(not(feature = "std"), no_std)]
+
 use core::marker::PhantomData;
 use frame_support::dispatch::DispatchErrorWithPostInfo;
 use frame_support::traits::OriginTrait;
