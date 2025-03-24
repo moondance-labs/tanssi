91bfd1e8e10fd8ab06a093a16eef66aac1967faf
Evgeny Svirsky <e.svirsky@tut.by>
2025-03-24 18:40:20 +0100

    - Added script to auto-generate

--- typescript-api/src/dancelight/interfaces/augment-types.ts
+++ typescript-api/src/dancelight/interfaces/augment-types.ts
@@ -1300,6 +1300,7 @@ declare module "@polkadot/types/types/registry" {
         AbridgedHostConfiguration: AbridgedHostConfiguration;
         AbridgedHrmpChannel: AbridgedHrmpChannel;
         AccountData: AccountData;
+
         AccountId: AccountId;
         AccountId20: AccountId20;
         AccountId32: AccountId32;
