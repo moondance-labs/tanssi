3b2a9233e60afb4f1e9c251ea61537f5c7c9946e
girazoki <gorka.irazoki@gmail.com>
2025-04-15 18:09:18 +0200

    increase spec

--- chains/orchestrator-relays/runtime/dancelight/src/lib.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/lib.rs
@@ -186,7 +186,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("dancelight"),
     impl_name: Cow::Borrowed("tanssi-dancelight-v2.0"),
     authoring_version: 0,
-    spec_version: 1202,
+    spec_version: 1203,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 26,
