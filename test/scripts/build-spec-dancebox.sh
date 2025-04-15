0f97eab0ec31ddba91f6927f4f279aa720ef5d84
Evgeny Svirsky <e.svirsky@tut.by>
2025-04-15 13:37:27 +0200

    - Create folder before spec generation

--- test/scripts/build-spec-dancebox.sh
+++ test/scripts/build-spec-dancebox.sh
@@ -3,7 +3,7 @@
 # Exit on any error
 set -e
 
-pwd
+mkdir -p specs
 tmp/tanssi-node build-spec --chain dancebox-local > specs/dancebox-plain-spec.json
 pnpm tsx scripts/modify-plain-specs.ts process specs/dancebox-plain-spec.json specs/dancebox-modified-spec.json
 tmp/tanssi-node build-spec --chain specs/dancebox-modified-spec.json --raw > specs/dancebox-raw-spec.json
