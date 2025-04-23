0420e5d10eb7f474d4b92bd01b26590c57850673
Evgeny Svirsky <e.svirsky@tut.by>
2025-04-23 13:29:29 +0200

    - Disabled all the required extrinsics

--- chains/orchestrator-relays/runtime/starlight/src/lib.rs
+++ chains/orchestrator-relays/runtime/starlight/src/lib.rs
@@ -285,38 +285,74 @@ impl Convert<AggregateMessageOrigin, ParaId> for GetParaFromAggregateMessageOrig
     }
 }
 
-/// The relay register and deregister calls should no longer be necessary
-/// Everything is handled by the containerRegistrar
-pub struct IsRelayRegister;
-impl Contains<RuntimeCall> for IsRelayRegister {
-    fn contains(c: &RuntimeCall) -> bool {
-        matches!(
-            c,
-            RuntimeCall::Registrar(paras_registrar::Call::register { .. })
-        ) || matches!(
-            c,
-            RuntimeCall::Registrar(paras_registrar::Call::deregister { .. })
-        )
-    }
-}
-
-/// Starlight shouold not permit parathread registration for now
-/// TODO: remove once they are enabled
-pub struct IsParathreadRegistrar;
-impl Contains<RuntimeCall> for IsParathreadRegistrar {
-    fn contains(c: &RuntimeCall) -> bool {
-        matches!(
-            c,
-            RuntimeCall::ContainerRegistrar(pallet_registrar::Call::register_parathread { .. })
-        )
-    }
-}
-
 /// Disable any extrinsic related to the balance transfer
 pub struct IsBalanceTransferExtrinsics;
 impl Contains<RuntimeCall> for IsBalanceTransferExtrinsics {
     fn contains(c: &RuntimeCall) -> bool {
         matches!(c, RuntimeCall::Balances(_))
+    }
+}
+
+pub struct IsContainerChainManagementExtrinsics;
+impl Contains<RuntimeCall> for IsContainerChainManagementExtrinsics {
+    fn contains(c: &RuntimeCall) -> bool {
+        matches!(
+            c,
+            RuntimeCall::ServicesPayment(_)
+                | RuntimeCall::StreamPayment(_)
+                | RuntimeCall::DataPreservers(_)
+        )
+    }
+}
+
+pub struct IsDemocracyExtrinsics;
+impl Contains<RuntimeCall> for IsDemocracyExtrinsics {
+    fn contains(c: &RuntimeCall) -> bool {
+        matches!(
+            c,
+            RuntimeCall::Treasury(_)
+                | RuntimeCall::ConvictionVoting(_)
+                | RuntimeCall::Referenda(_)
+                | RuntimeCall::FellowshipCollective(_)
+                | RuntimeCall::FellowshipReferenda(_)
+                | RuntimeCall::Whitelist(_)
+                | RuntimeCall::Preimage(_)
+        )
+    }
+}
+
+pub struct IsMiscellaneousExtrinsics;
+impl Contains<RuntimeCall> for IsMiscellaneousExtrinsics {
+    fn contains(c: &RuntimeCall) -> bool {
+        matches!(
+            c,
+            RuntimeCall::Proxy(_) | RuntimeCall::Utility(_) | RuntimeCall::Identity(_)
+        )
+    }
+}
+
+pub struct IsXcmExtrinsics;
+impl Contains<RuntimeCall> for IsXcmExtrinsics {
+    fn contains(c: &RuntimeCall) -> bool {
+        matches!(
+            c,
+            RuntimeCall::Hrmp(_)
+                | RuntimeCall::MessageQueue(_)
+                | RuntimeCall::AssetRate(_)
+                | RuntimeCall::XcmPallet(_)
+        )
+    }
+}
+
+pub struct IsContainerChainRegistrationExtrinsics;
+impl Contains<RuntimeCall> for IsContainerChainRegistrationExtrinsics {
+    fn contains(c: &RuntimeCall) -> bool {
+        matches!(
+            c,
+            RuntimeCall::ContainerRegistrar(_)
+                | RuntimeCall::OnDemandAssignmentProvider(_)
+                | RuntimeCall::Registrar(_)
+        )
     }
 }
 
@@ -349,9 +385,12 @@ parameter_types! {
 #[derive_impl(frame_system::config_preludes::RelayChainDefaultConfig)]
 impl frame_system::Config for Runtime {
     type BaseCallFilter = EverythingBut<(
-        IsRelayRegister,
-        IsParathreadRegistrar,
         IsBalanceTransferExtrinsics,
+        IsContainerChainManagementExtrinsics,
+        IsDemocracyExtrinsics,
+        IsMiscellaneousExtrinsics,
+        IsXcmExtrinsics,
+        IsContainerChainRegistrationExtrinsics,
         IsBridgesExtrinsics,
         IsStakingExtrinsics,
     )>;
@@ -439,7 +478,7 @@ impl EnsureOriginWithArg<RuntimeOrigin, RuntimeParametersKey> for DynamicParamet
         match key {
             Preimage(_) => frame_system::ensure_root(origin.clone()),
         }
-        .map_err(|_| origin)
+            .map_err(|_| origin)
     }
 
     #[cfg(feature = "runtime-benchmarks")]
@@ -502,7 +541,7 @@ impl pallet_babe::Config for Runtime {
     type MaxNominators = ConstU32<0>;
     type KeyOwnerProof = sp_session::MembershipProof;
     type EquivocationReportSystem =
-        pallet_babe::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+    pallet_babe::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
 }
 
 parameter_types! {
@@ -729,7 +768,7 @@ impl pallet_grandpa::Config for Runtime {
     type MaxSetIdSessionEntries = MaxSetIdSessionEntries;
     type KeyOwnerProof = sp_session::MembershipProof;
     type EquivocationReportSystem =
-        pallet_grandpa::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+    pallet_grandpa::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
 }
 
 /// Submits a transaction with the node's public and signature type. Adheres to the signed extension
@@ -1037,7 +1076,7 @@ impl parachains_session_info::Config for Runtime {
 }
 
 pub type RewardValidators =
-    pallet_external_validators_rewards::RewardValidatorsWithEraPoints<Runtime>;
+pallet_external_validators_rewards::RewardValidatorsWithEraPoints<Runtime>;
 
 impl parachains_inclusion::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
@@ -1093,7 +1132,7 @@ impl ProcessMessage for MessageProcessor {
                     xcm_executor::XcmExecutor<xcm_config::XcmConfig>,
                     RuntimeCall,
                 >::process_message(
-                    message, Junction::Parachain(para.into()), meter, id
+                    message, Junction::Parachain(para.into()), meter, id,
                 )
             }
             AggregateMessageOrigin::Snowbridge(_) => {
@@ -1121,7 +1160,7 @@ impl pallet_message_queue::Config for Runtime {
     type MessageProcessor = MessageProcessor;
     #[cfg(feature = "runtime-benchmarks")]
     type MessageProcessor =
-        pallet_message_queue::mock_helpers::NoopMessageProcessor<AggregateMessageOrigin>;
+    pallet_message_queue::mock_helpers::NoopMessageProcessor<AggregateMessageOrigin>;
     type QueueChangeHandler = ParaInclusion;
     type QueuePausedQuery = ();
     type WeightInfo = weights::pallet_message_queue::SubstrateWeight<Runtime>;
@@ -1139,10 +1178,10 @@ impl parachains_hrmp::Config for Runtime {
     type ChannelManager = EnsureRoot<AccountId>;
     type Currency = Balances;
     type DefaultChannelSizeAndCapacityWithSystem =
-        parachains_configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio<
-            Runtime,
-            HrmpChannelSizeAndCapacityWithSystemRatio,
-        >;
+    parachains_configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio<
+        Runtime,
+        HrmpChannelSizeAndCapacityWithSystemRatio,
+    >;
     type WeightInfo = weights::runtime_parachains_hrmp::SubstrateWeight<Runtime>;
     type VersionWrapper = XcmPallet;
 }
@@ -1159,7 +1198,7 @@ impl parachains_scheduler::Config for Runtime {
 
 pub struct CollatorAssignmentProvider;
 impl parachains_scheduler::common::AssignmentProvider<BlockNumberFor<Runtime>>
-    for CollatorAssignmentProvider
+for CollatorAssignmentProvider
 {
     fn pop_assignment_for_core(core_idx: CoreIndex) -> Option<Assignment> {
         let assigned_collators = TanssiCollatorAssignment::collator_container_chain();
@@ -1296,7 +1335,7 @@ impl parachains_disputes::Config for Runtime {
 impl parachains_slashing::Config for Runtime {
     type KeyOwnerProofSystem = Historical;
     type KeyOwnerProof =
-        <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, ValidatorId)>>::Proof;
+    <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, ValidatorId)>>::Proof;
     type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
         KeyTypeId,
         ValidatorId,
@@ -1352,7 +1391,7 @@ impl pallet_beefy::Config for Runtime {
     type WeightInfo = ();
     type KeyOwnerProof = <Historical as KeyOwnerProofSystem<(KeyTypeId, BeefyId)>>::Proof;
     type EquivocationReportSystem =
-        pallet_beefy::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+    pallet_beefy::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
     type AncestryHelper = BeefyMmrLeaf;
 }
 
@@ -2015,7 +2054,7 @@ pub type TxExtension = (
 
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
-    generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, TxExtension>;
+generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, TxExtension>;
 
 /// The runtime migrations per release.
 pub mod migrations {
@@ -2045,9 +2084,9 @@ pub struct InnerStarlightRegistrar<Runtime, AccountId, RegistrarManager, Registr
     PhantomData<(Runtime, AccountId, RegistrarManager, RegistrarWeightInfo)>,
 );
 impl<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo> RegistrarHandler<AccountId>
-    for InnerStarlightRegistrar<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo>
+for InnerStarlightRegistrar<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo>
 where
-    RegistrarManager: RegistrarInterface<AccountId = AccountId>,
+    RegistrarManager: RegistrarInterface<AccountId=AccountId>,
     RegistrarWeightInfo: paras_registrar::WeightInfo,
     Runtime: pallet_registrar::Config + paras_registrar::Config,
     sp_runtime::AccountId32: From<AccountId>,
@@ -2224,7 +2263,7 @@ impl pallet_registrar::RegistrarHooks for StarlightRegistrarHooks {
             <Runtime as pallet_data_preservers::Config>::AssignmentOrigin::try_successful_origin(
                 &para_id,
             )
-            .expect("should be able to get para manager");
+                .expect("should be able to get para manager");
 
         DataPreservers::start_assignment(
             para_manager,
@@ -2232,7 +2271,7 @@ impl pallet_registrar::RegistrarHooks for StarlightRegistrarHooks {
             para_id,
             tp_data_preservers_common::AssignerExtra::Free,
         )
-        .expect("assignment to work");
+            .expect("assignment to work");
 
         assert!(
             pallet_data_preservers::Assignments::<Runtime>::get(para_id).contains(&profile_id),
@@ -3524,7 +3563,7 @@ impl ParaIdAssignmentHooksImpl {
             &pallet_services_payment::Pallet::<Runtime>::parachain_tank(para_id),
             remaining_to_pay,
         )
-        .into_result(true)?;
+            .into_result(true)?;
         // TODO: Have proper weight
         Ok(Weight::zero())
     }
@@ -3544,7 +3583,7 @@ impl<AC> ParaIdAssignmentHooks<BalanceOf<Runtime>, AC> for ParaIdAssignmentHooks
                     &Some(max_tip),
                 ))
             })
-            .is_ok()
+                .is_ok()
         });
     }
 
@@ -3568,10 +3607,10 @@ impl<AC> ParaIdAssignmentHooks<BalanceOf<Runtime>, AC> for ParaIdAssignmentHooks
                     maybe_tip,
                 )
             })
-            .inspect(|weight| {
-                total_weight += *weight;
-            })
-            .is_ok()
+                .inspect(|weight| {
+                    total_weight += *weight;
+                })
+                .is_ok()
         });
         total_weight
     }
@@ -3659,7 +3698,7 @@ impl pallet_collator_assignment::Config for Runtime {
     type SessionIndex = u32;
     type SelfParaId = MockParaId;
     type ShouldRotateAllCollators =
-        RotateCollatorsEveryNSessions<ConfigurationCollatorRotationSessionPeriod>;
+    RotateCollatorsEveryNSessions<ConfigurationCollatorRotationSessionPeriod>;
     type GetRandomnessForNextBlock = BabeGetRandomnessForNextBlock;
     type RemoveInvulnerables = ();
     type ParaIdAssignmentHooks = ParaIdAssignmentHooksImpl;
@@ -3711,7 +3750,7 @@ mod benchmark_helpers {
                     slot: slot.into(),
                     authority_index: 0,
                 })
-                .encode(),
+                    .encode(),
             )],
         };
 
