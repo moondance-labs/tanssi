a93695ffe3b2fd2283b89ddad2fe426eedcaddec
Agustín Rodriguez <agus@moonsonglabs.com>
2025-04-29 10:19:26 -0300

    [Starlight] Add dev-test suite and adapt existing TS tests (#998)

--- test/suites/common-tanssi/services-payment/test_service_payment_removes_tank_money_and_refunds.ts
+++ test/suites/common-tanssi/services-payment/test_service_payment_removes_tank_money_and_refunds.ts
@@ -5,6 +5,7 @@ import { type KeyringPair, generateKeyringPair } from "@moonwall/util";
 import type { ApiPromise } from "@polkadot/api";
 import { jumpSessions } from "utils";
 import { paraIdTank } from "utils";
+import { STARLIGHT_VERSIONS_TO_EXCLUDE_FROM_SERVICES_PAYMENT, checkCallIsFiltered } from "helpers";
 
 describeSuite({
     id: "COMM0202",
@@ -20,15 +21,37 @@ describeSuite({
         let balanceTankBefore: bigint;
         let purchasedCredits: bigint;
         let registerAlias: any;
+        let isStarlight: boolean;
+        let specVersion: number;
+        let shouldSkipStarlightSP: boolean;
         beforeAll(async () => {
             polkadotJs = context.polkadotJs();
             alice = context.keyring.alice;
             const runtimeName = polkadotJs.runtimeVersion.specName.toString();
             registerAlias = runtimeName.includes("light") ? polkadotJs.tx.containerRegistrar : polkadotJs.tx.registrar;
 
+            isStarlight = runtimeName === "starlight";
+            specVersion = polkadotJs.consts.system.version.specVersion.toNumber();
+            shouldSkipStarlightSP =
+                isStarlight && STARLIGHT_VERSIONS_TO_EXCLUDE_FROM_SERVICES_PAYMENT.includes(specVersion);
+
             refundAddress = generateKeyringPair("sr25519");
             const tx2001OneSession = polkadotJs.tx.servicesPayment.setBlockProductionCredits(paraId2001, 0);
-            await context.createBlock([await polkadotJs.tx.sudo.sudo(tx2001OneSession).signAsync(alice)]);
+
+            if (shouldSkipStarlightSP) {
+                console.log(`Services payment tests for Starlight version ${specVersion}`);
+
+                // We check that the call (without sudo) is filtered.
+                await checkCallIsFiltered(context, polkadotJs, await tx2001OneSession.signAsync(alice));
+
+                // Purchase credits should be filtered too
+                const tx = polkadotJs.tx.servicesPayment.purchaseCredits(paraId2001, 100n);
+                await checkCallIsFiltered(context, polkadotJs, await tx.signAsync(alice));
+                return;
+            }
+
+            const sudoSignedTx = await polkadotJs.tx.sudo.sudo(tx2001OneSession).signAsync(alice);
+            await context.createBlock([sudoSignedTx]);
             const existentialDeposit = await polkadotJs.consts.balances.existentialDeposit.toBigInt();
             // Now, buy some credits for container chain 2001
             purchasedCredits = blocksPerSession * costPerBlock + existentialDeposit;
@@ -42,10 +65,20 @@ describeSuite({
             title: "Sudo can set refund address",
             test: async () => {
                 // We deregister the chain
-                const setRefundAddress = polkadotJs.tx.sudo.sudo(
-                    polkadotJs.tx.servicesPayment.setRefundAddress(paraId2001, refundAddress.address)
+
+                const setRefundAddress = polkadotJs.tx.servicesPayment.setRefundAddress(
+                    paraId2001,
+                    refundAddress.address
                 );
-                await context.createBlock([await setRefundAddress.signAsync(alice)]);
+
+                if (shouldSkipStarlightSP) {
+                    console.log(`Skipping E01 test for Starlight version ${specVersion}`);
+                    await checkCallIsFiltered(context, polkadotJs, await setRefundAddress.signAsync(alice));
+                    return;
+                }
+
+                const setRefundAddressSudo = polkadotJs.tx.sudo.sudo(setRefundAddress);
+                await context.createBlock([await setRefundAddressSudo.signAsync(alice)]);
                 // Check that we can fetch the address
                 const refundAddressOnChain = await polkadotJs.query.servicesPayment.refundAddress(paraId2001);
                 expect(refundAddressOnChain.toString(), "Refund address should be set").toBe(refundAddress.address);
@@ -55,6 +88,10 @@ describeSuite({
             id: "E02",
             title: "On deregistration we refund the address",
             test: async () => {
+                if (shouldSkipStarlightSP) {
+                    console.log(`Skipping E02 test for Starlight version ${specVersion}`);
+                    return;
+                }
                 // We deregister the chain
                 const deregister2001 = polkadotJs.tx.sudo.sudo(registerAlias.deregister(paraId2001));
                 await context.createBlock([await deregister2001.signAsync(alice)]);
