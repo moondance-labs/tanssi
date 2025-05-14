23a911c65f19bdee1f4e44603186e0721ac6ef56
augustocollado <augustocollado@gmail.com>
2025-05-14 10:27:41 -0300

    Removes comment

--- chains/orchestrator-paras/runtime/common/src/lib.rs
+++ chains/orchestrator-paras/runtime/common/src/lib.rs
@@ -41,8 +41,6 @@ where
         }
     }
 
-    // this is called from pallet_evm for Ethereum-based transactions
-    // (technically, it calls on_unbalanced, which calls this when non-zero)
     fn on_nonzero_unbalanced(amount: Credit<R::AccountId, pallet_balances::Pallet<R>>) {
         // 100% goes to the treasury
         ResolveTo::<pallet_treasury::TreasuryAccountId<R>, pallet_balances::Pallet<R>>::on_unbalanced(amount);
