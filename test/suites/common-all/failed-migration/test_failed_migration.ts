81cfc90be406f4a2f0325ac8b1b731f8d473d24c
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-05-23 10:54:28 +0200

    Better comments

--- test/suites/common-all/failed-migration/test_failed_migration.ts
+++ test/suites/common-all/failed-migration/test_failed_migration.ts
@@ -1,7 +1,7 @@
 import "@tanssi/api-augment";
 
 import { beforeAll, describeSuite, expect } from "@moonwall/cli";
-import { type KeyringPair, alith, baltathar, charleth, dorothy } from "@moonwall/util";
+import { type KeyringPair, alith } from "@moonwall/util";
 import type { ApiPromise } from "@polkadot/api";
 
 describeSuite({
@@ -12,36 +12,24 @@ describeSuite({
     testCases: ({ it, context }) => {
         let polkadotJs: ApiPromise;
         let alice_or_alith: KeyringPair;
-        let charlie_or_charleth: KeyringPair;
-        let dave_or_baltathar: KeyringPair;
-        let bob_or_dorothy: KeyringPair;
-        let specVersion: number;
-        let isStarlight: boolean;
-        let isFrontier: boolean;
 
         beforeAll(async () => {
             polkadotJs = context.polkadotJs();
             // This test will be run against frontier & substrate chains, hence the accounts used
             alice_or_alith = context.isEthereumChain ? alith : context.keyring.alice;
-            charlie_or_charleth = context.isEthereumChain ? charleth : context.keyring.charlie;
-            // Multisig extrinsics expect accounts to be sorted, that's why we swap bob and dave here
-            dave_or_baltathar = context.isEthereumChain ? baltathar : context.keyring.dave;
-            bob_or_dorothy = context.isEthereumChain ? dorothy : context.keyring.bob;
-            specVersion = polkadotJs.consts.system.version.specVersion.toNumber();
-            isStarlight = polkadotJs.consts.system.version.specName.toString() === "starlight";
-            isFrontier = polkadotJs.consts.system.version.specName.toString() === "frontier-template";
         });
 
         it({
             id: "E01",
             title: "Migrate runtime to same runtime and trigger stuck migrations error handler",
             test: async () => {
-                const wasmCode = await polkadotJs.rpc.state.getStorage("0x3a636f6465"); // :code
+                const wasmCode = await polkadotJs.rpc.state.getStorage(":code");
                 const wasmCodeHex = wasmCode.toHex();
                 console.log(`Current runtime code prefix: ${wasmCodeHex.slice(0, 20)}...`);
 
                 // Remove from storage the info about the current runtime version.
                 // This ensures that the new runtime is considered new, because `runtime_upgraded()` returns true.
+                // Without this on_runtime_upgrade will not be called.
                 // https://github.com/paritytech/polkadot-sdk/blob/26afcd65438f5518bb6a0c281cfcc6159a60587e/substrate/frame/executive/src/lib.rs#L602
                 const tx1 = polkadotJs.tx.system.killStorage([polkadotJs.query.system.lastRuntimeUpgrade.key()]);
 
@@ -57,6 +45,8 @@ describeSuite({
                     await polkadotJs.tx.utility
                         .batchAll([
                             polkadotJs.tx.sudo.sudo(tx1),
+                            // Must use sudoUncheckedWeight because setCode takes 100% of block weight,
+                            // so without this the node will not include the batch tx
                             polkadotJs.tx.sudo.sudoUncheckedWeight(tx2, { refTime: 1, proofSize: 1 }),
                             polkadotJs.tx.sudo.sudo(tx3),
                         ])
@@ -68,6 +58,10 @@ describeSuite({
                 console.log(cursor.toJSON());
                 expect(cursor.unwrap().isStuck).to.be.true;
 
+                // No maintenance mode yet
+                const maintenanceMode = await polkadotJs.query.maintenanceMode.maintenanceMode();
+                expect(maintenanceMode.toJSON()).to.be.false;
+
                 // Create block to trigger on_runtime_upgrade
                 await context.createBlock();
 
@@ -77,8 +71,8 @@ describeSuite({
                 expect(cursor2.isNone).to.be.true;
 
                 // Chain has entered maintenance mode
-                const maintenanceMode = await polkadotJs.query.maintenanceMode.maintenanceMode();
-                expect(maintenanceMode.toJSON()).to.be.true;
+                const maintenanceMode2 = await polkadotJs.query.maintenanceMode.maintenanceMode();
+                expect(maintenanceMode2.toJSON()).to.be.true;
             },
         });
     },
