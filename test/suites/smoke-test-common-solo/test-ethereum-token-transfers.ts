628335217908ffaba9e1754590fd1e3a42e5c3c1
Diego <diego2737@gmail.com>
2025-06-17 17:35:35 -0300

    Add smoke tests for native tokens send to ethereum

---
+++ test/suites/smoke-test-common-solo/test-ethereum-token-transfers.ts
@@ -0,0 +1,88 @@
+import "@tanssi/api-augment/dancelight";
+
+import { beforeAll, describeSuite, expect } from "@moonwall/cli";
+import type { ApiPromise } from "@polkadot/api";
+import { hexToU8a } from "@polkadot/util";
+import { encodeAddress } from "@polkadot/util-crypto";
+import { ETHEREUM_MAINNET_SOVEREIGN_ACCOUNT_ADDRESS, SEPOLIA_SOVEREIGN_ACCOUNT_ADDRESS } from "utils";
+
+const SS58_FORMAT = 42;
+
+const BLOCKS_AMOUNT_TO_CHECK = 100;
+
+describeSuite({
+    id: "SMOK15",
+    title: "Ethereum token transfers smoke tests",
+    foundationMethods: "read_only",
+    testCases: ({ it, context }) => {
+        let api: ApiPromise;
+        let sovereignAccount: string;
+
+        beforeAll(async () => {
+            api = context.polkadotJs();
+            const runtimeName = api.runtimeVersion.specName.toString();
+            const isStarlight = runtimeName === "starlight";
+            sovereignAccount = isStarlight
+                ? ETHEREUM_MAINNET_SOVEREIGN_ACCOUNT_ADDRESS
+                : SEPOLIA_SOVEREIGN_ACCOUNT_ADDRESS;
+
+            sovereignAccount = encodeAddress(hexToU8a(sovereignAccount), SS58_FORMAT);
+        });
+
+        it({
+            id: "C01",
+            title: "Token transfer channels exists",
+            test: async () => {
+                const channels = await api.query.ethereumSystem.channels.entries();
+                expect(channels.length).toBeGreaterThan(0);
+            },
+        });
+
+        it({
+            id: "C02",
+            title: "Sovereign account collects amount when native token is transferred",
+            test: async () => {
+                // Go through the last BLOCKS_AMOUNT_TO_CHECK blocks and check if the sovereign account is collecting
+                // the amount for each native token transfer event.
+
+                const currentBlock = (await api.rpc.chain.getBlock()).block.header.number.toNumber();
+
+                for (let i = 1; i <= BLOCKS_AMOUNT_TO_CHECK; i++) {
+                    const blockNumber = currentBlock - i;
+                    process.stdout.write(`\rProcessing block [${blockNumber}]: ${i}/${BLOCKS_AMOUNT_TO_CHECK}`);
+
+                    const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+                    const events = await api.query.system.events.at(blockHash);
+
+                    const tokenTransferEvents = events.filter(({ event }) =>
+                        api.events.ethereumTokenTransfers.NativeTokenTransferred.is(event)
+                    );
+
+                    const balanceTransferEvents = events.filter(
+                        ({ event }) =>
+                            api.events.balances?.Transfer?.is(event) || api.events.currencies?.Transferred?.is(event)
+                    );
+
+                    // Check sovereign is collecting the amount for each native token transfer event
+                    for (const { event } of tokenTransferEvents) {
+                        const nativeTokenTransferEvent = event.data;
+
+                        const recipientReceived = balanceTransferEvents.some(({ event }) => {
+                            const [from, to, amount] = event.data;
+                            return (
+                                from?.toString() === nativeTokenTransferEvent.source.toString() &&
+                                to?.toString() === sovereignAccount &&
+                                amount?.toString() === nativeTokenTransferEvent.amount.toString()
+                            );
+                        });
+
+                        expect(
+                            recipientReceived,
+                            `Expected transfer to ${sovereignAccount} not found in block ${blockNumber}`
+                        ).toBe(true);
+                    }
+                }
+            },
+        });
+    },
+});
