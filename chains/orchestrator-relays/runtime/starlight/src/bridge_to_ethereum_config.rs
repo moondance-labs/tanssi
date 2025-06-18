542ef52f7f951ef06652c1d5b3005bc9d66a6d96
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-06-18 18:40:59 +0200

    Add to starlight

--- chains/orchestrator-relays/runtime/starlight/src/bridge_to_ethereum_config.rs
+++ chains/orchestrator-relays/runtime/starlight/src/bridge_to_ethereum_config.rs
@@ -168,14 +168,25 @@ where
                 || channel.para_id != channel_info.para_id
                 || channel.agent_id != channel_info.agent_id
             {
+                log::debug!(
+                    "Unexpected channel id: {:?} != {:?}",
+                    (envelope.channel_id, channel.para_id, channel.agent_id),
+                    (
+                        channel_info.channel_id,
+                        channel_info.para_id,
+                        channel_info.agent_id
+                    )
+                );
                 return false;
             }
         } else {
+            log::warn!("CurrentChannelInfo not set in storage");
             return false;
         }
 
         // Check it is from the right gateway
         if envelope.gateway != T::GatewayAddress::get() {
+            log::warn!("Wrong gateway address: {:?}", envelope.gateway);
             return false;
         }
 
@@ -188,18 +199,44 @@ where
                 let token_location = T::TokenLocationReanchored::get();
 
                 if let Some(expected_token_id) = EthereumSystem::convert_back(&token_location) {
-                    return token_id == expected_token_id;
+                    if token_id == expected_token_id {
+                        return true;
+                    } else {
+                        // TODO: ensure this does not warn on container token transfers or other message types, if yes change to debug
+                        log::warn!(
+                            "NativeTokenTransferMessageProcessor: unexpected token_id: {:?}",
+                            token_id
+                        );
+                        return false;
+                    }
+                } else {
+                    log::warn!("NativeTokenTransferMessageProcessor: token id not found for location: {:?}", token_location);
+
+                    return false;
                 }
-                return false;
             }
-            _ => false,
+            Ok(msg) => {
+                log::trace!(
+                    "NativeTokenTransferMessageProcessor: unexpected message: {:?}",
+                    msg
+                );
+                false
+            }
+            Err(e) => {
+                log::trace!("NativeTokenTransferMessageProcessor: failed to decode message. This is expected if the message is not for this processor. Error: {:?}", e);
+                false
+            }
         }
     }
 
     fn process_message(_channel: Channel, envelope: Envelope) -> DispatchResult {
         // - Decode payload as SendNativeToken
         let message = VersionedXcmMessage::decode_all(&mut envelope.payload.as_slice())
-            .map_err(|_| DispatchError::Other("unable to parse the envelope payload"))?;
+            .map_err(|e| {
+                log::trace!("NativeTokenTransferMessageProcessor: failed to decode message. This is expected if the message is not for this processor. Error: {:?}", e);
+
+                DispatchError::Other("unable to parse the envelope payload")
+            })?;
 
         log::trace!("NativeTokenTransferMessageProcessor: {:?}", message);
 
@@ -228,7 +265,13 @@ where
 
                 Ok(())
             }
-            _ => return Err(DispatchError::Other("unexpected message")),
+            msg => {
+                log::trace!(
+                    "NativeTokenTransferMessageProcessor: unexpected message: {:?}",
+                    msg
+                );
+                Err(DispatchError::Other("unexpected message"))
+            }
         }
     }
 }
@@ -304,7 +347,7 @@ impl<T> RewardProcessor<T> for RewardThroughFeesAccount<T>
 where
     T: snowbridge_pallet_inbound_queue::Config + pallet_ethereum_token_transfers::Config,
     T::AccountId: From<sp_runtime::AccountId32>,
-    <T::Token as Inspect<T::AccountId>>::Balance: core::fmt::Display,
+    <T::Token as Inspect<T::AccountId>>::Balance: core::fmt::Debug,
 {
     fn process_reward(who: T::AccountId, _channel: Channel, message: Message) -> DispatchResult {
         let reward_amount = snowbridge_pallet_inbound_queue::Pallet::<T>::calculate_delivery_cost(
@@ -318,7 +361,7 @@ where
                 .min(reward_amount);
         if amount != reward_amount {
             log::warn!(
-                "RewardThroughFeesAccount: fees account running low on funds {}: {}",
+                "RewardThroughFeesAccount: fees account running low on funds {:?}: {:?}",
                 fees_account,
                 amount
             );
