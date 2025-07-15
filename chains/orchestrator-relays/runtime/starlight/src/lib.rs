15ad44abeb1c02016a054b5f34e8986d9e4956b7
Aleksandar Brayanov <aleksandar@moonsonglabs.com>
2025-07-15 22:51:23 +0100

    chore: added missing pallet-inactivity-tracking config and weights to Starlight after merge
    Signed-off-by: Aleksandar Brayanov <aleksandar@moonsonglabs.com>

--- chains/orchestrator-relays/runtime/starlight/src/lib.rs
+++ chains/orchestrator-relays/runtime/starlight/src/lib.rs
@@ -96,7 +96,7 @@ use {
     tp_bridge::ConvertLocation,
     tp_traits::{
         prod_or_fast_parameter_types, EraIndex, GetHostConfiguration, GetSessionContainerChains,
-        ParaIdAssignmentHooks, RegistrarHandler, Slot, SlotFrequency,
+        NodeActivityTrackingHelper, ParaIdAssignmentHooks, RegistrarHandler, Slot, SlotFrequency,
     },
     xcm::Version as XcmVersion,
     xcm_runtime_apis::{
@@ -1876,6 +1876,7 @@ pub struct CandidateHasRegisteredKeys;
 impl IsCandidateEligible<AccountId> for CandidateHasRegisteredKeys {
     fn is_candidate_eligible(a: &AccountId) -> bool {
         <Session as ValidatorRegistration<AccountId>>::is_registered(a)
+            && !InactivityTracking::is_node_offline(a)
     }
     #[cfg(feature = "runtime-benchmarks")]
     fn make_candidate_eligible(a: &AccountId, eligible: bool) {
@@ -1900,6 +1901,10 @@ impl IsCandidateEligible<AccountId> for CandidateHasRegisteredKeys {
             );
         } else {
             let _ = Session::purge_keys(RuntimeOrigin::signed(a.clone()));
+        }
+
+        if InactivityTracking::is_node_offline(a) {
+            InactivityTracking::make_node_online(a);
         }
     }
 }
@@ -1930,7 +1935,6 @@ parameter_types! {
 }
 impl pallet_inactivity_tracking::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
-    type CollatorId = AccountId;
     type MaxInactiveSessions = MaxInactiveSessions;
     type MaxCollatorsPerSession = MaxCandidatesBufferSize;
     type MaxContainerChains = MaxLengthParaIds;
@@ -1938,6 +1942,8 @@ impl pallet_inactivity_tracking::Config for Runtime {
     type CurrentCollatorsFetcher = TanssiCollatorAssignment;
     type GetSelfChainBlockAuthor = ();
     type ParaFilter = tp_parathread_filter_common::ExcludeAllParathreadsFilter<Runtime>;
+    type InvulnerablesFilter = tp_invulnerables_filter_common::InvulnerablesFilter<Runtime>;
+    type CollatorStakeHelper = PooledStaking;
     type WeightInfo = weights::pallet_inactivity_tracking::SubstrateWeight<Runtime>;
 }
 
