1874234cc6dc1e0599b4f9ccccbe0da32bde4cb9
Evgeny Svirsky <e.svirsky@tut.by>
2025-07-22 18:04:45 +0200

    - Fixed smoke for invulnerables

--- test/suites/smoke-test-common-solo/test-invulnerables-priority.ts
+++ test/suites/smoke-test-common-solo/test-invulnerables-priority.ts
@@ -22,7 +22,9 @@ describeSuite({
                 let collators = [];
                 const blockBabeEpochStart = (await api.query.babe.epochStart())[0].toNumber();
                 const apiJustBeforeTheSession = await api.at(await api.rpc.chain.getBlockHash(blockBabeEpochStart - 1));
-                const invulnerables = await apiJustBeforeTheSession.query.tanssiInvulnerables.invulnerables();
+                const invulnerables = (
+                    await apiJustBeforeTheSession.query.tanssiInvulnerables.invulnerables()
+                ).toJSON() as string[];
                 // we need to get current
                 const currentAssignment = await currentBlockApi.query.tanssiCollatorAssignment.collatorContainerChain();
                 // there should be no collator in orchestrator
@@ -45,28 +47,24 @@ describeSuite({
                 if (collators.length <= invulnerables.length) {
                     // Less collators than invulnerables: all collators must be invulnerables
                     for (const collator of collators) {
-                        expect(
-                            invulnerables.toJSON().includes(collator.toString()),
-                            `Collator should be in invulnerable list: ${collator.toString()}`
-                        ).to.be.true;
+                        expect(invulnerables.includes(collator), `Collator should be in invulnerable list: ${collator}`)
+                            .to.be.true;
                     }
                 } else {
                     // More collators than invulnerables: all invulnerables must be collators
                     for (const invulnerable of invulnerables) {
                         expect(
-                            collators.toJSON().includes(invulnerable.toString()),
-                            `Invulnerable should be in collators list: ${invulnerable.toString()}`
+                            collators.includes(invulnerable),
+                            `Invulnerable should be in collators list: ${invulnerable}`
                         ).to.be.true;
                     }
 
                     // Remaining collators must be from staking
-                    const collatorsNotInvulnerables = collators
-                        .toJSON()
-                        .filter((collator) => !invulnerables.toJSON().includes(collator.toString()));
+                    const collatorsNotInvulnerables = collators.filter((collator) => !invulnerables.includes(collator));
                     for (const collator of collatorsNotInvulnerables) {
                         expect(
-                            eligibleCandidates.includes(collator.toString()),
-                            `Collator should be a staking candidate: ${collator.toString()}`
+                            eligibleCandidates.includes(collator),
+                            `Collator should be a staking candidate: ${collator}`
                         ).to.be.true;
                     }
                 }
