6b487f60541c8610fb8648c481d9c97490a6d708
girazoki <gorka.irazoki@gmail.com>
2025-07-23 16:04:05 +0200

    try again

--- test/scripts/bridge/set-env.sh
+++ test/scripts/bridge/set-env.sh
@@ -26,7 +26,7 @@ relay_bin="$relayer_root_dir/build/tanssi-bridge-relayer"
 RELAYER_COMMIT="61773050e66a83139330a579a31b597b3a91faea" # TODO: Change to tag when we do releases
 
 # THIS IS WHERE CONTRACTS SHOULD BE DEPLOYED
-TANSSI_SYMBIOTIC_COMMIT="1aef8e7c3b204dea46dc99b8656d67a1a4a67cee" # TODO: Change to tag when we do release
+TANSSI_SYMBIOTIC_COMMIT="71dfbe0c7b0cfd810190d56b39caef006ef64d41" # TODO: Change to tag when we do release
 GETH_TAG="v1.15.5" # We will need to investigate if this is right
 LODESTAR_TAG="v1.27.0"
 
