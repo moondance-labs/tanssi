0bd3bf0c5bc4b874904bf60ca2406e343da41644
girazoki <gorka.irazoki@gmail.com>
2025-07-29 12:18:49 +0200

    1402 starlight

--- chains/orchestrator-relays/runtime/starlight/src/lib.rs
+++ chains/orchestrator-relays/runtime/starlight/src/lib.rs
@@ -194,7 +194,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: Cow::Borrowed("starlight"),
     impl_name: Cow::Borrowed("tanssi-starlight-v2.0"),
     authoring_version: 0,
-    spec_version: 1401,
+    spec_version: 1402,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 26,
