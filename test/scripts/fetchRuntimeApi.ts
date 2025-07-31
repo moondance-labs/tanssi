bc31cc68bd8b7a76e6901510e13efb9e81129445
girazoki <gorka.irazoki@gmail.com>
2025-07-31 14:54:15 +0200

    compare apis job

---
+++ test/scripts/fetchRuntimeApi.ts
@@ -0,0 +1,87 @@
+/**
+unhash-key
+
+Given a raw storage key, try to find which pallet it belongs to, and which storage item.
+Only works for public storage items (exported with pub).
+
+Key args decoded using chopsticks decode-key from
+
+https://github.com/AcalaNetwork/chopsticks/blob/5fb31092a879c1a1ac712b7b24bd9fa91f0bee53/packages/chopsticks/src/plugins/decode-key/cli.ts#L18
+
+Usage:
+# List all pallet prefixes for a selected chain
+pnpm unhash-key
+# Search for a key in all endpoints
+pnpm unhash-key 0x94eadf0156a8ad5156507773d0471e4a49f6c9aa90c04982c05388649310f22f
+# Search for a key in a specific endpoint
+pnpm unhash-key --url 'wss://services.tanssi-testnet.network/dancelight' 0x94eadf0156a8ad5156507773d0471e4a49f6c9aa90c04982c05388649310f22f
+ */
+
+import yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+import { NETWORK_YARGS_OPTIONS, getApiFor } from "./utils/network";
+import type { ApiPromise } from "@polkadot/api/promise/Api";
+import * as fs from 'fs';
+
+// List of endpoints from which to get pallet metadata.
+// Should contain one endpoint for each network.
+const DEFAULT_ENDPOINTS = [
+    "wss://rpc.polkadot.io",
+    "wss://services.tanssi-testnet.network/dancelight",
+    "wss://services.tanssi-dev.network/stagelight",
+    "wss://services.tanssi-testnet.network/dancebox",
+    "wss://fraa-flashbox-rpc.a.stagenet.tanssi.network",
+    "wss://services.tanssi-dev.network/stagebox",
+    // Relay chains, ideally we should use our relay endpoint
+    "wss://rococo-rpc.polkadot.io",
+    "wss://westend-rpc.polkadot.io",
+    // Frontier template
+    "wss://services.tanssi-testnet.network/dancebox-3001",
+    // TODO: add simple template rpc endpoint
+];
+
+yargs(hideBin(process.argv))
+    .usage("Usage: $0 [output]")
+    .version("1.0.0")
+    .command(
+        "$0 [output]",
+        "List pallets sorted by storage prefix or search for a storage key",
+        (yargs) => {
+            return yargs
+                .positional("output", {
+                    describe: "Output path where to store the json",
+                    required: true
+                })
+                .options({
+                    ...NETWORK_YARGS_OPTIONS,
+                });
+        },
+        async (argv) => {
+            let chosenEndpoint = argv.url;
+            if (!chosenEndpoint) {
+                // No URL provided via args, so display an interactive menu.
+                 const { select } = await import("@inquirer/prompts");
+                    chosenEndpoint = await select({
+                        message: "Select an endpoint to list pallet prefixes (scroll down for more):",
+                        choices: DEFAULT_ENDPOINTS,
+                    });
+                }
+                let api: ApiPromise;
+                try {
+                    api = await getApiFor({ ...argv, url: chosenEndpoint });
+                } catch (err) {
+                    console.error(`Failed to connect to ${chosenEndpoint}:"`, err);
+                    throw err;
+                }
+                try {
+                    const listOfRtApisAsJson = (await api.rpc.state.getRuntimeVersion()).toJSON();
+                    fs.writeFileSync(argv.output, JSON.stringify(listOfRtApisAsJson, null, 2), "utf8");
+                    console.log(listOfRtApisAsJson)
+                } finally {
+                    await api.disconnect();
+                }
+                return;
+            }
+        
+    )
+    .parse();
