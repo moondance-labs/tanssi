0c2c04eb3672028cabe641ba5ce75f90318a8c76
girazoki <gorka.irazoki@gmail.com>
2025-09-10 15:00:49 +0200

    add new test for insufficient weight

--- test/utils/xcm.ts
+++ test/utils/xcm.ts
@@ -287,7 +287,12 @@ export interface XcmFragmentConfig {
         multilocation: MultiLocation;
         fungible: bigint;
     }[];
-    weight_limit?: BN;
+    weight_limit?:
+        | BN
+        | {
+              refTime: BN | number | bigint;
+              proofSize: BN | number | bigint;
+          };
     descend_origin?: string;
     beneficiary?: string;
 }
