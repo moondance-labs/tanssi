090a896d0ffde3bcae6c08dfd5e754a23e0f110e
girazoki <gorka.irazoki@gmail.com>
2025-09-12 12:48:56 +0200

    new failure tests for export message, covering more cases (#1250)

--- test/suites/dev-tanssi-relay/xcm/test-export-expected-errors.ts
+++ test/suites/dev-tanssi-relay/xcm/test-export-expected-errors.ts
@@ -616,9 +616,23 @@ describeSuite({
                     },
                 };
 
+                const containerAsset2001ParentView = {
+                    parents: 0,
+                    interior: {
+                        X2: [
+                            {
+                                Parachain: 2001,
+                            },
+                            {
+                                PalletInstance: 10,
+                            },
+                        ],
+                    },
+                };
+
                 // Register the token of the container-chain
                 const versionedLocation = {
-                    V3: containerAsset2001,
+                    V3: containerAsset2001ParentView,
                 };
 
                 const metadata = {
