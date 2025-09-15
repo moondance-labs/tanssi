231d891e3bbdfc7b11bd1d456508fe5b16ed746f
girazoki <gorka.irazoki@gmail.com>
2025-09-15 15:54:51 +0200

    hopefully make tests work again

--- scripts/bump-runtime-versions.sh
+++ scripts/bump-runtime-versions.sh
@@ -79,6 +79,7 @@ update_cargo_toml "chains/orchestrator-relays/node/tanssi-relay/Cargo.toml" "$CL
 update_cargo_toml "chains/orchestrator-relays/node/tanssi-relay-service/Cargo.toml" "$CLIENT_VERSION"
 update_cargo_toml "client/service-orchestrator-chain/Cargo.toml" "$CLIENT_VERSION"
 update_cargo_toml "client/service-container-chain-rpc-provider/Cargo.toml" "$CLIENT_VERSION"
+update_cargo_toml "client/service-container-chain-spawner/Cargo.toml" "$CLIENT_VERSION"
 update_cargo_toml "client/node-common/Cargo.toml" "$CLIENT_VERSION"
 
 # Update lib.rs files
