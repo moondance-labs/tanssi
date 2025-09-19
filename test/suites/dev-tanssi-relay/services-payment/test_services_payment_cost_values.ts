cac51ca158824b5a689c755b41042ff16ee2699b
girazoki <gorka.irazoki@gmail.com>
2025-09-19 10:49:05 +0200

    have a specific dev test only for values

---
+++ test/suites/dev-tanssi-relay/services-payment/test_services_payment_cost_values.ts
@@ -0,0 +1,33 @@
+import "@tanssi/api-augment";
+
+import { beforeAll, customDevRpcRequest, describeSuite, expect } from "@moonwall/cli";
+import type { ApiPromise } from "@polkadot/api";
+import { isStarlightRuntime } from "../../../utils/runtime.ts";
+
+describeSuite({
+    id: "DEVT1204",
+    title: "Services payment RPC",
+    foundationMethods: "dev",
+    testCases: ({ it, context }) => {
+        let polkadotJs: ApiPromise;
+        beforeAll(async () => {
+            polkadotJs = context.polkadotJs();
+        });
+        it({
+            id: "E01",
+            title: "Services payment RPC",
+            test: async () => {
+                const isStarlight = isStarlightRuntime(polkadotJs);
+                const costPerBlock = isStarlight ? 30_000_000_000n : 1_000_000n;
+                const costPerSession = isStarlight ? 50_000_000_000_000n : 100_000_000n;
+
+                const chainBlockCost = BigInt((await polkadotJs.call.servicesPaymentApi.blockCost(1000)).toString());
+                const chainSessionCost = BigInt(
+                    (await polkadotJs.call.servicesPaymentApi.collatorAssignmentCost(1000)).toString()
+                );
+                expect(costPerBlock).eq(chainBlockCost);
+                expect(chainSessionCost).eq(costPerSession);
+            },
+        });
+    },
+});
