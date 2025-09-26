7b45738a5c4cbc2e8a4f497dc07f1df962f6d164
Aleksandar Brayanov <aleksandar@moonsonglabs.com>
2025-09-26 11:33:26 +0100

    fix: returning false instead of throwing error in checkIfErrorIsEmitted helper function
    Signed-off-by: Aleksandar Brayanov <aleksandar@moonsonglabs.com>

--- test/utils/events.ts
+++ test/utils/events.ts
@@ -1,5 +1,5 @@
 import type { ApiPromise } from "@polkadot/api";
-import { type BlockCreationResponse, expect } from "@moonwall/cli";
+import type { BlockCreationResponse } from "@moonwall/cli";
 import { BN } from "@polkadot/util";
 import type { ApiTypes, SubmittableExtrinsic } from "@polkadot/api/types";
 
@@ -25,9 +25,21 @@ export async function checkIfErrorIsEmitted(
     const metadata = await api.rpc.state.getMetadata();
     const palletIndex = metadata.asLatest.pallets.find(({ name }) => name.toString() === moduleName).index.toString();
 
-    const errorData = failedBlock.result.events.find((e) => e.event.method === "ExtrinsicFailed").event.toHuman()
-        .data as unknown as ExtrinsicFailedEventDataType;
-    expect(errorData.dispatchError.Module.index).toEqual(palletIndex);
+    const failedBlockResult = failedBlock.result;
+    if (failedBlockResult === undefined) {
+        return false;
+    }
+
+    const extrinsicFailedEvent = failedBlockResult.events.find((e) => e.event.method === "ExtrinsicFailed");
+    if (extrinsicFailedEvent === undefined) {
+        return false;
+    }
+
+    const errorData = extrinsicFailedEvent.event.toHuman().data as unknown as ExtrinsicFailedEventDataType;
+
+    if (errorData.dispatchError.Module.index !== palletIndex) {
+        return false;
+    }
 
     const errorBytes = Uint8Array.from(Buffer.from(errorData.dispatchError.Module.error.slice(2), "hex"));
     const errorIndex = errorBytes[0];
