f6f5b0fe8f732d4037197607413c5c2bb6909ac4
girazoki <gorka.irazoki@gmail.com>
2025-10-07 10:35:49 +0200

    fix test

--- chains/integration-tests/xcm/emulated/tests/assets/dancelight/src/tests/xcm_message_exporter.rs
+++ chains/integration-tests/xcm/emulated/tests/assets/dancelight/src/tests/xcm_message_exporter.rs
@@ -27,6 +27,10 @@ use {
 
 #[test]
 fn test_message_exporter_disabled_for_origin_account() {
+    use sp_tracing::{
+        test_log_capture::init_log_capture,
+        tracing::{subscriber, Level},
+    };
     Dancelight::execute_with(|| {
         // The only test we can do is with signed runtime origins since we are ensuring local origin in xcm config
         let origin = <Dancelight as Chain>::RuntimeOrigin::signed(AccountId::from(
@@ -34,20 +38,28 @@ fn test_message_exporter_disabled_for_origin_account() {
         ));
 
         let message = Xcm(vec![Instruction::ExportMessage {
-            network: NetworkId::Ethereum { chain_id: 1 },
+            network: NetworkId::Ethereum { chain_id: 11155111 },
             destination: Junctions::Here,
             xcm: Xcm(vec![]),
         }]);
 
-        assert_eq!(
-            <Dancelight as DancelightRelayPallet>::XcmPallet::execute(
-                origin,
-                Box::new(VersionedXcm::V5(message)),
-                Weight::from_parts(0, 0)
-            )
-            .unwrap_err()
-            .error,
-            DispatchError::from(Error::<<Dancelight as Chain>::Runtime>::LocalExecutionIncomplete)
-        );
+        let (log_capture, subscriber) = init_log_capture(Level::ERROR, true);
+        // this should inner fail with unroutable
+        subscriber::with_default(subscriber, || {
+            assert_eq!(
+                <Dancelight as DancelightRelayPallet>::XcmPallet::execute(
+                    origin,
+                    Box::new(VersionedXcm::V5(message)),
+                    Weight::from_parts(5_000_000_000, 1_000_000)
+                )
+                .unwrap_err()
+                .error,
+                DispatchError::from(
+                    Error::<<Dancelight as Chain>::Runtime>::LocalExecutionIncomplete
+                )
+            );
+            assert!(log_capture.contains("could not get parachain id from universal source"));
+            assert!(log_capture.contains("XCM execution failed with error error=Unroutable"));
+        });
     });
 }
