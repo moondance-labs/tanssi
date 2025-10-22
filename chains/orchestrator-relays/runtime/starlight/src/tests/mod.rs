e4b6a65a66f8ef92ab55b5db1c2dad5ef47af1c8
Evgeny Svirsky <e.svirsky@tut.by>
2025-10-22 17:10:14 +0200

    External validator rewards tests extracted to the separate file + added 1 more case.

--- chains/orchestrator-relays/runtime/starlight/src/tests/mod.rs
+++ chains/orchestrator-relays/runtime/starlight/src/tests/mod.rs
@@ -27,6 +27,7 @@ mod collator_assignment_tests;
 mod common;
 mod core_scheduling_tests;
 mod ethereum_token_transfers;
+mod external_validator_rewards_tests;
 mod external_validators_tests;
 mod inactivity_tracking;
 mod inbound_queue_tests;
