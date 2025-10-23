75d0d4a0e7d61cad069ca9259e0fa8bfc905b683
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-10-23 18:22:47 +0200

    Fix ensure_successful_delivery

--- pallets/xcm-core-buyer/src/benchmarks.rs
+++ pallets/xcm-core-buyer/src/benchmarks.rs
@@ -51,7 +51,7 @@ mod benchmarks {
     #[benchmark]
     fn buy_core() {
         let caller: T::AccountId = whitelisted_caller();
-        T::XcmSender::ensure_successful_delivery(Location::parent());
+        T::XcmSender::ensure_successful_delivery(Some(Location::parent()));
         assert_ok!(Pallet::<T>::set_relay_xcm_weight_config(
             RawOrigin::Root.into(),
             Some(RelayXcmWeightConfigInner {
@@ -106,7 +106,7 @@ mod benchmarks {
 
     #[benchmark]
     fn force_buy_core() {
-        T::XcmSender::ensure_successful_delivery(Location::parent());
+        T::XcmSender::ensure_successful_delivery(Some(Location::parent()));
         assert_ok!(Pallet::<T>::set_relay_xcm_weight_config(
             RawOrigin::Root.into(),
             Some(RelayXcmWeightConfigInner {
