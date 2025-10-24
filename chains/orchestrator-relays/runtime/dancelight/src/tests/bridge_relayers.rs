4c87b8e1b5029f834148a526c80c594a844907bf
Evgeny Svirsky <e.svirsky@tut.by>
2025-10-24 10:42:55 +0200

    - Added more cases for Bridge Relayer Module

--- chains/orchestrator-relays/runtime/dancelight/src/tests/bridge_relayers.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/tests/bridge_relayers.rs
@@ -17,8 +17,10 @@
 #![cfg(test)]
 
 use {
-    crate::bridge_to_ethereum_config::BridgeReward,
-    crate::{tests::common::*, BridgeRelayers},
+    crate::{
+        bridge_to_ethereum_config::BridgeReward, filter_events, tests::common::*, BridgeRelayers,
+        RuntimeEvent, SnowbridgeFeesAccount,
+    },
     alloc::vec,
     frame_support::{assert_noop, assert_ok},
 };
@@ -47,5 +49,137 @@ fn test_register_new_relayer() {
                 BridgeRelayers::claim_rewards(relayer_origin, BridgeReward::Snowbridge),
                 pallet_bridge_relayers::Error::<Runtime>::NoRewardForRelayer,
             );
+        });
+}
+
+#[test]
+fn relayer_can_claim_reward() {
+    ExtBuilder::default()
+        .with_balances(vec![
+            // Alice gets 10k extra tokens for her mapping deposit
+            (AccountId::from(ALICE), 210_000 * UNIT),
+            (AccountId::from(BOB), 100_000 * UNIT),
+            (AccountId::from(CHARLIE), 100_000 * UNIT),
+            (AccountId::from(DAVE), 100_000 * UNIT),
+            (SnowbridgeFeesAccount::get(), 100_000 * UNIT),
+        ])
+        .build()
+        .execute_with(|| {
+            run_to_block(2);
+
+            let relayer_origin =
+                <Runtime as frame_system::Config>::RuntimeOrigin::signed(AccountId::from(BOB));
+
+            assert_ok!(BridgeRelayers::register(relayer_origin.clone(), 100));
+            let reward_params = BridgeReward::Snowbridge;
+
+            pallet_bridge_relayers::RelayerRewards::<Runtime>::insert(
+                AccountId::from(BOB),
+                reward_params.clone(),
+                100,
+            );
+            assert_ok!(BridgeRelayers::claim_rewards(
+                RuntimeOrigin::signed(AccountId::from(BOB)),
+                reward_params,
+            ));
+
+            assert_eq!(
+                filter_events!(RuntimeEvent::BridgeRelayers(
+                    pallet_bridge_relayers::Event::RewardPaid { .. },
+                ))
+                .count(),
+                1,
+                "RewardPaid event should be emitted!"
+            );
+        });
+}
+
+#[test]
+fn relayer_register_doesnt_withdraw_from_rewards_account() {
+    ExtBuilder::default()
+        .with_balances(vec![
+            // Alice gets 10k extra tokens for her mapping deposit
+            (AccountId::from(ALICE), 210_000 * UNIT),
+            (AccountId::from(BOB), 100_000 * UNIT),
+            (AccountId::from(CHARLIE), 100_000 * UNIT),
+            (AccountId::from(DAVE), 100_000 * UNIT),
+        ])
+        .build()
+        .execute_with(|| {
+            run_to_block(2);
+
+            let balance_before = System::account(AccountId::from(BOB)).data.free;
+
+            let relayer_origin =
+                <Runtime as frame_system::Config>::RuntimeOrigin::signed(AccountId::from(BOB));
+
+            assert_ok!(BridgeRelayers::register(relayer_origin.clone(), 100));
+
+            assert_eq!(
+                filter_events!(RuntimeEvent::BridgeRelayers(
+                    pallet_bridge_relayers::Event::RegistrationUpdated { .. },
+                ))
+                .count(),
+                1,
+                "BridgeRelayers event should be emitted!"
+            );
+
+            let balance_after = System::account(AccountId::from(BOB)).data.free;
+
+            assert_eq!(balance_before, balance_after);
+        });
+}
+
+#[test]
+fn relayer_deregister_is_working() {
+    ExtBuilder::default()
+        .with_balances(vec![
+            // Alice gets 10k extra tokens for her mapping deposit
+            (AccountId::from(ALICE), 210_000 * UNIT),
+            (AccountId::from(BOB), 100_000 * UNIT),
+            (AccountId::from(CHARLIE), 100_000 * UNIT),
+            (AccountId::from(DAVE), 100_000 * UNIT),
+        ])
+        .build()
+        .execute_with(|| {
+            run_to_block(2);
+
+            let balance_before = System::account(AccountId::from(BOB)).data.free;
+
+            let relayer_origin =
+                <Runtime as frame_system::Config>::RuntimeOrigin::signed(AccountId::from(BOB));
+
+            let blocks_for_registration_to_be_active = 100;
+            assert_ok!(BridgeRelayers::register(
+                relayer_origin.clone(),
+                blocks_for_registration_to_be_active.clone()
+            ));
+
+            assert_eq!(
+                filter_events!(RuntimeEvent::BridgeRelayers(
+                    pallet_bridge_relayers::Event::RegistrationUpdated { .. },
+                ))
+                .count(),
+                1,
+                "BridgeRelayers event should be emitted!"
+            );
+
+            // Skip some blocks to make sure registration is not active anymore
+            run_to_block(blocks_for_registration_to_be_active + 5);
+
+            assert_ok!(BridgeRelayers::deregister(relayer_origin.clone()));
+
+            assert_eq!(
+                filter_events!(RuntimeEvent::BridgeRelayers(
+                    pallet_bridge_relayers::Event::Deregistered { .. },
+                ))
+                .count(),
+                1,
+                "Deregistered event should be emitted!"
+            );
+
+            let balance_after = System::account(AccountId::from(BOB)).data.free;
+
+            assert_eq!(balance_before, balance_after);
         });
 }
