9addb334451b85d578ddf5e32b25058c9540e874
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-11-19 15:52:37 +0100

    Check node logs

--- test/scripts/build-spec-dancelight-no-bootnodes.sh
+++ test/scripts/build-spec-dancelight-no-bootnodes.sh
@@ -13,10 +13,10 @@ else
 fi
 
 mkdir -p specs
-$BINARY_FOLDER/container-chain-simple-node build-spec --disable-default-bootnode --parachain-id 2000 --raw > specs/single-container-template-container-2000.json
-$BINARY_FOLDER/container-chain-frontier-node build-spec --disable-default-bootnode --parachain-id 2001 --raw > specs/single-container-template-container-2001.json
-$BINARY_FOLDER/container-chain-simple-node build-spec --disable-default-bootnode --parachain-id 2002 --raw > specs/single-container-template-container-2002.json
-$BINARY_FOLDER/tanssi-relay build-spec --chain dancelight-local --add-container-chain specs/single-container-template-container-2000.json --add-container-chain specs/single-container-template-container-2001.json --invulnerable "Collator-01" --invulnerable "Collator-02" --invulnerable "Collator-03" --invulnerable "Collator-04" --invulnerable "Collator-05" --invulnerable "Collator-06" > specs/tanssi-relay.json
+$BINARY_FOLDER/container-chain-simple-node build-spec --disable-default-bootnode --parachain-id 2000 --raw > specs/single-container-template-container-2000-no-bootnodes.json
+$BINARY_FOLDER/container-chain-frontier-node build-spec --disable-default-bootnode --parachain-id 2001 --raw > specs/single-container-template-container-2001-no-bootnodes.json
+$BINARY_FOLDER/container-chain-simple-node build-spec --disable-default-bootnode --parachain-id 2002 --raw > specs/single-container-template-container-2002-no-bootnodes.json
+$BINARY_FOLDER/tanssi-relay build-spec --chain dancelight-local --add-container-chain specs/single-container-template-container-2000-no-bootnodes.json --add-container-chain specs/single-container-template-container-2001-no-bootnodes.json --invulnerable "Collator-01" --invulnerable "Collator-02" --invulnerable "Collator-03" --invulnerable "Collator-04" --invulnerable "Collator-05" --invulnerable "Collator-06" > specs/tanssi-relay-no-bootnodes.json
 
 # Also need to build the genesis-state to be able to register the container 2002 later
-$BINARY_FOLDER/container-chain-simple-node export-genesis-state --chain specs/single-container-template-container-2002.json specs/para-2002-genesis-state
+$BINARY_FOLDER/container-chain-simple-node export-genesis-state --chain specs/single-container-template-container-2002-no-bootnodes.json specs/para-2002-genesis-state
