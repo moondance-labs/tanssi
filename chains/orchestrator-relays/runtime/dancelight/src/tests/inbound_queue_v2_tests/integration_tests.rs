c0c088167eeecd105f28185514612f2bcda55ab0
Evgeny Svirsky <e.svirsky@tut.by>
2025-11-21 11:52:02 +0100

    - added assets trapped test

--- chains/orchestrator-relays/runtime/dancelight/src/tests/inbound_queue_v2_tests/integration_tests.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/tests/inbound_queue_v2_tests/integration_tests.rs
@@ -619,3 +619,238 @@ fn test_inbound_queue_transfer_erc20_works() {
         assert!(found_issued, "Issued event for ETH not found");
     });
 }
+
+#[test]
+fn test_inbound_queue_tanssi_assets_trapped_incorrect_xcm_works() {
+    ExtBuilder::default()
+        .with_validators(
+            vec![]
+        )
+        .with_external_validators(
+            vec![
+                (AccountId::from(ALICE), 210 * UNIT),
+                (AccountId::from(BOB), 100 * UNIT),
+            ]
+        ).build().execute_with(|| {
+        let nonce = 1;
+
+        let dummy_proof = mock_snowbridge_message_proof();
+        let erc20_token_id = hex_literal::hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
+
+        let erc20_asset_location = Location {
+            parents: 1,
+            interior: X2([
+                GlobalConsensus(Ethereum { chain_id: 11155111 }),
+                AccountKey20 {
+                    network: Some(Ethereum { chain_id: 11155111 }),
+                    key: erc20_token_id,
+                }
+            ]
+                .into()),
+        };
+
+        let asset_id = 43u16;
+
+        assert_ok!(ForeignAssetsCreator::create_foreign_asset(
+            root_origin(),
+            erc20_asset_location.clone(),
+            asset_id,
+            AccountId::from(ALICE),
+            true,
+            1
+        ));
+
+        let token_value = 123_456_000u128;
+        let assets = vec![
+            NativeTokenERC20 { token_id: erc20_token_id.into(), value: token_value },
+        ];
+
+        println!("assets: {:?}", assets);
+
+        let _execution_fee = 0;
+
+        let instructions = vec![];
+
+        let xcm: Xcm<()> = instructions.into();
+        let versioned_message_xcm = VersionedXcm::V5(xcm);
+
+        let xcm_bytes = RawPayload::Xcm(versioned_message_xcm.encode());
+
+        let encoded_data = encode_message_to_eth_payload(&Message {
+            gateway: H160::random(),
+            nonce,
+            origin: EthereumGatewayAddress::get(),
+            assets,
+            payload: Payload::Raw(xcm_bytes.encode()),
+            claimer: None,
+            value: 0,
+            execution_fee: 0,
+            relayer_fee: 0,
+        });
+
+        assert_eq!(EthereumInboundQueueV2::submit(OriginFor::<Runtime>::signed(AccountId::new([0; 32])), Box::new(EventProof {
+            event_log: Log {
+                address: <Runtime as snowbridge_pallet_inbound_queue::Config>::GatewayAddress::get(),
+                topics: vec![hex!("550e2067494b1736ea5573f2d19cdc0ac95b410fff161bf16f11c6229655ec9c").into()],
+                data: encoded_data,
+            },
+            proof: dummy_proof.clone(),
+        })), Ok(()));
+
+        let events = frame_system::Pallet::<Runtime>::events();
+        for e in events.clone() {
+            println!("Event: {:?}", e.event);
+        }
+
+        let mut found_message = false;
+        let mut found_trapped = false;
+
+        for record in events {
+            match &record.event {
+                RuntimeEvent::EthereumInboundQueueV2(
+                    snowbridge_pallet_inbound_queue_v2::Event::MessageReceived { nonce, .. }
+                ) if *nonce == 1 => {
+                    found_message = true;
+                }
+
+                RuntimeEvent::XcmPallet(
+                    pallet_xcm::Event::AssetsTrapped { assets, .. }
+                ) => {
+                    let Ok(assets) = <Assets as TryFrom<_>>::try_from(assets.clone()) else {
+                        panic!("Unsupported assets version");
+                    };
+
+                    if let Some(asset) = assets.get(0) {
+                        if let Fungibility::Fungible(f) = asset.fun {
+                            if f == token_value {
+                                found_trapped = true;
+                            }
+                        }
+                    }
+                }
+
+                _ => {}
+            }
+        }
+
+        assert!(found_message, "MessageReceived event not found");
+        assert!(found_trapped, "AssetsTrapped event not found");
+    });
+}
+
+#[test]
+fn test_inbound_queue_erc20_assets_trapped_incorrect_xcm_works() {
+    ExtBuilder::default()
+        .with_validators(
+            vec![]
+        )
+        .with_external_validators(
+            vec![
+                (AccountId::from(ALICE), 210 * UNIT),
+                (AccountId::from(BOB), 100 * UNIT),
+            ]
+        ).build().execute_with(|| {
+        let nonce = 1;
+
+        let dummy_proof = mock_snowbridge_message_proof();
+        let erc20_token_id = hex_literal::hex!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2");
+
+        let erc20_asset_location = Location {
+            parents: 1,
+            interior: X2([
+                GlobalConsensus(Ethereum { chain_id: 11155111 }),
+                AccountKey20 {
+                    network: Some(Ethereum { chain_id: 11155111 }),
+                    key: erc20_token_id,
+                }
+            ]
+                .into()),
+        };
+
+        let asset_id = 43u16;
+
+        assert_ok!(ForeignAssetsCreator::create_foreign_asset(
+            root_origin(),
+            erc20_asset_location.clone(),
+            asset_id,
+            AccountId::from(ALICE),
+            true,
+            1
+        ));
+
+        let assets = vec![
+        ];
+
+        println!("assets: {:?}", assets);
+
+        let _execution_fee = 0;
+
+        let instructions = vec![];
+
+        let xcm: Xcm<()> = instructions.into();
+        let versioned_message_xcm = VersionedXcm::V5(xcm);
+
+        let xcm_bytes = RawPayload::Xcm(versioned_message_xcm.encode());
+
+        let eth_amount = 789_000_000u128;
+        let encoded_data = encode_message_to_eth_payload(&Message {
+            gateway: H160::random(),
+            nonce,
+            origin: EthereumGatewayAddress::get(),
+            assets,
+            payload: Payload::Raw(xcm_bytes.encode()),
+            claimer: None,
+            value: eth_amount,
+            execution_fee: 0,
+            relayer_fee: 0,
+        });
+
+        assert_eq!(EthereumInboundQueueV2::submit(OriginFor::<Runtime>::signed(AccountId::new([0; 32])), Box::new(EventProof {
+            event_log: Log {
+                address: <Runtime as snowbridge_pallet_inbound_queue::Config>::GatewayAddress::get(),
+                topics: vec![hex!("550e2067494b1736ea5573f2d19cdc0ac95b410fff161bf16f11c6229655ec9c").into()],
+                data: encoded_data,
+            },
+            proof: dummy_proof.clone(),
+        })), Ok(()));
+
+        let events = frame_system::Pallet::<Runtime>::events();
+        for e in events.clone() {
+            println!("Event: {:?}", e.event);
+        }
+
+        let mut found_message = false;
+        let mut found_trapped = false;
+
+        for record in events {
+            match &record.event {
+                RuntimeEvent::EthereumInboundQueueV2(
+                    snowbridge_pallet_inbound_queue_v2::Event::MessageReceived { nonce, .. }
+                ) if *nonce == 1 => {
+                    found_message = true;
+                }
+
+                RuntimeEvent::XcmPallet(
+                    pallet_xcm::Event::AssetsTrapped { assets, .. }
+                ) => {
+                    let Ok(assets) = <Assets as TryFrom<_>>::try_from(assets.clone()) else {
+                        panic!("Unsupported assets version");
+                    };
+
+                    if let Some(asset) = assets.get(0) {
+                        if let Fungibility::Fungible(f) = asset.fun {
+                            if f == eth_amount {
+                                found_trapped = true;
+                            }
+                        }
+                    }
+                }
+
+                _ => {}
+            }
+        }
+
+        assert!(found_message, "MessageReceived event not found");
+        assert!(found_trapped, "AssetsTrapped event not found");
+    });
+}
