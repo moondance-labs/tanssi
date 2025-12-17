10c8a64018d735bcf90c9f1ca8a764baae1c4689
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-12-17 14:07:35 +0100

    inflation rewards

--- pallets/inflation-rewards/src/lib.rs
+++ pallets/inflation-rewards/src/lib.rs
@@ -278,6 +278,9 @@ pub mod pallet {
 // Any additional check should be done in the calling function
 impl<T: Config> AuthorNotingHook<T::AccountId> for Pallet<T> {
     fn on_container_authors_noted(info: &[AuthorNotingInfo<T::AccountId>]) -> Weight {
+        if info.is_empty() {
+            return Weight::zero();
+        }
         let mut total_weight = T::DbWeight::get().reads_writes(1, 0);
         // We take chains to reward, to see what containers are left to reward
         if let Some(mut container_chains_to_reward) = ChainsToReward::<T>::get() {
@@ -343,9 +346,30 @@ impl<T: Config> AuthorNotingHook<T::AccountId> for Pallet<T> {
         )
         .expect("to mint tokens");
 
+        // TODO: this API doesn't make sense, we want to add a para id to the list of chains to
+        // reward. So change `prepare_worst_case_for_bench` into something that takes a list
+        let old_para_ids: alloc::vec::Vec<_> = ChainsToReward::<T>::get()
+            .map(|x| x.para_ids.into_iter().collect())
+            .unwrap_or_default();
         ChainsToReward::<T>::put(ChainsToRewardValue {
-            para_ids: alloc::vec![para_id].try_into().expect("to be in bound"),
+            para_ids: alloc::vec::Vec::from_iter(
+                old_para_ids.into_iter().chain([para_id]),
+            )
+            .try_into()
+            .expect("to be in bound"),
             rewards_per_chain: BalanceOf::<T>::from(reward_amount),
         });
+
+        T::StakingRewardsDistributor::prepare_worst_case_for_bench(a);
+    }
+
+    #[cfg(feature = "runtime-benchmarks")]
+    fn bench_advance_block() {
+        T::StakingRewardsDistributor::bench_advance_block()
+    }
+
+    #[cfg(feature = "runtime-benchmarks")]
+    fn bench_execute_pending() {
+        T::StakingRewardsDistributor::bench_execute_pending()
     }
 }
