31b54f9fc27833c6fc4e14bb20c2759ecd828ef5
Evgeny Svirsky <e.svirsky@tut.by>
2025-12-18 15:50:15 +0100

    - added consistent user message for condition

--- test/scripts/build-spec-light-single.sh
+++ test/scripts/build-spec-light-single.sh
@@ -31,6 +31,7 @@ mkdir -p specs
 # IF and Fallback will be removed after merge in the next PR
 if bash ./check-export-chain-spec-cmd.sh $BINARY_FOLDER/tanssi-relay | grep -q "export-chain-spec"; then
   $BINARY_FOLDER/tanssi-relay export-chain-spec --chain "${RUNTIME}-local" > "specs/tanssi-relay.json"
+  echo "Spec for $RUNTIME saved to specs/tanssi-relay.json"
   exit 0
 fi
 $BINARY_FOLDER/tanssi-relay build-spec --chain "${RUNTIME}-local" > "specs/tanssi-relay.json"
