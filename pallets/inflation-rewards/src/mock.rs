dc948458e74506f6256765b9c990738a69038903
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2025-12-18 17:34:01 +0100

    weird import merge conflict

--- pallets/inflation-rewards/src/mock.rs
+++ pallets/inflation-rewards/src/mock.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
 
-use std::collections::BTreeSet;
-use tp_traits::ForSession;
 use {
     crate::{self as pallet_inflation_rewards, MaybeSelfChainBlockAuthor},
     bounded_collections::bounded_vec,
