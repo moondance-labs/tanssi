baf99cb22b104b7912c69fdad7bc3920e6e5cc5a
girazoki <gorka.irazoki@gmail.com>
2025-12-18 12:45:44 +0100

    ts-api

--- typescript-api/src/starlight/interfaces/augment-api-consts.ts
+++ typescript-api/src/starlight/interfaces/augment-api-consts.ts
@@ -548,6 +548,16 @@ declare module "@polkadot/api-base/types/consts" {
              **/
             [key: string]: Codec;
         };
+        openTechCommitteeCollective: {
+            /**
+             * The maximum weight of a dispatch call that can be proposed and executed.
+             **/
+            maxProposalWeight: SpWeightsWeightV2Weight & AugmentedConst<ApiType>;
+            /**
+             * Generic const
+             **/
+            [key: string]: Codec;
+        };
         paras: {
             unsignedPriority: u64 & AugmentedConst<ApiType>;
             /**
