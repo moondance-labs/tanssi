d336b432d92c0fe845e39d6bd5746da1766fbb03
Parth Desai <desaiparth08@gmail.com>
2026-01-13 10:03:20 +0400

    add max xcm weight config in message processors

--- chains/orchestrator-relays/runtime/dancelight/src/tests/processors/v2/symbiotic_message_processor.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/tests/processors/v2/symbiotic_message_processor.rs
@@ -14,6 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Tanssi.  If not, see <http://www.gnu.org/licenses/>
 
+use crate::bridge_to_ethereum_config::MaxXcmWeight;
 use {
     super::ALICE,
     crate as dancelight_runtime,
@@ -68,6 +69,7 @@ fn symbiotic_try_extract_message_fails_with_invalid_symbiotic_payload() {
             TanssiUniversalLocation,
             xcm_executor::XcmExecutor<xcm_config::XcmConfig>,
             <xcm_config::XcmConfig as xcm_executor::Config>::Weigher,
+            MaxXcmWeight,
         >;
 
         let result = <Processor as MessageProcessorWithFallback<AccountId>>::try_extract_message(
@@ -111,6 +113,7 @@ fn symbiotic_try_extract_message_fails_with_invalid_origin_payload() {
             TanssiUniversalLocation,
             xcm_executor::XcmExecutor<xcm_config::XcmConfig>,
             <xcm_config::XcmConfig as xcm_executor::Config>::Weigher,
+            MaxXcmWeight,
         >;
 
         let result = <Processor as MessageProcessorWithFallback<AccountId>>::try_extract_message(
