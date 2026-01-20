19c5f0c1f95e5b97a8a70a288ea316bbc38ffe59
Evgeny Svirsky <e.svirsky@tut.by>
2026-01-20 10:52:22 +0100

    - fmt

--- chains/orchestrator-relays/runtime/dancelight/src/lib.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/lib.rs
@@ -448,7 +448,7 @@ impl EnsureOriginWithArg<RuntimeOrigin, RuntimeParametersKey> for DynamicParamet
         match key {
             Preimage(_) => frame_system::ensure_root(origin.clone()),
         }
-            .map_err(|_| origin)
+        .map_err(|_| origin)
     }
 
     #[cfg(feature = "runtime-benchmarks")]
@@ -510,7 +510,7 @@ impl pallet_babe::Config for Runtime {
     type MaxNominators = ConstU32<0>;
     type KeyOwnerProof = sp_session::MembershipProof;
     type EquivocationReportSystem =
-    pallet_babe::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+        pallet_babe::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
 }
 
 parameter_types! {
@@ -546,7 +546,7 @@ parameter_types! {
 impl pallet_transaction_payment::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type OnChargeTransaction =
-    FungibleAdapter<Balances, tanssi_runtime_common::DealWithFees<Runtime>>;
+        FungibleAdapter<Balances, tanssi_runtime_common::DealWithFees<Runtime>>;
     type OperationalFeeMultiplier = OperationalFeeMultiplier;
     type WeightToFee = WeightToFee;
     type LengthToFee = ConstantMultiplier<Balance, TransactionByteFee>;
@@ -736,7 +736,7 @@ impl pallet_grandpa::Config for Runtime {
     type MaxSetIdSessionEntries = MaxSetIdSessionEntries;
     type KeyOwnerProof = sp_session::MembershipProof;
     type EquivocationReportSystem =
-    pallet_grandpa::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+        pallet_grandpa::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
 }
 
 /// Submits a transaction with the node's public and signature type. Adheres to the signed extension
@@ -1062,7 +1062,7 @@ impl parachains_session_info::Config for Runtime {
 }
 
 pub type RewardValidators =
-pallet_external_validators_rewards::RewardValidatorsWithEraPoints<Runtime>;
+    pallet_external_validators_rewards::RewardValidatorsWithEraPoints<Runtime>;
 
 impl parachains_inclusion::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
@@ -1162,7 +1162,7 @@ impl pallet_message_queue::Config for Runtime {
     type MessageProcessor = MessageProcessor;
     #[cfg(feature = "runtime-benchmarks")]
     type MessageProcessor =
-    pallet_message_queue::mock_helpers::NoopMessageProcessor<TanssiAggregateMessageOrigin>;
+        pallet_message_queue::mock_helpers::NoopMessageProcessor<TanssiAggregateMessageOrigin>;
     type QueueChangeHandler = OnQueueChangedWrapper<
         TanssiAggregateMessageOrigin,
         ParaInclusionAggregateMessageOrigin,
@@ -1184,10 +1184,10 @@ impl parachains_hrmp::Config for Runtime {
     type ChannelManager = EnsureRoot<AccountId>;
     type Currency = Balances;
     type DefaultChannelSizeAndCapacityWithSystem =
-    parachains_configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio<
-        Runtime,
-        HrmpChannelSizeAndCapacityWithSystemRatio,
-    >;
+        parachains_configuration::ActiveConfigHrmpChannelSizeAndCapacityRatio<
+            Runtime,
+            HrmpChannelSizeAndCapacityWithSystemRatio,
+        >;
     type WeightInfo = weights::runtime_parachains_hrmp::SubstrateWeight<Runtime>;
     type VersionWrapper = XcmPallet;
 }
@@ -1204,7 +1204,7 @@ impl parachains_scheduler::Config for Runtime {
 
 pub struct CollatorAssignmentProvider;
 impl parachains_scheduler::common::AssignmentProvider<BlockNumberFor<Runtime>>
-for CollatorAssignmentProvider
+    for CollatorAssignmentProvider
 {
     fn pop_assignment_for_core(core_idx: CoreIndex) -> Option<Assignment> {
         let assigned_collators = TanssiCollatorAssignment::collator_container_chain();
@@ -1340,7 +1340,7 @@ impl parachains_disputes::Config for Runtime {
 impl parachains_slashing::Config for Runtime {
     type KeyOwnerProofSystem = Historical;
     type KeyOwnerProof =
-    <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, ValidatorId)>>::Proof;
+        <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(KeyTypeId, ValidatorId)>>::Proof;
     type KeyOwnerIdentification = <Self::KeyOwnerProofSystem as KeyOwnerProofSystem<(
         KeyTypeId,
         ValidatorId,
@@ -1396,7 +1396,7 @@ impl pallet_beefy::Config for Runtime {
     type WeightInfo = ();
     type KeyOwnerProof = <Historical as KeyOwnerProofSystem<(KeyTypeId, BeefyId)>>::Proof;
     type EquivocationReportSystem =
-    pallet_beefy::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
+        pallet_beefy::EquivocationReportSystem<Self, Offences, Historical, ReportLongevity>;
     type AncestryHelper = BeefyMmrLeaf;
 }
 
@@ -1567,7 +1567,7 @@ impl pallet_external_validators_rewards::Config for Runtime {
         UseSnowbridgeV2,
     >;
     type OutboundQueue =
-    tp_bridge::VersionedTanssiEthMessageSender<Runtime, GetAggregateMessageOriginTanssi>;
+        tp_bridge::VersionedTanssiEthMessageSender<Runtime, GetAggregateMessageOriginTanssi>;
     type Currency = Balances;
     type RewardsEthereumSovereignAccount = EthereumSovereignAccount;
     type TokenLocationReanchored = TokenLocationReanchored;
@@ -1592,7 +1592,7 @@ impl pallet_external_validator_slashes::Config for Runtime {
         UseSnowbridgeV2,
     >;
     type OutboundQueue =
-    tp_bridge::VersionedTanssiEthMessageSender<Runtime, GetAggregateMessageOriginTanssi>;
+        tp_bridge::VersionedTanssiEthMessageSender<Runtime, GetAggregateMessageOriginTanssi>;
     type ExternalIndexProvider = ExternalValidators;
     type QueuedSlashesProcessedPerBlock = ConstU32<10>;
     type WeightInfo = weights::pallet_external_validator_slashes::SubstrateWeight<Runtime>;
@@ -1756,7 +1756,7 @@ impl pallet_services_payment::Config for Runtime {
     /// The maximum number of session credits that can be accumulated
     type FreeCollatorAssignmentCredits = FreeCollatorAssignmentCredits;
     type ManagerOrigin =
-    EitherOfDiverse<pallet_registrar::EnsureSignedByManager<Runtime>, EnsureRoot<AccountId>>;
+        EitherOfDiverse<pallet_registrar::EnsureSignedByManager<Runtime>, EnsureRoot<AccountId>>;
     type WeightInfo = weights::pallet_services_payment::SubstrateWeight<Runtime>;
 }
 
@@ -2100,7 +2100,7 @@ pub type TxExtension = (
 
 /// Unchecked extrinsic type as expected by this runtime.
 pub type UncheckedExtrinsic =
-generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, TxExtension>;
+    generic::UncheckedExtrinsic<Address, RuntimeCall, Signature, TxExtension>;
 
 /// The runtime migrations per release.
 pub mod migrations {
@@ -2130,9 +2130,9 @@ pub struct InnerDancelightRegistrar<Runtime, AccountId, RegistrarManager, Regist
     PhantomData<(Runtime, AccountId, RegistrarManager, RegistrarWeightInfo)>,
 );
 impl<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo> RegistrarHandler<AccountId>
-for InnerDancelightRegistrar<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo>
+    for InnerDancelightRegistrar<Runtime, AccountId, RegistrarManager, RegistrarWeightInfo>
 where
-    RegistrarManager: RegistrarInterface<AccountId=AccountId>,
+    RegistrarManager: RegistrarInterface<AccountId = AccountId>,
     RegistrarWeightInfo: paras_registrar::WeightInfo,
     Runtime: pallet_registrar::Config + paras_registrar::Config,
     sp_runtime::AccountId32: From<AccountId>,
@@ -2225,7 +2225,7 @@ where
 
 impl pallet_registrar::Config for Runtime {
     type RegistrarOrigin =
-    EitherOfDiverse<pallet_registrar::EnsureSignedByManager<Runtime>, EnsureRoot<AccountId>>;
+        EitherOfDiverse<pallet_registrar::EnsureSignedByManager<Runtime>, EnsureRoot<AccountId>>;
     type MarkValidForCollatingOrigin = EnsureRoot<AccountId>;
     type MaxLengthParaIds = MaxLengthParaIds;
     type MaxGenesisDataSize = MaxEncodedGenesisDataSize;
@@ -2311,7 +2311,7 @@ impl pallet_registrar::RegistrarHooks for DancelightRegistrarHooks {
             <Runtime as pallet_data_preservers::Config>::AssignmentOrigin::try_successful_origin(
                 &para_id,
             )
-                .expect("should be able to get para manager");
+            .expect("should be able to get para manager");
 
         DataPreservers::start_assignment(
             para_manager,
@@ -2319,7 +2319,7 @@ impl pallet_registrar::RegistrarHooks for DancelightRegistrarHooks {
             para_id,
             tp_data_preservers_common::AssignerExtra::Free,
         )
-            .expect("assignment to work");
+        .expect("assignment to work");
 
         assert!(
             pallet_data_preservers::Assignments::<Runtime>::get(para_id).contains(&profile_id),
@@ -3754,7 +3754,7 @@ impl ParaIdAssignmentHooksImpl {
             &pallet_services_payment::Pallet::<Runtime>::parachain_tank(para_id),
             remaining_to_pay,
         )
-            .into_result(true)?;
+        .into_result(true)?;
 
         Ok(())
     }
@@ -3774,7 +3774,7 @@ impl<AC> ParaIdAssignmentHooks<BalanceOf<Runtime>, AC> for ParaIdAssignmentHooks
                     &Some(max_tip),
                 ))
             })
-                .is_ok()
+            .is_ok()
         });
     }
 
@@ -3797,7 +3797,7 @@ impl<AC> ParaIdAssignmentHooks<BalanceOf<Runtime>, AC> for ParaIdAssignmentHooks
                     maybe_tip,
                 )
             })
-                .is_ok()
+            .is_ok()
         });
     }
 
@@ -3872,7 +3872,7 @@ impl pallet_collator_assignment::Config for Runtime {
     type SessionIndex = u32;
     type SelfParaId = MockParaId;
     type ShouldRotateAllCollators =
-    RotateCollatorsEveryNSessions<ConfigurationCollatorRotationSessionPeriod>;
+        RotateCollatorsEveryNSessions<ConfigurationCollatorRotationSessionPeriod>;
     type Randomness = pallet_collator_assignment::SolochainRandomness<
         BabeGetCollatorAssignmentRandomness<Runtime>,
     >;
@@ -3926,7 +3926,7 @@ mod benchmark_helpers {
                     slot: slot.into(),
                     authority_index: 0,
                 })
-                    .encode(),
+                .encode(),
             )],
         };
 
