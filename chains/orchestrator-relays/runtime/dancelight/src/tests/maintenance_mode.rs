0b9e3e0d3f85dc3bf720bcec3e1020b30fbd9215
Tomasz Polaczyk <tmpolaczyk@gmail.com>
2026-01-23 11:45:07 +0100

    WIP

--- chains/orchestrator-relays/runtime/dancelight/src/tests/maintenance_mode.rs
+++ chains/orchestrator-relays/runtime/dancelight/src/tests/maintenance_mode.rs
@@ -274,7 +274,7 @@ fn test_non_filtered_calls_maintenance_mode() {
 
             assert_call_not_filtered(RuntimeCall::EthereumBeaconClient(
                 snowbridge_pallet_ethereum_client::Call::<Runtime>::force_checkpoint {
-                    update: Box::new(snowbridge_pallet_ethereum_client::mock_electra::load_checkpoint_update_fixture()),
+                    update: Box::new(snowbridge_pallet_ethereum_client::mock::load_checkpoint_update_fixture()),
                 },
             ));
 
